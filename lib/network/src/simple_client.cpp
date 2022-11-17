51a2af1ef49e54fe51debb6f1462b8f12b1f4a68
Max Plauth <smaxen@me.com>
2022-11-17 07:49:34 +0100

    migrated over 3_Network_Communication

---
+++ lib/network/src/simple_client.cpp
@@ -0,0 +1,61 @@
+//
+// Created by ankit on 08.11.22.
+//
+
+#include <iostream>
+#include "../include/net_uhcustom.h"
+
+/**
+ * @brief CustomClient class inherits the client interface class and is used to connect to a server in an asynchronous fashion.
+ *
+ * The implementation details of the client-server communication is abstracted away by the client interface class.
+ *
+ */
+class CustomClient : public uh::net::client_interface<CustomMsgTypes> {
+public:
+    /**
+     * @brief pingServer is used to generate a custom ping message that encapsulates the time in order to see the round trip time.
+     * @return void
+     */
+    void pingServer() {
+        uh::net::message<CustomMsgTypes> pingMsg;
+        pingMsg.header.id = CustomMsgTypes::Ping;
+
+        // bad practice, but for now we know the system
+        std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
+        pingMsg << timeNow;
+        std::cout << "Sending Ping" << std::endl;
+        sendCus(pingMsg);
+    }
+};
+
+template<> std::atomic<std::size_t> uh::net::connection<CustomMsgTypes>::s_runningConnections{0};
+
+int main() {
+    CustomClient cl;
+    cl.connect("127.0.0.1", 6000);
+    bool bQuit = false;
+    cl.pingServer();
+    while (!bQuit) {
+        if (cl.isConnected()) {
+            if (!cl.incoming().empty()) {
+                auto msg = cl.incoming().pop_front().msg;
+                switch (msg.header.id) {
+                    case CustomMsgTypes::Ping: {
+                        std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
+                        std::chrono::system_clock::time_point timeThen;
+                        msg >> timeThen;
+                        std::cout << "Received Pong: " << std::chrono::duration<double>(timeNow - timeThen).count() << "\n";
+//                        bQuit = true;
+                    }
+                        break;
+                }
+            }
+        } else {
+            std::cout << "Disconnected from the server\n";
+            bQuit = true;
+        }
+    }
+    return 0;
+}
+
---
+++ lib/network/test/testDummy.cpp
@@ -0,0 +1,25 @@
+//
+// Created by max on 02.11.22.
+//
+
+#define BOOST_TEST_DYN_LINK
+#ifdef SINGLE_TEST_RUNNER
+#define BOOST_TEST_NO_MAIN
+#else
+#define BOOST_TEST_MODULE "uhLibNetwork Dummy Tests"
+#endif
+
+#include <boost/test/unit_test.hpp>
+#include "net_client.h"
+#include "net_common.h"
+#include "net_connection.h"
+#include "net_message.h"
+#include "net_queue_ts.h"
+#include "net_server.h"
+#include "net_uhcustom.h"
+
+// ------------- Tests Follow --------------
+BOOST_AUTO_TEST_CASE( dummy )
+{
+    BOOST_CHECK(true);
+}
