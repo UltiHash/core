6890bb95be19534ce9ed4ee5f1fb82f4a506c0e3
Juan Manuel Carmona Loaiza <14291849+juanmcloaiza@users.noreply.github.com>
2022-12-12 14:14:19 +0100

    TEA-156: Enable Agency Node integration (#9)
    * Dump storage ready to be integrated; hashing function working
    * Implement get_filepath_from_hash
    * Use vec<char> for hashes; use formatted std::strings for filenames
    * Avoid race condition: write to tmp file then std::rename it
    *Tests :
      * hashing_function_expected_hash
      * dump_storage_io
      * dump_storage_no_duplicates
      * dump_storage_expected_hash

--- server-db/src/storage_backend.cpp
+++ server-db/src/storage_backend.cpp
@@ -1,35 +1,100 @@
 #include "storage_backend.h"
-#include <fstream>
-#include <iostream>
-#include <boost/filesystem.hpp>
 #include <logging/logging_boost.h>
 
 using namespace uh::dbn;
 namespace fs = boost::filesystem;
 
+// ---------------------------------------------------------------------
 
-std::vector<char> dump_storage::write_block(const std::vector<char>& some_data){
+bool uh::dbn::maybe_write_data_to_filepath(const std::vector<char> &some_data, const fs::path &filepath){
+    if(fs::exists(filepath)) {
+        INFO << "Data block already exists at " << filepath;
+        return false;
+    }
 
-    std::string hash_str = "ThisIsTheHashKey";
-    std::vector<char> hash_vec(hash_str.begin(), hash_str.end());
+    std::vector<char> buffer(L_tmpnam, 0);
+    fs::path tmpf = std::tmpnam(&buffer[0]);
 
-
-    fs::path filepath = this->m_config.db_file_fullpath;
-    std::ofstream outfile(filepath, std::ios::out | std::ios::binary);
+    std::ofstream outfile(tmpf, std::ios::out | std::ios::binary);
     if(!outfile.is_open()) {
         std::string msg("Error opening file: " + filepath.string());
         throw std::ofstream::failure(msg);
     }
     outfile.write(some_data.data(), some_data.size());
-
     DEBUG << "Block written to " << filepath;
+
+    fs::rename(tmpf, filepath);
+    return true;
+}
+
+// ---------------------------------------------------------------------
+
+std::vector<char> uh::dbn::sha512(const std::vector<char>& some_data){
+    DEBUG << "Beginning hash func";
+
+    unsigned char result[SHA512_DIGEST_LENGTH];
+    SHA512((unsigned char*) some_data.data(), some_data.size(), result);
+    DEBUG << "sha512 calculated";
+
+    // What is a better way of accomplishing this?
+    ///////////////////////////////////////////////////////////////////////////
+    char result_char[SHA512_DIGEST_LENGTH];
+    for(int i=0; i<SHA512_DIGEST_LENGTH; i++)
+        result_char[i] = static_cast<char>(result[i]);
+    std::vector<char> hash_vec(std::begin(result_char), std::end(result_char));
+    ///////////////////////////////////////////////////////////////////////////
 
     return hash_vec;
 }
 
+// ---------------------------------------------------------------------
+
+fs::path dump_storage::get_filepath_from_hash(const std::vector<char> &hash){
+
+    std::string hash_string = to_hex_string(hash.begin(), hash.end());
+
+    DEBUG << "---------------------------";
+    DEBUG << "Here is the formated sha512 string";
+    DEBUG << hash_string;
+    DEBUG << "---------------------------";
+
+
+   fs::path filepath = this->m_config.db_dir / hash_string;
+   return filepath;
+}
+
+// ---------------------------------------------------------------------
+
+std::vector<char> dump_storage::hashing_function(const std::vector<char> &data) {
+    return uh::dbn::sha512(data);
+}
+
+// ---------------------------------------------------------------------
+
+std::vector<char> dump_storage::write_block(const std::vector<char>& some_data){
+
+    std::vector<char> hash_vec = this->hashing_function(some_data);
+    std::string hash_string(hash_vec.begin(), hash_vec.end());
+
+    DEBUG << "hash string (should look like binary rubish):" << hash_string;
+
+    fs::path filepath = this->get_filepath_from_hash(hash_vec);
+
+    if(maybe_write_data_to_filepath(some_data, filepath)){
+        INFO << "Data block written to " << filepath;
+    }
+    else{
+        INFO << "Skipped writing to existing filepath: " << filepath;
+    };
+
+    return  hash_vec;
+}
+
 std::vector<char> dump_storage::read_block(const std::vector<char>& some_hash){
 
-    fs::path filepath = this->m_config.db_file_fullpath;
+    std::string hash_string(some_hash.begin(), some_hash.end());
+
+    fs::path filepath = this->get_filepath_from_hash(some_hash);
     std::ifstream infile(filepath, std::ios::binary);
 
     if(!infile.is_open()) {
