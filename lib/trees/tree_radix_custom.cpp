21b1a7d56f149494acf5b78146f5250df3f9ec2f
Benjamin-Elias Probst <benni.probst@gmx.de>
2022-12-18 13:10:02 +0100

    split function and definition

--- lib/trees/tree_radix_custom.cpp
+++ lib/trees/tree_radix_custom.cpp
@@ -3,3 +3,130 @@
 //
 
 #include "tree_radix_custom.h"
+
+uh::util::tree_radix_custom::tree_radix_custom() {
+    for(auto & i : children){
+        i = nullptr;
+    }
+}
+
+std::list<uh::util::tree_radix_custom *>
+uh::util::tree_radix_custom::add(const char *bin, std::size_t len, std::list<tree_radix_custom *> enlist) {
+    if(len>0) {
+        std::shared_lock lock(local_m);
+        if (length == 0) {
+            bool has_children = false;
+            for(const auto & i : children){
+                if(i != nullptr){
+                    has_children = true;
+                    break;
+                }
+            }
+            if (!has_children) {
+                if (data != nullptr)std::free(data);
+                data = static_cast<char *>(malloc(sizeof(char) * len));
+                std::memcpy(data, bin, len);
+                length = len;
+            } else {
+                if(children[(unsigned char)bin[0]] == nullptr){
+                    // no match, create new node for rest of string
+                    children[(unsigned char)bin[0]] = (struct tree_radix_custom*) std::malloc(sizeof(struct tree_radix_custom));
+                    new (children[(unsigned char)bin[0]]) tree_radix_custom();
+                    auto enlist_append = children[(unsigned char)bin[0]] -> add(bin,len,enlist);
+                    enlist.push_back(children[(unsigned char)bin[0]]);
+                    enlist.splice(enlist.end(),enlist_append);
+                    return enlist;
+                }
+                else{
+                    enlist.push_back(children[(unsigned char)bin[0]]);
+                    return children[(unsigned char)bin[0]]->add(bin,len,enlist);
+                }
+            }
+        } else {
+            std::size_t i = 0;
+            for(; i < std::min(length,len); i++){
+                if(data[i] != bin[i])break;
+            }
+            if(i == length-1){
+                if(len>length){
+                    //insert deeper node directly on rest
+                    if(children[(unsigned char)bin[i+1]] == nullptr){
+                        auto* tmp = (struct tree_radix_custom*) std::malloc(sizeof(struct tree_radix_custom));
+                        new (tmp) tree_radix_custom();
+                        children[(unsigned char)bin[i+1]] = tmp;
+                        enlist.push_back(tmp);
+                        return tmp->add(bin+i+1,length-i,enlist);
+                    }
+                    else{
+                        enlist.push_back(children[(unsigned char)bin[i+1]]);
+                        return children[(unsigned char)bin[i+1]]->add(bin+i+1,length-i,enlist);
+                    }
+                }
+                // direct match, direct redirect
+                enlist.push_back(this);
+                return enlist;
+            }
+            if(i < length - 1){
+                // match string is too short -> split node
+                // TODO: notifiy changes in case of depending tree
+                char* higher_node = (char*) std::malloc(i*sizeof(char));
+                std::memcpy(higher_node,data,i);
+                length = i;
+                char* lower_node = (char*) std::malloc((length-i)*sizeof(char));
+                std::memcpy(lower_node,data+i+1,length-i);
+                std::free(data);
+                data = higher_node;
+                auto* tmp = (struct tree_radix_custom*) std::malloc(sizeof(struct tree_radix_custom));
+                new (tmp) tree_radix_custom();
+                std::memcpy(tmp->children,this->children,N * sizeof(tree_radix_custom*));
+                tmp->data = lower_node;
+                tmp->length = length-i;
+                for(auto & i1 : children){
+                    i1 = nullptr;
+                }
+                children[(unsigned char)lower_node[0]] = tmp;
+                enlist.push_back(tmp);
+                enlist.push_back(this);
+                return enlist;
+            }
+        }
+    }
+}
+
+uh::util::tree_radix_custom *uh::util::tree_radix_custom::copy() {
+    auto* tmp = (struct tree_radix_custom*) std::malloc(sizeof(struct tree_radix_custom));
+    new (tmp) tree_radix_custom();
+    std::memcpy(tmp->children,this->children,N * sizeof(tree_radix_custom*));
+    tmp->data = (char*) std::malloc(length*sizeof(char));
+    std::memcpy(tmp->data,data,length);
+    tmp->length=length;
+    return tmp;
+}
+
+uh::util::tree_radix_custom *uh::util::tree_radix_custom::copy_recursive() {
+    auto* tmp = copy();
+    for(unsigned char i=0;i<(unsigned char)N;i++){
+        if(children[i] != nullptr){
+            tmp->children[i] = children[i]->copy_recursive();
+        }
+    }
+    return tmp;
+}
+
+void uh::util::tree_radix_custom::destroy_recursive() {
+    for(auto & i : children){
+        if(i != nullptr){
+            i -> destroy_recursive();
+            delete i;
+        }
+    }
+}
+
+void uh::util::tree_radix_custom::destroy_recursive(char sub) {
+    if(children[sub] != nullptr){
+        children[sub] -> destroy_recursive();
+        std::free(children[sub] -> data);
+        std::free(children[sub]);
+        children[sub] = nullptr;
+    }
+}
