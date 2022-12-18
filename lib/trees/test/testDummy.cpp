e2ef551dde26280335ff26f7238a598605201bd1
Benjamin-Elias Probst <benni.probst@gmx.de>
2022-12-18 21:20:07 +0100

    constructor test radix

--- lib/trees/test/testDummy.cpp
+++ lib/trees/test/testDummy.cpp
@@ -23,7 +23,7 @@ BOOST_AUTO_TEST_CASE( constructor )
     BOOST_CHECK(std::strncmp(t_hello.data_blob(),"Hello World",11)==0);
 
     char input[11];
-    std::strcpy(input,"Hello World");
+    std::strncpy(input,"Hello World",11);
     uh::trees::tree_radix_custom t_hello2(input,11);
     BOOST_CHECK(!t_hello2.has_children());
     BOOST_CHECK(t_hello2.size()==11);
