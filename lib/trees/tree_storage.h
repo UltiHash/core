d4855c048b96cc657f5f27f611497839cdd3e7ff
Benjamin-Elias Probst <benni.probst@gmx.de>
2022-12-19 16:22:01 +0100

    start storage tree

---
+++ lib/trees/tree_storage.h
@@ -0,0 +1,37 @@
+//
+// Created by benjamin-elias on 19.12.22.
+//
+
+#ifndef UHLIBCOMMON_TREE_RADIX_CUSTOM_H
+#define UHLIBCOMMON_TREE_RADIX_CUSTOM_H
+
+#include "conceptTypes/conceptTypes.h"
+#include "logging/logging_boost.h"
+#include <shared_mutex>
+
+namespace uh::trees{
+#define N 256
+    typedef struct tree_storage tree_storage;
+
+    struct tree_storage {
+    protected:
+        tree_storage* children[N]{};//deeper blocks
+        std::vector<bool> available_store_chunks{};
+        std::size_t size{};
+    public:
+        tree_storage() {
+            available_store_chunks.reserve(N);
+
+            for(auto & i : children){
+                i = nullptr;
+            }
+        }
+
+        tree_storage * child(char i){
+            return children[(unsigned char)i];
+        }
+        
+    };
+}
+
+#endif //UHLIBCOMMON_TREE_RADIX_CUSTOM_H
