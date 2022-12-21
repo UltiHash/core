e306b0059635becf4c71495c2c7bf5a9e849434e
Benjamin-Elias Probst <benni.probst@gmx.de>
2022-12-21 09:59:59 +0100

    binary generator

--- lib/trees/test/test_storage.cpp
+++ lib/trees/test/test_storage.cpp
@@ -11,10 +11,28 @@
 
 #include <boost/test/unit_test.hpp>
 #include <trees/tree_storage.h>
+#include <random>
+
+std::vector<unsigned char> binary_generator(){
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(16,STORE_MAX);
+
+    std::size_t len = dist(rng);
+
+    std::random_device dev2;
+    std::mt19937 rng2(dev2());
+    std::uniform_int_distribution<std::mt19937::result_type> dist2(0,UINT8_MAX);
+    std::vector<unsigned char> rands;
+    rands.reserve(len);
+    for(std::size_t i = 0; i<len;i++){
+        rands.push_back(dist2(rng2));
+    }
+    return rands;
+}
 
 // ------------- Tests Follow --------------
-BOOST_AUTO_TEST_CASE( constructor )
+BOOST_AUTO_TEST_CASE( constructor1 )
 {
-    uh::trees::tree_storage t1("/mnt/md0");
-    //TODO: more tests
+    uh::trees::tree_storage t1("/mnt/md0");//A test folder reserved for tree storage
 }
