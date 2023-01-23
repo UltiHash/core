4f8d2f2664ccfc076bac9796cedead985607bbc8
Benjamin-Elias Probst <benjamin@ultihash.io>
2023-01-23 20:22:17 +0100

    compare_ultihash fails

--- lib/trees/test/test_radix.cpp
+++ lib/trees/test/test_radix.cpp
@@ -18,9 +18,21 @@ BOOST_AUTO_TEST_CASE( compare_test )
     BOOST_CHECK(t.children_reference().empty());
     std::string hello_string = "Hello World";
     std::string hello_string_long = "Hello World of tomorrow!";
-    const auto data_string = std::vector<unsigned char>{hello_string.begin(),hello_string.end()};
-    const auto data_string_long = std::vector<unsigned char>{hello_string_long.begin(),hello_string_long.end()};
-    auto result = t.compare_ultihash(data_string_long.cbegin(),data_string_long.cend(),data_string.cbegin(),data_string.cend());
+    std::string hello_string_late = "Do not say \"Hello World!\"";
+    auto data_string = std::vector<unsigned char>{hello_string.begin(),hello_string.end()};
+    auto data_string_long = std::vector<unsigned char>{hello_string_long.begin(),hello_string_long.end()};
+    auto data_string_late = std::vector<unsigned char>{hello_string_late.begin(),hello_string_late.end()};
+
+    auto result1 = t.compare_ultihash(data_string_long.cbegin(),data_string_long.cend(),data_string.cbegin(),data_string.cend());
+    auto result2 = t.compare_ultihash(data_string_late.cbegin(),data_string_late.cend(),data_string_long.cbegin(),data_string_long.cend());
+
+    BOOST_CHECK(std::get<0>(result1[0])==data_string.cbegin());
+    BOOST_CHECK(std::get<1>(result1[0])==data_string.cbegin()+11);
+    BOOST_CHECK(std::get<2>(result1[0])==data_string_long.cbegin());
+
+    BOOST_CHECK(std::get<0>(result2[0])==data_string_long.cbegin());
+    BOOST_CHECK(std::get<1>(result2[0])==data_string_long.cbegin()+11);
+    BOOST_CHECK(std::get<2>(result2[0])==data_string_late.cbegin()+12);
 }
 BOOST_AUTO_TEST_CASE( constructor )
 {
