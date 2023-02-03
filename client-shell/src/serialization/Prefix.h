2144f6f991d3e08c019207477fec4b9133719ad7
Max Plauth <max@ultihash.io>
2023-02-03 11:46:59 +0100

    further attempt to fix darwin build

--- client-shell/src/serialization/Prefix.h
+++ client-shell/src/serialization/Prefix.h
@@ -5,6 +5,7 @@
 #ifndef SCHOOL_PROJECT_PREFIX_H
 #define SCHOOL_PROJECT_PREFIX_H
 
+#include <boost/predef.h>
 #include "EnDecoder.h"
 
 class Prefix{
@@ -58,23 +59,45 @@ public:
 
         std::string aSec,aNSec,mSec,mNSec;
 
+ #ifdef BOOST_OS_MACOS
+        aSec = write_pod(advancedFileInfo.st_atimespec.tv_sec).str();
+ #else
         aSec = write_pod(advancedFileInfo.st_atim.tv_sec).str();
+ #endif
         std::for_each(aSec.begin(),aSec.end(),[&oss](char c){oss.push_back(c);});
 
+#ifdef BOOST_OS_MACOS
+        aNSec = write_pod(advancedFileInfo.st_atimespec.tv_nsec).str();
+#else
         aNSec = write_pod(advancedFileInfo.st_atim.tv_nsec).str();
+#endif
         std::for_each(aNSec.begin(),aNSec.end(),[&oss](char c){oss.push_back(c);});
 
+#ifdef BOOST_OS_MACOS
+        mSec = write_pod(advancedFileInfo.st_mtimespec.tv_sec).str();
+#else
         mSec = write_pod(advancedFileInfo.st_mtim.tv_sec).str();
+#endif
         std::for_each(mSec.begin(),mSec.end(),[&oss](char c){oss.push_back(c);});
 
+#ifdef BOOST_OS_MACOS
+        mNSec = write_pod(advancedFileInfo.st_mtimespec.tv_nsec).str();
+#else
         mNSec = write_pod(advancedFileInfo.st_mtim.tv_nsec).str();
+#endif
         std::for_each(mNSec.begin(),mNSec.end(),[&oss](char c){oss.push_back(c);});
 
+#ifdef BOOST_OS_MACOS
+        newTimes[0].tv_sec = advancedFileInfo.st_atimespec.tv_sec;
+        newTimes[0].tv_usec = advancedFileInfo.st_atimespec.tv_nsec;
+        newTimes[1].tv_sec = advancedFileInfo.st_mtimespec.tv_sec;
+        newTimes[1].tv_usec = advancedFileInfo.st_mtimespec.tv_nsec;
+#else
         newTimes[0].tv_sec = advancedFileInfo.st_atim.tv_sec;
         newTimes[0].tv_usec = advancedFileInfo.st_atim.tv_nsec;
         newTimes[1].tv_sec = advancedFileInfo.st_mtim.tv_sec;
         newTimes[1].tv_usec = advancedFileInfo.st_mtim.tv_nsec;
-
+#endif
         EnDecoder coder2{};
         return coder2.encode<OutType>(oss);//TODO: encoder later on also contains parity checksum
     }
