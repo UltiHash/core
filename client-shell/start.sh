4951030301f10162cd767a98bebced6dd1bfba57
Max Plauth <smaxen@me.com>
2023-02-03 09:49:25 +0100

    fixed entrypoint script for client-shell (#7)
    Co-authored-by: Max Plauth <max@ultihash.io>

--- client-shell/start.sh
+++ client-shell/start.sh
@@ -25,18 +25,21 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
+# prepare crude test workload, using the uhClient binary itself
 mkdir -p test
 cp /usr/local/bin/uhClient test
+
+# store checksums of test workload, integrate data into UltiHash volume and delete files afterwards
 sha512sum test/uhClient > checksum.txt
 uhClient -i test.uh test -a uh-server-agency:21832 -M
-
 rm -Rf test
-yes | uhClient -r ./test.uh -a uh-server-agency:21832 -M
-mv test test_reconstructed
-mv test_reconstructed/test .
-rm -Rf test_reconstructed
 
+# retrieve test workload and validate their checksums
+yes | uhClient -r -T ./ ./test.uh -a uh-server-agency:21832 -M
 cat checksum.txt | sha512sum -c
+
+# cleanup
 rm -Rf test*
+rm -f checksum.txt
 
 sleep infinity
