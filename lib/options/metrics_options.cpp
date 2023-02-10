7b40e55b55008653ea86ad14f01b80037ef0bc3c
Stefan Jank <stefan@ultihash.io>
2023-02-10 13:05:50 +0100

    Skip reading metrics over variables.

--- lib/options/metrics_options.cpp
+++ lib/options/metrics_options.cpp
@@ -12,9 +12,9 @@ metrics_options::metrics_options()
     : m_desc("Metrics Options")
 {
     m_desc.add_options()
-        ("metrics-address", value<std::string>()->default_value("0.0.0.0:8080"), "listening address for metrics service")
-        ("metrics-threads", value<std::size_t>()->default_value(2), "number of threads used for metrics service")
-        ("metrics-path", value<std::string>()->default_value("/metrics"), "path used to reach metrics information");
+        ("metrics-address", value<std::string>(&m_config.address)->default_value("0.0.0.0:8080"), "listening address for metrics service")
+        ("metrics-threads", value<std::size_t>(&m_config.threads)->default_value(2), "number of threads used for metrics service")
+        ("metrics-path", value<std::string>(&m_config.path)->default_value("/metrics"), "path used to reach metrics information");
 }
 
 // ---------------------------------------------------------------------
@@ -28,9 +28,6 @@ void metrics_options::apply(options& opts)
 
 void metrics_options::evaluate(const boost::program_options::variables_map& vars)
 {
-    m_config.address = vars["metrics-address"].as<std::string>();
-    m_config.threads = vars["metrics-threads"].as<std::size_t>();
-    m_config.path = vars["metrics-path"].as<std::string>();
 }
 
 // ---------------------------------------------------------------------
