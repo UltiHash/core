41bcf9d99c24fcce8b6fe7965959344ce79246df
Ankit Pandit <anktpndit@gmail.com>
2023-02-15 17:02:56 +0100

    prettify

--- client-shell/src/serialization/f_upload.cpp
+++ client-shell/src/serialization/f_upload.cpp
@@ -6,7 +6,10 @@ namespace uh::client::serialization
 
 // ---------------------------------------------------------------------
 
-f_upload::f_upload(std::unique_ptr<protocol::client_pool>&& cl_pool, common::job_queue<std::unique_ptr<common::f_meta_data>>& in_jq, common::job_queue<std::unique_ptr<common::f_meta_data>>& out_jq, size_t num_threads) :
+f_upload::f_upload(std::unique_ptr<protocol::client_pool>&& cl_pool,
+                   common::job_queue<std::unique_ptr<common::f_meta_data>>& in_jq,
+                   common::job_queue<std::unique_ptr<common::f_meta_data>>& out_jq,
+                   size_t num_threads) :
         m_client_pool(std::move(cl_pool)), m_input_jq(in_jq), m_output_jq(out_jq), common::thread_manager(num_threads)
 {
 
@@ -21,32 +24,37 @@ f_upload::~f_upload()
 
 // ---------------------------------------------------------------------
 
-void f_upload::upload_files(std::unique_ptr<common::f_meta_data>&& f_meta_data, protocol::client_pool::handle& client_handle)
+void f_upload::upload_files(std::unique_ptr<common::f_meta_data>&& f_meta_data,
+                            protocol::client_pool::handle& client_handle)
 {
     if (S_ISREG(f_meta_data->get_f_stat().st_mode))
     {
 
         std::ifstream input_file(f_meta_data->get_f_path(),std::ios::in | std::ios::binary);
+
         if (!input_file.is_open())
             throw std::runtime_error("Error: Could not open file " + f_meta_data->get_f_path().string() + "\n");
 
         if (input_file.peek() != std::ifstream::traits_type::eof())
         {
-
             constexpr std::size_t buf_size = 1 << 22;
             std::vector<char> tmp_buffer(std::min(std::size_t(input_file.tellg()), buf_size));
+
             while (true)
             {
                 input_file.read((tmp_buffer.data()), buf_size);
                 std::streamsize bytes_read = input_file.gcount();
+
                 if (bytes_read == 0)
                 {
                     break;
                 }
+
                 auto recv_hash = client_handle.m_client->write_chunk(tmp_buffer);
                 f_meta_data->add_hash(recv_hash);
 
             }
+
         }
 
         input_file.close();
@@ -65,6 +73,7 @@ void f_upload::spawn_threads()
            {
 
                protocol::client_pool::handle&& client_connection_handle = m_client_pool->get();
+
                while (auto&& item = m_input_jq.get_job())
                {
 
@@ -74,6 +83,7 @@ void f_upload::spawn_threads()
                        upload_files(std::move(item.value()), client_connection_handle);
 
                }
+
                client_connection_handle.m_pool.put_back(std::move(client_connection_handle.m_client));
 
            });
