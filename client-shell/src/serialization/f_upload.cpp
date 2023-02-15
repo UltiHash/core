9b6008320ef4fc8ca348e1df21d0ff7b91455bd4
Ankit Pandit <anktpndit@gmail.com>
2023-02-15 17:07:42 +0100

    prettify

--- client-shell/src/serialization/f_upload.cpp
+++ client-shell/src/serialization/f_upload.cpp
@@ -9,8 +9,12 @@ namespace uh::client::serialization
 f_upload::f_upload(std::unique_ptr<protocol::client_pool>&& cl_pool,
                    common::job_queue<std::unique_ptr<common::f_meta_data>>& in_jq,
                    common::job_queue<std::unique_ptr<common::f_meta_data>>& out_jq,
-                   size_t num_threads) :
-        m_client_pool(std::move(cl_pool)), m_input_jq(in_jq), m_output_jq(out_jq), common::thread_manager(num_threads)
+                   size_t num_threads)
+                   :
+                    m_client_pool(std::move(cl_pool)),
+                    m_input_jq(in_jq),
+                    m_output_jq(out_jq),
+                    common::thread_manager(num_threads)
 {
 
 }
@@ -30,7 +34,8 @@ void f_upload::upload_files(std::unique_ptr<common::f_meta_data>&& f_meta_data,
     if (S_ISREG(f_meta_data->get_f_stat().st_mode))
     {
 
-        std::ifstream input_file(f_meta_data->get_f_path(),std::ios::in | std::ios::binary);
+        std::ifstream input_file(f_meta_data->get_f_path(),
+                                 std::ios::in | std::ios::binary);
 
         if (!input_file.is_open())
             throw std::runtime_error("Error: Could not open file " + f_meta_data->get_f_path().string() + "\n");
@@ -71,7 +76,6 @@ void f_upload::spawn_threads()
     {
         m_thread_pool.emplace_back([&]()
            {
-
                protocol::client_pool::handle&& client_connection_handle = m_client_pool->get();
 
                while (auto&& item = m_input_jq.get_job())
