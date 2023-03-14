5f38efcdfec7198f142a8c306f493af0d33dd203
Ankit Pandit <anktpndit@gmail.com>
2023-03-14 10:32:48 +0100

    fixed test issues

--- lib/options/loader.h
+++ lib/options/loader.h
@@ -16,6 +16,8 @@ class loader
 public:
     virtual ~loader() = default;
 
+    virtual action evaluate(int argc, const char** argv);
+
     loader& add(options& opt);
 
     const boost::program_options::options_description& visible() const;
@@ -26,7 +28,6 @@ protected:
 
     void parse(const std::filesystem::path &path);
     action finalize();
-    virtual action evaluate(int argc, const char** argv);
 
 private:
     std::list<options*> m_opts;
