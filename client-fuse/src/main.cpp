257e31ccdb13789373b725a1ab8e38c14a3edaac
Masi <masoud@ultihash.io>
2023-03-22 15:05:24 +0100

    Fuse main call

--- client-fuse/src/main.cpp
+++ client-fuse/src/main.cpp
@@ -41,91 +41,39 @@ static const struct fuse_opt option_spec[] =
         FUSE_OPT_END
     };
 
-static void *hello_init(struct fuse_conn_info *conn,
-                        struct fuse_config *cfg)
+int uh_getattr (const char *, struct stat *)
 {
-    (void) conn;
-    cfg->kernel_cache = 1;
+    return 0;
+}
+
+void *uh_init (struct fuse_conn_info *conn)
+{
     return NULL;
 }
 
-static int hello_getattr(const char *path, struct stat *stbuf,
-                         struct fuse_file_info *fi)
+int uh_readdir (const char *, void *, fuse_fill_dir_t, off_t, struct fuse_file_info *)
 {
-    (void) fi;
-    int res = 0;
-
-    memset(stbuf, 0, sizeof(struct stat));
-    if (strcmp(path, "/") == 0)
-    {
-        stbuf->st_mode = S_IFDIR | 0755;
-        stbuf->st_nlink = 2;
-    } else if (strcmp(path+1, options.filename) == 0)
-    {
-        stbuf->st_mode = S_IFREG | 0444;
-        stbuf->st_nlink = 1;
-        stbuf->st_size = strlen(options.contents);
-    } else
-        res = -ENOENT;
-
-    return res;
-}
-
-static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-                         off_t offset, struct fuse_file_info *fi,
-                         enum fuse_readdir_flags flags)
-{
-    (void) offset;
-    (void) fi;
-    (void) flags;
-
-    if (strcmp(path, "/") != 0)
-        return -ENOENT;
-
-    filler(buf, ".", NULL, 0, 0);
-    filler(buf, "..", NULL, 0, 0);
-    filler(buf, options.filename, NULL, 0, 0);
-
     return 0;
 }
 
-static int hello_open(const char *path, struct fuse_file_info *fi)
+int uh_open (const char *, struct fuse_file_info *)
 {
-    if (strcmp(path+1, options.filename) != 0)
-        return -ENOENT;
-
-    if ((fi->flags & O_ACCMODE) != O_RDONLY)
-        return -EACCES;
-
     return 0;
 }
 
-static int hello_read(const char *path, char *buf, size_t size, off_t offset,
-                      struct fuse_file_info *fi)
+int uh_read (const char *, char *, size_t, off_t,
+		     struct fuse_file_info *)
 {
-    size_t len;
-    (void) fi;
-    if(strcmp(path+1, options.filename) != 0)
-        return -ENOENT;
-
-    len = strlen(options.contents);
-    if (offset < len) {
-        if (offset + size > len)
-            size = len - offset;
-        memcpy(buf, options.contents + offset, size);
-    } else
-        size = 0;
-
-    return size;
+    return 0;
 }
 
-static const struct fuse_operations hello_oper =
+static const struct fuse_operations uh_operations =
     {
-    .init           = hello_init,
-    .getattr        = hello_getattr,
-    .readdir        = hello_readdir,
-    .open           = hello_open,
-    .read           = hello_read,
+    .getattr        = uh_getattr,
+    .open           = uh_open,
+    .read           = uh_read,
+    .readdir        = uh_readdir,
+    .init           = uh_init,
     };
 
 static void show_help(const char *progname)
@@ -143,6 +91,8 @@ int main(int argc, char *argv[])
 {
     try
     {
+        int ret;
+
         struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
 
         options.UHVpath= strdup("/home/");
@@ -159,7 +109,7 @@ int main(int argc, char *argv[])
             args.argv[0][0] = '\0';
         }
 
-        ret = fuse_main(args.argc, args.argv, &hello_oper, NULL);
+        ret = fuse_main(args.argc, args.argv, &uh_operations, NULL);
         fuse_opt_free_args(&args);
         return ret;
     }
