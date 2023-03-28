4a88f73e624da1fbac537252b51455a33ae32b6c
Juan M. Carmona Loaiza <juan@ultihash.io>
2023-03-28 14:39:51 +0200

    Add comment on chunking strategies

--- client-shell/src/chunking/mod.h
+++ client-shell/src/chunking/mod.h
@@ -9,6 +9,13 @@ namespace uh::client::chunking
 {
 enum class ChunkingStrategyEnum {FixedSize, OtherChunkingStrategy};
 
+/*
+  Chunking can be done by following one of several strategies: For instance, Fixed size chunking
+  splits a file in several equal-sized chunks. For such a strategy, different chunk sizes can be
+  chosen. Another strategy is Content Defined Chunking (CDC), for which several algorithms exist.
+  A minimum chunk size and a maximum chunk size can be defined as well.
+*/
+
 static std::unordered_map<std::string, ChunkingStrategyEnum> string2backendtype = {
   {"FixedSize", ChunkingStrategyEnum::FixedSize},
   {"OtherChunkingStrategy", ChunkingStrategyEnum::OtherChunkingStrategy}
