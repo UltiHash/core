7667ab86f77505e660d1324d8fed57d5bbd45e34
Gongotar <masoud@ultihash.io>
2023-04-14 16:13:53 +0200

    message server busy if all threads of server are busy (#68)
    * server_info class that provides the protocol with information about the status of server
    
    * cleanup the comments

--- server-db/src/server/protocol_factory.h
+++ server-db/src/server/protocol_factory.h
@@ -6,6 +6,7 @@
 #include <metrics/protocol_metrics.h>
 
 #include <storage/mod.h>
+#include "net/server_info.h"
 
 
 namespace uh::dbn::server
@@ -18,13 +19,15 @@ class protocol_factory : public uh::protocol::protocol_factory
 public:
     protocol_factory(
         storage::mod& storage,
-        const uh::metrics::protocol_metrics& metrics);
+        const uh::metrics::protocol_metrics& metrics,
+        const uh::net::server_info &serv_info);
 
     std::unique_ptr<uh::protocol::protocol> create(std::shared_ptr<net::socket> client) override;
 
 private:
     storage::mod& m_storage;
     const uh::metrics::protocol_metrics& m_metrics;
+    const uh::net::server_info &m_serv_info;
 };
 
 // ---------------------------------------------------------------------
