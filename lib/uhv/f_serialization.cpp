39e674a7905295e891dc1e0658425ba81fbfed92
Gongotar <masoud@ultihash.io>
2023-04-17 13:01:33 +0200

    added the out flag mode to fstream (#74)

--- lib/uhv/f_serialization.cpp
+++ lib/uhv/f_serialization.cpp
@@ -73,9 +73,9 @@ uint64_t f_serialization::serialize(const std::vector<std::filesystem::path>& ro
     std::uint64_t raw_size = 0;
     std::uint64_t effective_size = 0;
 
-    auto mode = std::ios::app | std::ios::binary;
+    auto mode = std::ios::out | std::ios::app | std::ios::binary;
     if (m_overwrite) {
-        mode = std::ios::trunc | std::ios::binary;
+        mode = std::ios::out | std::ios::trunc | std::ios::binary;
     }
 
     io::file file (m_UHV_path, mode);
