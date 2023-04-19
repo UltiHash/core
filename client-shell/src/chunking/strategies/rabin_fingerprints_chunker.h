a4a504c5d07291ae77365278ff5f999b25f98614
Juan M. Carmona Loaiza <juan@ultihash.io>
2023-04-19 12:03:32 +0200

    Create class rabin_fingerprint_chunker (Internally is still fixed_size)

---
+++ client-shell/src/chunking/strategies/rabin_fingerprints_chunker.h
@@ -0,0 +1,36 @@
+#ifndef CLIENT_CHUNKING_FIXED_SIZE_H
+#define CLIENT_CHUNKING_FIXED_SIZE_H
+
+#include <chunking/file_chunker.h>
+#include <io/device.h>
+
+
+namespace uh::client::chunking
+{
+
+// ---------------------------------------------------------------------
+
+class rabin_fingerprints_chunker : public file_chunker
+{
+public:
+    rabin_fingerprints_chunker(io::device& dev, size_t chunk_size);
+
+    /**
+     * Return the next chunk to upload. If there are no more chunks, return
+     * an empty chunk instead.
+     *
+     * @throw may throw any derivative of exception on error
+     */
+    std::span<char> next_chunk() override;
+
+private:
+    io::device& m_dev;
+    size_t m_chunk_size = 0;
+    std::vector<char> m_buffer;
+};
+
+// ---------------------------------------------------------------------
+
+} // namespace uh::dbn::storage
+
+#endif
