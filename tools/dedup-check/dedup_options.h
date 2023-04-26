e380c7fdc46cefee683bbbf8de970027e5a7f8ac
Masoud Gholami <masoud@ultihash.io>
2023-04-26 17:52:49 +0200

    dedup check

---
+++ tools/dedup-check/dedup_options.h
@@ -0,0 +1,30 @@
+//
+// Created by masi on 4/26/23.
+//
+
+#ifndef CORE_DEDUP_OPTIONS_H
+#define CORE_DEDUP_OPTIONS_H
+#include <options/options.h>
+#include <string>
+
+namespace uh::tools {
+
+struct dedup_config {
+    std::string path;
+};
+
+class dedup_options : public uh::options::options {
+public:
+    dedup_options();
+
+    uh::options::action evaluate(const boost::program_options::variables_map &vars) override;
+
+    const dedup_config &config() const;
+
+private:
+    dedup_config m_config;
+    boost::program_options::options_description m_desc;
+};
+
+}
+#endif //CORE_DEDUP_OPTIONS_H
