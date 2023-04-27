ebbc01241ed69a5208addb1fa37f7b6314771e42
Juan M. Carmona Loaiza <juan@ultihash.io>
2023-04-27 11:12:25 +0200

    Remember partial chunks between read buffers

--- lib/chunking/rabin_polynomial.c
+++ lib/chunking/rabin_polynomial.c
@@ -352,8 +352,9 @@ struct rab_block_info *read_rabin_block(void *buf, size_t size, struct rab_block
 
     }
    
-    size_t bytes_offset = block->total_bytes_read;
-    size_t i, j, J, block_tail_start, partial_fill, block_tail_length;
+    size_t i;
+    size_t bytes_read_prev = block->total_bytes_read;
+    uint8_t partial_chunk_needed = 1; //bool true
     for(i=0;i<size;i++) {
     	char cur_byte=*((char *)(buf+i));
         char pushed_out=block->current_window_data[block->window_pos];
@@ -369,36 +370,43 @@ struct rab_block_info *read_rabin_block(void *buf, size_t size, struct rab_block
         if(block->window_pos == rabin_sliding_window_size){ //Loop back around
             block->window_pos=0;
         }
-        
+
         //If we hit our special value or reached the max win size create a new block
         if((block->tail->length >= rabin_polynomial_min_block_size && (block->cur_roll_checksum % rabin_polynomial_average_block_size) == rabin_polynomial_prime)|| block->tail->length == rabin_polynomial_max_block_size) {
-            block->tail->start=block->total_bytes_read-block->tail->length;
-            block_tail_start = block->tail->start;
-            block_tail_length = block->tail->length;
-            partial_fill = 0; //This will change in a future PR
+            // JM >>>
+            partial_chunk_needed = 0; //bool false
+            block->tail->chunk_data = (char*) realloc(block->tail->chunk_data, sizeof(char)*(block->tail->length));
 
-            block->tail->chunk_data=malloc(sizeof(char)*(block->tail->length));
-            if(bytes_offset > block_tail_start){
-                J = 0;
-            }
-            else
+            int bytes_read_in_current_call = block->total_bytes_read - bytes_read_prev;
+            int start_buf_at = bytes_read_in_current_call - block->tail->length;
+            int copy_n_bytes = block->tail->length;
+            int start_chunk = 0;
+            while(start_buf_at < 0)
             {
-                J = block_tail_start - bytes_offset;
+                start_buf_at++;
+                copy_n_bytes--;
+                start_chunk++;
             }
 
-            memcpy(block->tail->chunk_data+partial_fill, (buf+J), block_tail_length);
+            memcpy(block->tail->chunk_data+start_chunk, buf+start_buf_at, copy_n_bytes);
+            // <<< JM
 
+            block->tail->start=block->total_bytes_read-block->tail->length;
             struct rabin_polynomial *new_poly=gen_new_polynomial(NULL,0,0,0);
             block->tail->next_polynomial=new_poly;
             block->tail=new_poly;
             
-            if(i==size-1){
+            if(i==size-1)
                 block->current_poly_finished=1;
-            }
         }
     }
+    // JM >>>
+    if(partial_chunk_needed)
+    {
+        block->tail->chunk_data=malloc(sizeof(char)*block->tail->length);
+        memcpy(block->tail->chunk_data, buf+size-block->tail->length, block->tail->length);
+    }
+    // <<< JM
     
     return block;
-    
 }
-
