18cad28776a2414f0d60c41f43762046f35c207c
Beelphegor <juanjoseguevara91@gmail.com>
2023-04-28 13:58:55 +0200

    removes grafana dashboard generation from monorepo

--- monitoring/dashboards.tf
+++
@@ -1,7 +0,0 @@
-resource "grafana_dashboard" "hardwaremonitoring" {
-   provider = grafana.base
-
-   for_each    = fileset("${path.module}/dashboards", "*.json")
-   config_json = file("${path.module}/dashboards/${each.key}")
-   folder      = grafana_folder.HardwareMonitoring.id
-}
--- monitoring/folders.tf
+++
@@ -1,6 +0,0 @@
-resource "grafana_folder" "HardwareMonitoring" {
-   provider = grafana.base
-
-   uid   = "Hq-KbkyVk"
-   title = "Hardware Monitoring"
-}
