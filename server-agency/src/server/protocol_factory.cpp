0fec1aaf5c3a1f496181a30bd5ccfa9f56159f94
Ankit Pandit <anktpndit@gmail.com>
2023-05-10 14:33:16 +0200

    for starting the workflows

--- server-agency/src/server/protocol_factory.cpp
+++ server-agency/src/server/protocol_factory.cpp
@@ -30,6 +30,7 @@ std::unique_ptr<uh::protocol::protocol> protocol_factory::create(std::shared_ptr
                     std::make_unique<uh::metrics::protocol_metrics_wrapper>
                             (m_protocol_metrics, std::make_unique<uh::an::server::protocol>
                                     (m_cluster, m_persistence.clientM_persistence(), m_client_metrics, m_serv_info)));
+
 }
 
 // ---------------------------------------------------------------------
