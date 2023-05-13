cbf85f5fa8be37aa7416c0ee3ae8ce0a8768f324
Benjamin-Elias Probst <benjamin@ultihash.io>
2023-05-13 21:10:42 +0200

    create new scope to close tmp file to finally write back compressed file result at compressed_file_store.cpp #75

--- server-db/src/storage/compressed_file_store.cpp
+++ server-db/src/storage/compressed_file_store.cpp
@@ -44,7 +44,7 @@ comp::type read_comp_type(io::device& in)
 
 std::unique_ptr<io::device> open_reader(const std::filesystem::path& path)
 {
-    auto in = std::make_unique<io::file>(path,"r");
+    auto in = std::make_unique<io::file>(path,"rb");
 
     comp::type type = read_comp_type(*in);
     in->reset_file_state();
@@ -71,25 +71,30 @@ void compression_worker::operator()(const std::filesystem::path& path, comp::typ
     std::streamsize savings = 0;
     try
     {
-        m_store.start(path);
-        auto in = open_reader(path);
-        io::count count_in(*in);
-
-        auto temp = io::temp_file(path.parent_path());
-        io::count count_out(temp);
-
-        write_comp_type(count_out, type);
-        temp.reset_file_state();
-        temp.seek(0,SEEK_END);
-
+        std::streamsize read_size,write_size;
         {
-            auto out = comp::create(count_out, type);
-            copy(count_in, *out);
+            m_store.start(path);
+            auto in = open_reader(path);
+            io::count count_in(*in);
+
+            auto temp = io::temp_file(path.parent_path(),"wb");
+            io::count count_out(temp);
+
+            write_comp_type(count_out, type);
+            temp.reset_file_state();
+            temp.seek(0,SEEK_END);
+
+            {
+                auto out = comp::create(count_out, type);
+                copy(count_in, *out);
+            }
+            read_size = count_in.read();
+            write_size = count_out.written();
         }
 
         temp.rename(path);
 
-        savings = count_in.read() - count_out.written();
+        savings = read_size - write_size;
     }
     catch (...)
     {
