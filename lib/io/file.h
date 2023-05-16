5db64239bd2f16d7e6645007630e9ba0d4bc6e2c
Benjamin-Elias Probst <benjamin@ultihash.io>
2023-05-16 15:59:17 +0200

    set standard behaviour of file to writing mode

--- lib/io/file.h
+++ lib/io/file.h
@@ -21,7 +21,7 @@ public:
      *
      * @param path input path to work on
      */
-    explicit file(const std::filesystem::path& path, std::ios_base::openmode mode = std::ios_base::in);
+    explicit file(const std::filesystem::path& path, std::ios_base::openmode mode = std::ios_base::out);
 
     std::streamsize write(std::span<const char> buffer) override;
     std::streamsize read(std::span<char> buffer) override;
