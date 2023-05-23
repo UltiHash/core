4257b830f8115a9bc580d221fc5198d49f392f6f
Stefan Jank <stefan@ultihash.io>
2023-05-23 09:14:33 +0200

    Add io::group_generator.

--- lib/io/buffer.h
+++ lib/io/buffer.h
@@ -20,6 +20,8 @@ public:
     std::streamsize read(std::span<char> buffer) override;
 
     bool valid() const override;
+
+    const std::vector<char>& data() const;
 private:
     std::vector<char> m_buffer;
     std::size_t m_rptr;
