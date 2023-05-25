c94ee9c4978ac29b15b417a2d6ec384ae4f72cb8
Benjamin-Elias Probst <benjamin@ultihash.io>
2023-05-25 18:43:00 +0200

    reset device test

--- lib/io/test/test_device.cpp
+++ lib/io/test/test_device.cpp
@@ -11,7 +11,6 @@
 #include <io/buffer.h>
 #include <io/buffered_device.h>
 #include <io/sstream_device.h>
-#include <io/fragment_on_device.h>
 #include <io/test_generator.h>
 
 
@@ -24,16 +23,15 @@ namespace
 
 // ---------------------------------------------------------------------
 
-typedef boost::mpl::vector<
-    sstream_device,
-    buffered_device<sstream_device>,
-    buffer,
-    fragment_on_device
-> device_types;
+    typedef boost::mpl::vector<
+            sstream_device,
+            buffered_device<sstream_device>,
+            buffer
+    > device_types;
 
 // ---------------------------------------------------------------------
 
-struct Fixture {};
+    struct Fixture {};
 
 // ---------------------------------------------------------------------
 
@@ -41,118 +39,104 @@ struct Fixture {};
  * To be implemented for each type in `device_types`: constructs a device
  * that will read the text given in LOREM_IPSUM.
  */
-template <typename T>
-std::unique_ptr<T> make_test_device();
+    template <typename T>
+    std::unique_ptr<T> make_test_device();
 
 // ---------------------------------------------------------------------
 
-template <>
-std::unique_ptr<sstream_device> make_test_device<sstream_device>()
-{
-    return std::make_unique<sstream_device>(LOREM_IPSUM);
-}
-
-// ---------------------------------------------------------------------
-
-template <>
-std::unique_ptr<buffered_device<sstream_device>> make_test_device<buffered_device<sstream_device>>()
-{
-    static std::unique_ptr<sstream_device> base;
-    base = make_test_device<sstream_device>();
-
-    return std::make_unique<buffered_device<sstream_device>>(*base);
-}
-
-// ---------------------------------------------------------------------
-
-template <>
-std::unique_ptr<buffer> make_test_device()
-{
-    auto rv = std::make_unique<buffer>();
-    rv->write(LOREM_IPSUM);
-    return rv;
-}
-
-// ---------------------------------------------------------------------
-
-template <>
-std::unique_ptr<fragment_on_device> make_test_device()
-{
-    static std::unique_ptr<buffer> buf;
-    buf = std::make_unique<buffer>();
-
-    auto rv = std::make_unique<fragment_on_device>(*buf);
-    rv->write(TEST_TEXT);
-
-    return rv;
-}
-
-// ---------------------------------------------------------------------
-
-BOOST_FIXTURE_TEST_CASE_TEMPLATE( valid_default, T, device_types, Fixture )
-{
-    auto dev = make_test_device<T>();
-
-    BOOST_CHECK(dev->valid());
-}
-
-// ---------------------------------------------------------------------
-
-BOOST_FIXTURE_TEST_CASE_TEMPLATE( read_full, T, device_types, Fixture )
-{
-    auto dev = make_test_device<T>();
-
-    std::vector<char> buffer(LOREM_IPSUM.size());
-
+    template <>
+    std::unique_ptr<sstream_device> make_test_device<sstream_device>()
     {
-        auto count = dev->read(buffer);
+        return std::make_unique<sstream_device>(LOREM_IPSUM);
+    }
+
+// ---------------------------------------------------------------------
+
+    template <>
+    std::unique_ptr<buffered_device<sstream_device>> make_test_device<buffered_device<sstream_device>>()
+    {
+        static std::unique_ptr<sstream_device> base;
+        base = make_test_device<sstream_device>();
+
+        return std::make_unique<buffered_device<sstream_device>>(*base);
+    }
+
+// ---------------------------------------------------------------------
+
+    template <>
+    std::unique_ptr<buffer> make_test_device()
+    {
+        auto rv = std::make_unique<buffer>();
+        rv->write(LOREM_IPSUM);
+        return rv;
+    }
+
+// ---------------------------------------------------------------------
+
+    BOOST_FIXTURE_TEST_CASE_TEMPLATE( valid_default, T, device_types, Fixture )
+    {
+        auto dev = make_test_device<T>();
+
+        BOOST_CHECK(dev->valid());
+    }
+
+// ---------------------------------------------------------------------
+
+    BOOST_FIXTURE_TEST_CASE_TEMPLATE( read_full, T, device_types, Fixture )
+    {
+        auto dev = make_test_device<T>();
+
+        std::vector<char> buffer(LOREM_IPSUM.size());
+
+        {
+            auto count = dev->read(buffer);
+            BOOST_CHECK_EQUAL(count, LOREM_IPSUM.size());
+            BOOST_CHECK_EQUAL(LOREM_IPSUM, std::string(&buffer[0], count));
+        }
+
+        {
+            auto count = dev->read(buffer);
+
+            BOOST_CHECK_EQUAL(count, 0u);
+        }
+
+        BOOST_CHECK(!dev->valid());
+    }
+
+// ---------------------------------------------------------------------
+
+    BOOST_FIXTURE_TEST_CASE_TEMPLATE( read_partial, T, device_types, Fixture )
+    {
+        auto dev = make_test_device<T>();
+
+        std::vector<char> buffer(8);
+        std::string complete;
+        std::size_t total = 0;
+
+        while (dev->valid())
+        {
+            auto count = dev->read(buffer);
+            total += count;
+            complete += std::string(&buffer[0], count);
+        }
+
+        BOOST_CHECK_EQUAL(total, LOREM_IPSUM.size());
+        BOOST_CHECK_EQUAL(LOREM_IPSUM, complete);
+        BOOST_CHECK(!dev->valid());
+    }
+
+// ---------------------------------------------------------------------
+
+    BOOST_FIXTURE_TEST_CASE_TEMPLATE( data_generator_api, T, device_types, Fixture )
+    {
+        std::vector v(LOREM_IPSUM.begin(), LOREM_IPSUM.end());
+        io::test::generator gen(v);
+
+        auto dev = make_test_device<T>();
+
+        auto count = dev->write_range(gen);
         BOOST_CHECK_EQUAL(count, LOREM_IPSUM.size());
-        BOOST_CHECK_EQUAL(LOREM_IPSUM, std::string(&buffer[0], count));
     }
-
-    {
-        auto count = dev->read(buffer);
-
-        BOOST_CHECK_EQUAL(count, 0u);
-    }
-
-    BOOST_CHECK(!dev->valid());
-}
-
-// ---------------------------------------------------------------------
-
-BOOST_FIXTURE_TEST_CASE_TEMPLATE( read_partial, T, device_types, Fixture )
-{
-    auto dev = make_test_device<T>();
-
-    std::vector<char> buffer(8);
-    std::string complete;
-    std::size_t total = 0;
-
-    while (dev->valid())
-    {
-        auto count = dev->read(buffer);
-        total += count;
-        complete += std::string(&buffer[0], count);
-    }
-
-    BOOST_CHECK_EQUAL(total, LOREM_IPSUM.size());
-    BOOST_CHECK_EQUAL(LOREM_IPSUM, complete);
-    BOOST_CHECK(!dev->valid());
-}
-
-// ---------------------------------------------------------------------
-
-BOOST_FIXTURE_TEST_CASE_TEMPLATE( data_generator_api, T, device_types, Fixture )
-{
-    std::vector v(LOREM_IPSUM.begin(), LOREM_IPSUM.end());
-    io::test::generator gen(v);
-
-    auto dev = make_test_device<T>();
-
-    auto count = dev->write_range(gen);
-    BOOST_CHECK_EQUAL(count, LOREM_IPSUM.size());
-}
 
 // ---------------------------------------------------------------------
 
