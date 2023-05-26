a82220b341c177801d25d84ca004ffb9080dae2d
Max Plauth <max@ultihash.io>
2023-05-26 15:54:03 +0200

    add lib/fdb for interfacing with FoundationDB

---
+++ lib/fdb/fdb.h
@@ -0,0 +1,166 @@
+//
+// Created by max on 26.05.23.
+// inspired by https://github.com/FreeYourSoul/free_fdb
+//
+
+#ifndef CORE_FDB_H
+#define CORE_FDB_H
+//#include <fmt/format.h>
+
+#include <util/exception.h>
+#include <logging/logging_boost.h>
+
+#include <span>
+#include <optional>
+
+#define FDB_API_VERSION 630
+#include <foundationdb/fdb_c.h>
+
+namespace uh::fdb {
+
+    DEFINE_EXCEPTION(fdb_exception);
+    DEFINE_EXCEPTION(fdb_transaction_exception);
+
+    static void check_fdb_code(fdb_error_t error) {
+        if (error != 0) {
+            if (fdb_error_predicate(FDBErrorPredicate::FDB_ERROR_PREDICATE_RETRYABLE, error)) {
+                THROW(fdb_transaction_exception, "Future, Non retry-able error : " + std::string(fdb_get_error(error)));
+            }
+            THROW(fdb_exception, "Future, Other error : " + std::string(fdb_get_error(error)));
+        }
+    }
+
+    struct fdb_result {
+        std::vector<char> key;
+        std::vector<char> value;
+    };
+
+    class fdb_future {
+    public:
+        ~fdb_future() {
+            if (_data) {
+                fdb_future_destroy(_data);
+            }
+        }
+        fdb_future(const fdb_future &) = delete;
+
+        explicit fdb_future(FDBFuture *fut) : _data(fut) {
+        }
+
+        template<typename Handler>
+        auto get(Handler &&handler) {
+            if (!_data) {
+                THROW(fdb_exception, "Error: Future data is null and thus cant be awaited.");
+            }
+            if (auto error = fdb_future_block_until_ready(_data); error != 0) {
+                THROW(fdb_exception, "Error on future block : " + std::string(fdb_get_error(error)));
+            }
+            check_fdb_code(fdb_future_get_error(_data));
+            return std::forward<Handler>(handler)(_data);
+        }
+
+        void get() {
+            get([](FDB_future *) { return std::optional<fdb_result>{}; });
+        }
+
+    private:
+        FDBFuture *_data;
+    };
+
+/**
+ * @brief RAII object encapsulating a FDBTransaction
+ * If not committed, transaction is rolled back at destruction time.
+ *
+ * @see https://apple.github.io/foundationdb/api-c.html#transaction
+ */
+    class fdb_transaction {
+
+    public:
+        ~fdb_transaction();
+        fdb_transaction(const fdb_transaction &) = delete;
+        explicit fdb_transaction(FDBDatabase *db);
+
+        /**
+         * @brief Enable snapshot
+         * @see https://apple.github.io/foundationdb/api-c.html#snapshot-reads
+         */
+        void enable_snapshot();
+
+        /**
+         * @brief Commit the current transaction
+         *
+         * @see https://apple.github.io/foundationdb/api-c.html#c.fdb_transaction_commit
+         */
+        void commit();
+
+        /**
+         * @brief Reset the current transaction to its original state
+         *
+         * @see https://apple.github.io/foundationdb/api-c.html#c.fdb_transaction_reset
+         */
+        void reset();
+
+        /**
+         * @brief Insert a new key / value pair in foundationdb
+         *
+         * @param key to insert
+         * @param value to insert
+         */
+        void put(const std::span<char> &key, const std::span<char> &value);
+
+        /**
+         * @brief Remove a selected key from the DB
+         * @param key to delete in foundationdb
+         */
+        void del(const std::span<char> &key);
+
+        /**
+         * @warning this method is for internal purpose only and should not be used in order to improvise C API calls
+         * @return the raw C API foundationdb transaction encapsulated in the current transaction
+         */
+        [[nodiscard]] FDBTransaction *raw() const;
+
+        /**
+         * @brief Get the key value at the specified key in foundationdb
+         *
+         * @param key to retrieve from the database
+         * @return a key value structure if present, std::nullopt otherwise
+         */
+        std::optional<fdb_result> get(const std::span<char> &key);
+
+    private:
+        FDBTransaction *_trans = nullptr;
+        bool _snapshot_enabled = false;
+    };
+
+/**
+ * @brief RAII Object representing an instance of the foundationdb,
+ * At construction time a thread is launched in order to handle the fdb network.
+ * At the first initialization of the foundationdb (ensured by a std::once_flag semaphore) network is setup
+ *
+ * Encapsulate a FDBDatabase pointer, connection to the database and network thread is closed when the object
+ * is destructed
+ *
+ * @see https://apple.github.io/foundationdb/api-c.html#database
+ * @see https://apple.github.io/foundationdb/api-c.html#network
+ */
+    class fdb {
+        struct internal;
+
+    public:
+        ~fdb();
+        explicit fdb(const std::string &cluster_file_path);
+
+        /**
+         * @return a pointer on a newly created transaction raii object
+         */
+        [[nodiscard]] std::unique_ptr<fdb_transaction> make_transaction();
+
+
+    private:
+        std::unique_ptr<internal> _impl;
+    };
+
+}// namespace uh::fdb
+
+#endif //CORE_FDB_H
---
+++ lib/fdb/test/testFdb.cpp
@@ -0,0 +1,162 @@
+//
+// Created by max on 02.11.22.
+//
+
+#ifdef SINGLE_TEST_RUNNER
+#define BOOST_TEST_NO_MAIN
+#else
+#define BOOST_TEST_MODULE "uhLibFdb Tests"
+#endif
+
+#include <boost/test/unit_test.hpp>
+#include <thread>
+#include "fdb/fdb.h"
+
+#define CHAR_TO_SPAN(variable, stringLiteral) \
+    constexpr char* variable = stringLiteral; \
+    constexpr const std::span<char> variable##Span = {variable, std::strlen(variable)};
+
+CHAR_TO_SPAN(key1, "key_1");
+CHAR_TO_SPAN(key2, "key_2");
+CHAR_TO_SPAN(key3, "key_3");
+CHAR_TO_SPAN(key4, "key_4");
+CHAR_TO_SPAN(val1, "value_1");
+CHAR_TO_SPAN(val2, "value_2");
+CHAR_TO_SPAN(val3, "value_3");
+CHAR_TO_SPAN(val4, "value_4");
+CHAR_TO_SPAN(notFound, "key_42")
+
+static auto fdb = uh::fdb::fdb("/etc/foundationdb/fdb.cluster");
+
+bool areEqual(const std::vector<char>& vec, const char* cString) {
+    std::size_t cStringLength = std::strlen(cString);
+
+    if (vec.size() != cStringLength) {
+        return false;
+    }
+
+    return std::equal(vec.begin(), vec.end(), cString);
+}
+
+
+// ------------- Tests Follow --------------
+BOOST_AUTO_TEST_CASE( setup )
+{
+    auto trans = fdb.make_transaction();
+
+    trans->del(key1Span);
+    trans->del(key2Span);
+    trans->del(key3Span);
+    trans->del(key4Span);
+
+    auto kv1 = trans->get(key1Span);
+    auto kv2 = trans->get(key2Span);
+    auto kv3 = trans->get(key3Span);
+    auto kv4 = trans->get(key4Span);
+
+    BOOST_CHECK(!kv1.has_value());
+    BOOST_CHECK(!kv2.has_value());
+    BOOST_CHECK(!kv3.has_value());
+    BOOST_CHECK(!kv4.has_value());
+
+    trans->commit();
+}
+
+BOOST_AUTO_TEST_CASE( put_get_test )
+{
+    auto trans = fdb.make_transaction();
+    trans->put(key1Span, val1Span);
+    trans->put(key2Span, val2Span);
+    trans->put(key3Span, val3Span);
+    trans->put(key4Span, val4Span);
+
+    auto kv1 = trans->get(key1Span);
+    auto kv2 = trans->get(key2Span);
+    auto kv3 = trans->get(key3Span);
+    auto kv4 = trans->get(key4Span);
+
+    BOOST_CHECK(kv1.has_value());
+    BOOST_CHECK(kv2.has_value());
+    BOOST_CHECK(kv3.has_value());
+    BOOST_CHECK(kv4.has_value());
+
+    BOOST_CHECK(areEqual(kv1->key, "key_1"));
+    BOOST_CHECK(areEqual(kv1->value, "value_1"));
+
+    BOOST_CHECK(areEqual(kv2->key, "key_2"));
+    BOOST_CHECK(areEqual(kv2->value, "value_2"));
+
+    BOOST_CHECK(areEqual(kv3->key, "key_3"));
+    BOOST_CHECK(areEqual(kv3->value, "value_3"));
+
+    BOOST_CHECK(areEqual(kv4->key, "key_4"));
+    BOOST_CHECK(areEqual(kv4->value, "value_4"));
+
+    auto kv_not_found = trans->get(notFoundSpan);
+    BOOST_CHECK(!kv_not_found.has_value());
+}
+
+BOOST_AUTO_TEST_CASE( nothing_committed )
+{
+    auto trans = fdb.make_transaction();
+    auto kv_not_found = trans->get(key1Span);
+    BOOST_CHECK(!kv_not_found.has_value());
+}
+
+BOOST_AUTO_TEST_CASE( transaction_commit_test )
+{
+    auto trans = fdb.make_transaction();
+
+    trans->put(key1Span, val1Span);
+    trans->put(key2Span, val2Span);
+    trans->put(key3Span, val3Span);
+    trans->put(key4Span, val4Span);
+
+    {
+        auto trans2 = fdb.make_transaction();
+        auto kv1 = trans2->get(key1Span);
+        auto kv2 = trans2->get(key2Span);
+        auto kv3 = trans2->get(key3Span);
+        auto kv4 = trans2->get(key4Span);
+        BOOST_CHECK(!kv1.has_value());
+        BOOST_CHECK(!kv2.has_value());
+        BOOST_CHECK(!kv3.has_value());
+        BOOST_CHECK(!kv4.has_value());
+    }
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    trans->commit();
+
+    {
+        auto trans2 = fdb.make_transaction();
+        auto kv1 = trans2->get(key1Span);
+        auto kv2 = trans2->get(key2Span);
+        auto kv3 = trans2->get(key3Span);
+        auto kv4 = trans2->get(key4Span);
+        BOOST_CHECK(kv1.has_value());
+        BOOST_CHECK(kv2.has_value());
+        BOOST_CHECK(kv3.has_value());
+        BOOST_CHECK(kv4.has_value());
+    }
+}
+
+BOOST_AUTO_TEST_CASE( teardown )
+{
+    auto trans = fdb.make_transaction();
+
+    trans->del(key1Span);
+    trans->del(key2Span);
+    trans->del(key3Span);
+    trans->del(key4Span);
+
+    auto kv1 = trans->get(key1Span);
+    auto kv2 = trans->get(key2Span);
+    auto kv3 = trans->get(key3Span);
+    auto kv4 = trans->get(key4Span);
+
+    BOOST_CHECK(!kv1.has_value());
+    BOOST_CHECK(!kv2.has_value());
+    BOOST_CHECK(!kv3.has_value());
+    BOOST_CHECK(!kv4.has_value());
+
+    trans->commit();
+}
