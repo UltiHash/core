02d2192e53bf1f202437e1e298cc634d8fd25640
Benjamin-Elias Probst <benjamin@ultihash.io>
2023-05-27 03:36:26 +0200

    implemented fragment_on_seekable_reset_device.h

---
+++ lib/io/fragment_on_seekable_reset_device.cpp
@@ -0,0 +1,22 @@
+//
+// Created by benjamin-elias on 27.05.23.
+//
+
+#include "fragment_on_seekable_reset_device.h"
+
+namespace uh::io {
+
+// ---------------------------------------------------------------------
+
+    fragment_on_seekable_reset_device::fragment_on_seekable_reset_device(seekable_device &dev, uint8_t index,
+                                                                         std::streamoff starts_at):
+            start_pos(starts_at), fragment_on_seekable_device(dev,index){}
+
+// ---------------------------------------------------------------------
+
+    bool fragment_on_seekable_reset_device::reset() {
+        dev_seek.seek(start_pos,std::ios_base::beg);
+        return fragment_on_device::reset();
+    }
+
+} // namespace uh::io
---
+++ lib/io/fragment_on_seekable_reset_device.h
@@ -0,0 +1,37 @@
+//
+// Created by benjamin-elias on 27.05.23.
+//
+
+#ifndef CORE_FRAGMENT_ON_SEEKABLE_RESET_DEVICE_H
+#define CORE_FRAGMENT_ON_SEEKABLE_RESET_DEVICE_H
+
+#include "io/fragment_on_seekable_device.h"
+
+namespace uh::io {
+
+    class fragment_on_seekable_reset_device: public fragment_on_seekable_device{
+
+    public:
+
+        /**
+         * fragment on seekable reset device is a virtual device running on a seekable host device
+         *
+         * @param dev incoming seekable device to build this virtual device upon
+         * @param index position that is either given on write or read from underlying device
+         * @param starts_at fragment starting position to be seeked to
+         */
+        explicit fragment_on_seekable_reset_device(io::seekable_device& dev, uint8_t index = 0, std::streamoff starts_at = 0);
+
+        /**
+         * reset the state machine to the stored fragment beginning
+         * @return if the underlying device is still valid to deliver the fragment structure of the stored position
+         */
+        bool reset() override;
+
+    private:
+        std::streamoff start_pos;
+    };
+
+} // namespace uh::io
+
+#endif //CORE_FRAGMENT_ON_SEEKABLE_RESET_DEVICE_H
