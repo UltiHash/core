c896fa0cd38ce14b0f4c6f377904de34f506aa91
Benjamin-Elias Probst <benjamin@ultihash.io>
2023-05-28 21:47:29 +0200

    tree navigator interface

--- lib/io/tree_navigator.cpp
+++ lib/io/tree_navigator.cpp
@@ -10,6 +10,16 @@
 
 namespace uh::io {
 
+    tree_navigator::~tree_navigator() {
+        std::for_each(sub_trees.begin(),sub_trees.end(),[](auto& sub_tree_entry){
+            delete sub_tree_entry.first;
+        });
+
+        std::for_each(chunk_collections.begin(),chunk_collections.end(),[](auto& chunk_collection_entry){
+            delete chunk_collection_entry.first;
+        });
+    }
+
     tree_navigator::tree_navigator(const std::filesystem::path& root): root(root){
 
         for(const auto&file_object:std::filesystem::directory_iterator(root))
@@ -30,5 +40,62 @@ namespace uh::io {
             }
         }
     }
+
+    serialization::fragment_serialize_size_format
+    tree_navigator::write_indexed(std::span<const char> buffer, uint32_t alloc) {
+        return serialization::fragment_serialize_size_format();
+    }
+
+    std::pair<std::vector<char>, serialization::fragment_serialize_size_format>
+    tree_navigator::read_indexed(uint8_t at) {
+        return std::pair<std::vector<char>, serialization::fragment_serialize_size_format>();
+    }
+
+    std::vector<serialization::fragment_serialize_size_format>
+    tree_navigator::write_indexed_multi(const std::vector<std::span<const char>> &buffer) {
+        return std::vector<serialization::fragment_serialize_size_format>();
+    }
+
+    std::vector<std::pair<std::vector<char>, serialization::fragment_serialize_size_format>>
+    tree_navigator::read_indexed_multi(const std::vector<uint8_t> &at) {
+        return std::vector<std::pair<std::vector<char>, serialization::fragment_serialize_size_format>>();
+    }
+
+    void tree_navigator::remove(uint8_t at) {
+
+    }
+
+    uint16_t tree_navigator::count() {
+        return 0;
+    }
+
+    std::size_t tree_navigator::size() {
+        return 0;
+    }
+
+    std::size_t tree_navigator::level_size() {
+        return 0;
+    }
+
+    std::size_t tree_navigator::content_size() {
+        return 0;
+    }
+
+    std::size_t tree_navigator::content_level_size() {
+        return 0;
+    }
+
+    bool tree_navigator::full() const {
+        return false;
+    }
+
+    uint64_t tree_navigator::free() {
+        return 0;
+    }
+
+    std::filesystem::path tree_navigator::getRoot() {
+        return std::filesystem::path();
+    }
+
 
 } // namespace uh::io
--- lib/io/tree_navigator.h
+++ lib/io/tree_navigator.h
@@ -13,7 +13,106 @@ namespace uh::io {
 
     class tree_navigator {
 
+    public:
+
+        ~tree_navigator();
+
+        /**
+         * a tree navigator takes care of 256 chunk collections and 256 tree navigators. The main job
+         * is to balance storage location and size
+         *
+         * @param root is the root path of the tree
+         */
         explicit tree_navigator(const std::filesystem::path& root);
+
+        /**
+         * write with returning the index that was assigned to the written buffer
+         * or instead give an index
+         *
+         * @param buffer to be written
+         * @param alloc allocate space and write fragment to chunk collection with the help of multiple buffers
+         * WARNING: allocated space must be written completely (accumulated buffer size is longer or equal alloc)
+         * @return tuple<stream size, assigned index position>
+         */
+        serialization::fragment_serialize_size_format write_indexed
+                (std::span<const char> buffer,uint32_t alloc = 0);
+
+        /**
+         * read a certain index pointer and return a vector buffer of the content
+         *
+         * @param at index
+         * @return buffer
+         */
+        std::pair<std::vector<char>,serialization::fragment_serialize_size_format>
+        read_indexed(uint8_t at);
+
+        /**
+         * Write indexed multiple buffers and return a list of fragment size structs that contain written fragment
+         * information.
+         * Does not close filestream.
+         */
+        std::vector<serialization::fragment_serialize_size_format>
+        write_indexed_multi(const std::vector<std::span<const char>> &buffer);
+
+        /**
+         * read indexed multiple positions with smart seeking
+         */
+        std::vector<std::pair<std::vector<char>, serialization::fragment_serialize_size_format>>
+        read_indexed_multi(const std::vector<uint8_t>& at);
+
+        /**
+         *
+         * @param at remove fragment with this index
+         */
+        void remove(uint8_t at);
+
+        /**
+         *
+         * @return the count of addresses used
+         */
+        uint16_t count();
+
+        /**
+         *
+         * @return the accumulated size of the tree navigator with level size and all sub trees
+         */
+        std::size_t size();
+
+        /**
+         *
+         * @return the size of all chunk collections of the tree navigator level
+         */
+        std::size_t level_size();
+
+        /**
+         *
+         * @return the size of the content of the tree navigator from level size and all sub trees
+         */
+        std::size_t content_size();
+
+        /**
+         *
+         * @return the size of the content of the tree navigator from level size
+         */
+        std::size_t content_level_size();
+
+        /**
+         *
+         * @return if the tree navigator is full
+         */
+        [[nodiscard]] bool full() const;
+
+        /**
+         *
+         * @return tell how many addresses are still free
+         */
+        uint64_t free();
+
+        /**
+         *
+         * @return path of chunk collection
+         */
+        std::filesystem::path getRoot();
 
     private:
         std::vector<std::pair<tree_navigator*,uint8_t>> sub_trees;
