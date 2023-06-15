e9b481127930d75981fcebd54a20de1907f27472
Benjamin-Elias Probst <benjamin@ultihash.io>
2023-06-15 18:24:17 +0200

    licensing options, missing are options on licensing for each node and mod

---
+++ server-db/src/licensing/options.h
@@ -0,0 +1,30 @@
+//
+// Created by benjamin-elias on 15.06.23.
+//
+
+#ifndef CORE_OPTIONS_H
+#define CORE_OPTIONS_H
+
+#include <options/options.h>
+#include <filesystem>
+#include <options/licensing_options.h>
+
+namespace uh::dbn::licensing {
+
+
+// ---------------------------------------------------------------------
+
+    class options : public uh::options::licensing_options
+    {
+    public:
+        options() = default;
+
+        uh::options::action evaluate(const boost::program_options::variables_map& vars) override;
+
+    };
+
+// ---------------------------------------------------------------------
+
+} // licensing
+
+#endif //CORE_OPTIONS_H
