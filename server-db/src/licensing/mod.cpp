d2c5440f1b6901e285e90dcbda8a956aa8df73da
Benjamin-Elias Probst <benjamin@ultihash.io>
2023-06-16 17:35:42 +0200

    rebase from master

--- server-db/src/licensing/mod.cpp
+++ server-db/src/licensing/mod.cpp
@@ -22,7 +22,6 @@ namespace
 
 // ---------------------------------------------------------------------
 
-
 void maybe_create_license_root_directory(std::filesystem::path license_root)
 {
     //Check whether the directory already exists:
