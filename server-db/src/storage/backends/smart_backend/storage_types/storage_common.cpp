77def0e910fa4f0648800252a545102244ad075c
Masoud Gholami <masoud@ultihash.io>
2023-06-23 11:46:07 +0200

    merge

--- server-db/src/storage/backends/smart_backend/storage_types/storage_common.cpp
+++ server-db/src/storage/backends/smart_backend/storage_types/storage_common.cpp
@@ -19,6 +19,35 @@ void sync_ptr (void *ptr, std::size_t size) {
     }
 }
 
+
+offset_ptr::offset_ptr(size_t offset, void *addr) :
+        m_addr (static_cast <char*> (addr)), m_offset (offset) {}
+
+offset_ptr offset_ptr::get_offset_ptr_at(size_t offset) const {
+    if (m_addr == nullptr) {
+        throw std::logic_error ("error: nullptr in offset_ptr");
+    }
+    return {offset, (offset - m_offset) + static_cast <char*> (m_addr)};
+}
+
+offset_ptr offset_ptr::get_offset_ptr_at(void *raw_ptr) const {
+    if (m_addr == nullptr) {
+        throw std::logic_error ("error: nullptr in offset_ptr");
+    }
+    return {(static_cast <char*> (raw_ptr) - static_cast <char*> (m_addr)) + m_offset, raw_ptr};
+}
+
+resource_entry::resource_entry(void *addr, std::filesystem::path path, size_t size, size_t offset) :
+        m_path (std::move(path)),
+        m_ptr (offset, addr),
+        m_size (size),
+        m_monotonic_buffer(addr, size, std::pmr::null_memory_resource()),
+        m_pool_resource(&m_monotonic_buffer) {}
+
+std::pmr::memory_resource& resource_entry::get_pool_resource() {
+    return m_pool_resource;
+}
+
 void managed_storage::mmap_file(const std::filesystem::path &file, uint64_t offset, size_t file_size) {
 
     const auto fd = open(file.c_str(), O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
