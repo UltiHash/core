dce8f1af97286b79bb07f5cdf0110a11c7af56ea
Benjamin-Elias Probst <benjamin@ultihash.io>
2023-06-28 10:42:38 +0200

    set pwd for integration path to fulfill absolute path requirement

--- client-shell/start.sh
+++ client-shell/start.sh
@@ -40,7 +40,8 @@ rm -Rf test
 
 # retrieve test workload and validate their checksums
 mkdir test
-yes | uh-cli --retrieve test.uh --target ./test --agency-node ${SERVER_NAME}:${SERVER_PORT}
+curdir=$(pwd)
+yes | uh-cli --retrieve test.uh --target ${curdir}/test --agency-node ${SERVER_NAME}:${SERVER_PORT}
 cat checksum.txt | sha512sum -c
 
 # cleanup
