cdadba186702e61f22de68b63e2c58ec1cce32c9
Benjamin-Elias Probst <benjamin@ultihash.io>
2023-07-20 11:26:36 +0200

    kill data node after retrieval done

--- scripts/spreadsheet_helper.sh
+++ scripts/spreadsheet_helper.sh
@@ -9,33 +9,35 @@ echo "db root: ${third_arg}"
 
 start_db(){
 echo "Processing UltiHash project path = ${first_arg}, integration folder path = ${second_arg}"
-{
-    echo "Removing contents of ${third_arg}"
-    rm ${third_arg}/*
 
-    ${first_arg}/cmake-build-release/server-db/uh-data-node --threads 15 --port 12345 --data-directory ${third_arg}
-}||{
-    echo "Data node failed to start!"
-}
+echo "Removing contents of ${third_arg}"
+rm "${third_arg}/*"
+
+${first_arg}/cmake-build-release/server-db/uh-data-node --threads 15 --port 12345 --data-directory ${third_arg}
 }
 
 integrate_retrieve(){
 {
 sleep 10
 
+result_data="${second_arg}.result"
+
 echo "UHV target: ${second_arg}.uh"
 echo "Integrate from: ${second_arg}"
+echo "Result at: ${result_data}"
 
-${first_arg}/cmake-build-release/client-shell/uh-cli --integrate ${second_arg}.uh ${second_arg} --agency-node localhost:12345
+echo "Integrate:$(${first_arg}/cmake-build-release/client-shell/uh-cli --integrate ${second_arg}.uh ${second_arg} --agency-node localhost:12345)" > "${result_data}"
 }||{
 echo "Integration failed!"
 }
 
 {
-${first_arg}/cmake-build-release/client-shell/uh-cli --retrieve ${second_arg}.uh --target ${second_arg} --agency-node localhost:12345
+echo "Retrieve:$(${first_arg}/cmake-build-release/client-shell/uh-cli --retrieve ${second_arg}.uh --target ${second_arg} --agency-node localhost:12345)" >> "${result_data}"
 }||{
 echo "Retrieval failed!"
 }
+
+killall uh-data-node
 }
 
 export -f start_db && export -f integrate_retrieve
