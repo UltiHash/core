c48b507c69a7839260650506a9b3924abf86d57a
Masoud Gholami <masoud@ultihash.io>
2023-07-25 16:08:45 +0200

    separated source file for free spot manager

--- cluster/src/free_spot_manager.h
+++ cluster/src/free_spot_manager.h
@@ -17,151 +17,29 @@ namespace uh::cluster {
 class free_spot_manager {
 public:
 
-    explicit free_spot_manager (std::filesystem::path hole_log):
-        m_hole_log(std::move (hole_log)),
-        m_file (open_file()),
-        m_total_free_size (get_total_free_size ()){
+    explicit free_spot_manager (std::filesystem::path hole_log);
 
-        shift_forward ();
+    void push_free_spot (uint128_t pointer, std::size_t size);
 
-        m_file.exceptions(std::ios::badbit | std::ios::failbit);
-    }
+    std::optional <wide_span> pop_free_spot ();
 
-    void push_free_spot (uint128_t pointer, std::size_t size) {
+    void apply_popped_items ();
 
-        shift_forward ();
+    [[nodiscard]] uint128_t total_free_spots () const noexcept;
 
-        m_file.seekp(m_end_pos, std::ios::beg);
-        unsigned long free_spot_data[3];
-        free_spot_data[0] = pointer.get_data()[0];
-        free_spot_data[1] = pointer.get_data()[1];
-        free_spot_data[2] = size;
+    void sync ();
 
-        m_file.write (reinterpret_cast<const char *>(&free_spot_data), ELEMENT_SIZE);
-
-        m_end_pos += sizeof(free_spot_data);
-        m_total_free_size += size;
-    }
-
-    std::optional <wide_span> pop_free_spot () {
-        if (m_start_pos + m_pop_count * ELEMENT_SIZE == m_end_pos) {
-            return std::nullopt;
-        }
-
-        m_file.seekg (m_start_pos + m_pop_count * ELEMENT_SIZE, std::ios::beg);
-        unsigned long buffer [3];
-        read_size (m_file, ELEMENT_SIZE, reinterpret_cast <char *> (&buffer));
-        wide_span wspan {{buffer[0], buffer[1]}, buffer[2]};
-        m_pop_count ++;
-        m_popped_size += buffer[2];
-        return wspan;
-    }
-
-    void apply_popped_items () {
-        if (m_pop_count == 0) {
-            return;
-        }
-        m_file.seekg (m_start_pos, std::ios::beg);
-        const auto free_size = m_pop_count * ELEMENT_SIZE;
-        const auto zeros = std::unique_ptr <char []> (new char [free_size]);
-        std::memset(zeros.get(), 0, free_size);
-        m_file.write (zeros.get(), free_size);
-        m_start_pos += free_size;
-        m_pop_count = 0;
-        m_total_free_size = m_total_free_size - m_popped_size;
-        m_popped_size = 0;
-    }
-
-    uint128_t total_free_spots () {
-        return m_total_free_size;
-    }
-
-    void sync () {
-        m_file.sync();
-    }
-
-    ~free_spot_manager() {
-        sync();
-    }
+    ~free_spot_manager();
 
 private:
 
-    std::fstream open_file () {
-        if (std::filesystem::exists(m_hole_log)) {
-            std::fstream file {m_hole_log, std::ios::binary | std::ios::in | std::ios::out};
-            if (!file) {
-                throw std::filesystem::filesystem_error ("Could not open/create the log file in the data store root",
-                                                         std::error_code(errno, std::system_category()));
-            }
-            m_end_pos = static_cast <long> (std::filesystem::file_size(m_hole_log));
-            return file;
-        }
-        else {
-            if (!m_hole_log.parent_path().empty()) {
-                std::filesystem::create_directories(m_hole_log.parent_path());
-            }
+    std::fstream open_file ();
 
-            std::fstream file {m_hole_log, std::ios::binary | std::ios::in | std::ios::out | std::ios::trunc};
-            if (!file) {
-                throw std::filesystem::filesystem_error ("Could not open/create the log file in the data store root",
-                                                         std::error_code(errno, std::system_category()));
-            }
-            m_end_pos = 0;
-            return file;
-        }
-    }
+    uint128_t get_total_free_size ();
 
-    uint128_t get_total_free_size() {
-        ospan <char> buffer (m_end_pos);
-        m_file.seekg (0, std::ios::beg);
-        read_size (m_file, static_cast <long> (buffer.size), buffer.data.get());
-        uint128_t total_size = 0;
-        const auto* long_buffer = reinterpret_cast <unsigned long*> (buffer.data.get());
+    void shift_forward ();
 
-        for (std::size_t i = 2; i < buffer.size / sizeof (unsigned long); i += 3) {
-            total_size += long_buffer[i];
-        }
-        return total_size;
-    }
-
-    void shift_forward () {
-        if (m_end_pos == 0) {
-            return;
-        }
-        long offset = 0;
-        m_file.seekg (offset, std::ios::beg);
-        unsigned long free_spot_data[3];
-        do {
-            read_size(m_file, ELEMENT_SIZE, reinterpret_cast<char *>(free_spot_data));
-            offset += ELEMENT_SIZE;
-        } while (free_spot_data[2] == 0 and offset < m_end_pos);
-        offset -= ELEMENT_SIZE;
-
-        if (offset > 0 and free_spot_data[2] != 0) {
-
-            ospan <char> buffer (m_end_pos - offset);
-            std::memcpy (buffer.data.get(), &free_spot_data, ELEMENT_SIZE);
-            read_size(m_file, static_cast <long> (buffer.size - ELEMENT_SIZE), buffer.data.get() + ELEMENT_SIZE);
-            m_file.seekp(0, std::ios::beg);
-            m_end_pos = static_cast <long> (buffer.size);
-            m_file.write(buffer.data.get(), m_end_pos);
-        }
-        else if (offset > 0 and free_spot_data[2] == 0) {
-            m_end_pos = 0;
-        }
-        std::filesystem::resize_file(m_hole_log, m_end_pos);
-        m_start_pos = 0;
-
-    }
-
-
-    static void read_size (std::fstream& file, long size, char* buffer) {
-        long r = 0;
-        while (r < size) {
-            file.read (buffer + r, size - r);
-            r += file.gcount ();
-        }
-    }
+    static void read_size (std::fstream& file, long size, char* buffer);
 
     const std::filesystem::path m_hole_log;
     long m_end_pos {};
