1d3546cbd14904546477c6af22b99d01726ace10
Juan M. Carmona-Loaiza <juan@ultihash.io>
2023-09-12 15:54:08 +0200

    Juan modifs

--- cluster/src/entry_node/rest_server.cpp
+++ cluster/src/entry_node/rest_server.cpp
@@ -65,6 +65,9 @@ namespace uh::cluster
                 received_request.body_limit((std::numeric_limits<std::uint64_t>::max)());
                 co_await http::async_read(stream, buffer, received_request, net::use_awaitable);
 
+                // JUAN added
+                std::cout << received_request.get().base() << std::endl;
+
 
                 s3_parser s3_parser(received_request);
                 auto parsed_request = s3_parser.parse();
