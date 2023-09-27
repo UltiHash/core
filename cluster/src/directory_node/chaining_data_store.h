30a06f4c68c3c225b92990b87b97e658f7d3a5d2
Masoud Gholami <masoud@ultihash.io>
2023-09-27 18:39:40 +0200

    chaingin data store read operation

--- cluster/src/directory_node/chaining_data_store.h
+++ cluster/src/directory_node/chaining_data_store.h
@@ -27,6 +27,7 @@ struct chaining_data_store_config {
     size_t min_file_size;
     size_t max_file_size;
     size_t max_storage_size;
+    size_t max_chunk_size;
 };
 
 class chaining_data_store {
@@ -38,10 +39,13 @@ class chaining_data_store {
 
         constexpr static size_t small_size = sizeof (uint32_t);
         constexpr static size_t large_size = sizeof (uint32_t) + sizeof (uint64_t);
+        constexpr static size_t root_size = sizeof (uint32_t) + sizeof (uint64_t) + sizeof (size_t);
+
 
         uint32_t has_next: 1;
         uint32_t size: 31;
         uint64_t next_ptr;
+        size_t total_size = 0;
 
         [[nodiscard]] inline std::vector <uint32_t> serialize () const {
             std::vector <uint32_t> buf {1};
@@ -50,11 +54,15 @@ class chaining_data_store {
             if (has_next) [[unlikely]] {
                 buf.resize (3);
                 std::memcpy(buf.data() + 1, &next_ptr, sizeof(next_ptr));
+                if (total_size) {
+                    buf.resize (5);
+                    std::memcpy(buf.data() + 3, &total_size, sizeof(total_size));
+                }
             }
             return buf;
         }
 
-        inline void load_from_file (const int fd) {
+        inline void load_from_file (const int fd, bool root_header) {
             uint32_t buf;
             const auto r1 = ::read (fd, &buf, sizeof (buf));
             if (r1 != sizeof (buf)) [[unlikely]] {
@@ -66,6 +74,12 @@ class chaining_data_store {
                 const auto r2 = ::read (fd, &next_ptr, sizeof (next_ptr));
                 if (r2 != sizeof (next_ptr)) [[unlikely]] {
                     throw std::runtime_error ("Could not read the header from file");
+                }
+                if (root_header) {
+                    const auto r3 = ::read (fd, &total_size, sizeof (total_size));
+                    if (r3 != sizeof (total_size)) [[unlikely]] {
+                        throw std::runtime_error ("Could not read the header from file");
+                    }
                 }
             }
         }
@@ -129,16 +143,31 @@ public:
 
         unsigned long offset = 0;
         int index = 0;
+        size_t total_acquired_size = 0;
         for (const auto& partial_alloc: alloc) {
             if (lseek(partial_alloc.fd, partial_alloc.seek, SEEK_SET) != partial_alloc.seek) [[unlikely]] {
                 throw std::runtime_error ("Could not seek to the allocated offset");
             }
             header h;
             h.has_next = index < (alloc.size() - 1);
-            const auto header_size = (h.has_next) ? header::large_size: header::small_size;
+            size_t header_size = 0;
+            if (h.has_next) [[unlikely]] {
+                if (index == 0) {
+                    header_size = header::root_size;
+                }
+                else {
+                    header_size = header::large_size;
+                }
+            }
+            else {
+                header_size = header::small_size;
+            }
             h.size = partial_alloc.size - header_size;
             if (h.has_next) {
                 h.next_ptr = alloc [index + 1].global_offset;
+                if (index == 0) {
+                    h.total_size = data.size();
+                }
             }
             const auto header_buffer = h.serialize();
             const auto ws = ::write (partial_alloc.fd, header_buffer.data(), header_size);
@@ -149,16 +178,63 @@ public:
                  written < h.size;
                  written += ::write(partial_alloc.fd, data.data() + offset + written, h.size - written));
             offset += h.size;
+            total_acquired_size += partial_alloc.size;
             index ++;
         }
 
         m_free_spot_manager.apply_popped_items();
-        m_used += data.size();
+        m_used += total_acquired_size;
+
+        return alloc.begin()->global_offset;
 
     }
 
-    size_type read (char* buffer, index_type index) const {
-        
+    ospan <char> read (index_type index) const {
+        const auto [fd, seek] = get_file_offset_pair(index);
+        if (::lseek (fd, seek, SEEK_SET) != seek) [[unlikely]] {
+            throw std::runtime_error ("Could not seek to the read position.");
+        }
+
+        header h;
+        h.load_from_file (fd, true);
+        ospan <char> buf;
+        if (h.has_next) [[unlikely]] {
+            buf.resize(h.total_size);
+        }
+        else [[likely]] {
+            buf.resize(h.size);
+        }
+
+        index_type offset = 0;
+        size_t tr = 0;
+        do {
+            const auto r = ::read (fd, buf.data.get() + offset, h.size - tr);
+            if (r == 0) [[unlikely]] {
+                break;
+            }
+            tr += r;
+            offset += r;
+        } while (tr < h.size);
+
+        while (h.has_next) {
+            const auto [next_fd, next_seek] = get_file_offset_pair(h.next_ptr);
+            if (::lseek (next_fd, next_seek, SEEK_SET) != next_seek) [[unlikely]] {
+                throw std::runtime_error ("Could not seek to the read position.");
+            }
+            h.load_from_file(next_fd, false);
+
+            tr = 0;
+            do {
+                const auto r = ::read (fd, buf.data.get() + offset, h.size - tr);
+                if (r == 0) [[unlikely]] {
+                    break;
+                }
+                tr += r;
+                offset += r;
+            } while (tr < h.size);
+        }
+
+        return buf;
     }
 
     void remove (index_type index);
@@ -184,13 +260,14 @@ private:
 
     [[nodiscard]] std::pair <int, long> get_file_offset_pair (uint128_t pointer) const;
 
-    [[nodiscard]] size_type fetch_used_space () const noexcept; //
+    [[nodiscard]] size_type fetch_used_space () const noexcept;
 
     alloc_t allocate (std::size_t size) {
 
         alloc_t alloc;
 
         auto required_size = size + header::small_size;
+        size_t account_total_size = 1;
 
         auto free_spot = m_free_spot_manager.pop_free_spot();
         std::size_t partial_size = 0;
@@ -201,7 +278,8 @@ private:
 
             required_size -= partial_size;
             if (required_size > 0) {
-                required_size += header::large_size;
+                required_size += header::large_size + account_total_size * sizeof (size);
+                account_total_size = 0;
             }
 
             free_spot = m_free_spot_manager.pop_free_spot();
@@ -214,7 +292,7 @@ private:
 
         auto last_file_data_end = m_last_file_data_end;
         while (required_size > 0) {
-            partial_size = std::min (required_size, m_conf.max_file_size - last_file_data_end);
+            partial_size = std::min ({m_conf.max_chunk_size, required_size, m_conf.max_file_size - last_file_data_end});
 
             if (partial_size == 0) [[unlikely]] {
                 //TODO revert the new file if IO not successful
@@ -223,13 +301,14 @@ private:
                 add_new_file (m_conf.max_file_size * m_open_files.size(),
                               static_cast <long> (m_conf.min_file_size));
                 m_used += sizeof (last_file_data_end);
-                partial_size = std::min (required_size, m_conf.max_file_size);
+                partial_size = std::min ({m_conf.max_chunk_size, required_size, m_conf.max_file_size});
                 last_file_data_end = m_last_file_data_end;
             }
 
             required_size -= partial_size;
             if (required_size > 0) [[unlikely]] {
-                required_size += header::large_size;
+                required_size += header::large_size + account_total_size * sizeof (size);
+                account_total_size = 0;
             }
 
             if (last_file_data_end + partial_size <= m_last_file_size) [[likely]] {   // write in last file
@@ -261,13 +340,13 @@ private:
         return alloc;
     }
 
-    int add_new_file (const index_type& offset, long file_size); //
+    int add_new_file (const index_type& offset, long file_size);
 
-    [[nodiscard]] static std::pair <int, index_type> parse_file_name (const std::string& filename); //
+    [[nodiscard]] static std::pair <int, index_type> parse_file_name (const std::string& filename);
 
-    [[nodiscard]] std::string get_name (const index_type& offset) const; //
+    [[nodiscard]] std::string get_name (const index_type& offset) const;
 
-    static bool is_data_file (const std::filesystem::path& path); //
+    static bool is_data_file (const std::filesystem::path& path);
 
     int m_last_fd {};
     std::size_t m_last_file_data_end {};
