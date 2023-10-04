4e37599faee7abc7679beddef238302f775cddaf
Max Plauth <max@ultihash.io>
2023-10-04 13:46:01 +0200

    added locks to object access functions in bucket class

--- cluster/src/directory_node/bucket.h
+++ cluster/src/directory_node/bucket.h
@@ -6,15 +6,13 @@
 #define CORE_BUCKET_H
 
 #include "chaining_data_store.h"
-#include "object_store.h"
+#include "transaction_log.h"
 
 namespace uh::cluster{
 
 struct bucket_config {
     std::string bucket_id;
     std::filesystem::path root;
-    //std::filesystem::path directory;
-    //std::filesystem::path free_spot_log;
     size_t min_file_size;
     size_t max_file_size;
     size_t max_storage_size;
@@ -26,63 +24,62 @@ class bucket {
 
 
     bucket (bucket_config& conf, std::unordered_map < std::string, uint64_t >& object_ptrs):
-        m_data_store({
-            .directory = conf.root/"ds",
-            .free_spot_log = conf.root/"fsl",
+            m_data_store({
+            .directory = conf.root/"dr",
+            .free_spot_log = conf.root/"dr/free_spot_log",
             .min_file_size = conf.min_file_size,
             .max_file_size = conf.max_file_size,
             .max_storage_size = conf.max_storage_size,
             .max_chunk_size = conf.max_chunk_size,
         }),
-        m_log_file(conf.root/"log"),
-        m_object_ptrs(object_ptrs) {}
+            m_transaction_log(conf.root / "dr/transaction_log"),
+            m_object_ptrs(object_ptrs) {}
 
 
     void insert_object (const std::string& key, std::span<char> data) {
-        if (m_object_ptrs.contains(key)) [[unlikely]] {
-            throw
-                    std::runtime_error(
-                            "Attempt to insert object ' + key + ' failed: an object with the same name already exists.");
-        }
         std::unique_lock <std::shared_mutex> lock(m_mutex);
+        if (m_object_ptrs.contains(key)) [[unlikely]] {
+            throw std::runtime_error("Attempt to insert object ' + key + ' failed: an object with the same name already exists.");
+        }
         const auto index = m_data_store.post_write(data);
-        m_log_file.append(key, index, transaction_log::operation::INSERT_START);
+        m_transaction_log.append(key, index, transaction_log::operation::INSERT_START);
 
         //TODO: handle rollback in case something goes up in smoke during the transaction
         m_data_store.apply_write();
         m_object_ptrs[key] = index;
         lock.unlock();
 
-        m_log_file.append(key, index, transaction_log::operation::INSERT_END);
+        m_transaction_log.append(key, index, transaction_log::operation::INSERT_END);
     }
 
     ospan<char> get_obj(const std::string& key) {
+        std::shared_lock lock(m_mutex);
         if (!m_object_ptrs.contains(key)) [[unlikely]] {
-            throw
-                    std::runtime_error("Attempt to get object ' + key + ' failed: no such object.");
+            throw std::runtime_error("Attempt to get object ' + key + ' failed: no such object.");
         }
         const auto index = m_object_ptrs.at(key);
         return m_data_store.read(index);
     }
 
     void delete_object (const std::string& key) {
+        std::unique_lock <std::shared_mutex> lock(m_mutex);
         if (!m_object_ptrs.contains(key)) [[unlikely]] {
-            throw
-                    std::runtime_error("Attempt to remove object ' + key + ' failed: no such object.");
+            throw std::runtime_error("Attempt to remove object ' + key + ' failed: no such object.");
         }
         const auto index = m_object_ptrs.at(key);
-        m_log_file.append(key, index, transaction_log::operation::REMOVE_START);
+        m_transaction_log.append(key, index, transaction_log::operation::REMOVE_START);
 
         m_object_ptrs.erase(key);
         m_data_store.remove(index);
+        lock.unlock();
 
-        m_log_file.append(key, index, transaction_log::operation::REMOVE_END);
+        m_transaction_log.append(key, index, transaction_log::operation::REMOVE_END);
     }
 
     void update_object (const std::string &key, std::span<char> data) {
         std::unique_lock <std::shared_mutex> lock(m_mutex);
         const auto index = m_data_store.post_write(data);
-        m_log_file.append(key, index, transaction_log::operation::UPDATE_START);
+        m_transaction_log.append(key, index, transaction_log::operation::UPDATE_START);
 
         //TODO: handle rollback in case something goes up in smoke during the transaction
         m_data_store.apply_write();
@@ -91,7 +88,7 @@ class bucket {
         m_object_ptrs[key] = index;
         lock.unlock();
 
-        m_log_file.append(key, index, transaction_log::operation::UPDATE_END);
+        m_transaction_log.append(key, index, transaction_log::operation::UPDATE_END);
     }
 
     bool contains_object (const std::string &key) {
@@ -100,34 +97,17 @@ class bucket {
 
 
 private:
+    std::string m_bucket_id;
+    std::filesystem::path m_root;
     chaining_data_store m_data_store;
-    transaction_log m_log_file;
+    transaction_log m_transaction_log;
     std::unordered_map <std::string, uint64_t> m_object_ptrs;
     std::shared_mutex m_mutex;
 
-    std::string m_bucket_id;
-    std::filesystem::path m_root;
 
-    // acl
-    // log file bucket_name
+
 };
 
 }
 
 #endif //CORE_BUCKET_H
-
-
-
-/*+
- *
- *
- *
- * bucket.object_store.put...
- * get
- * bucket.put_object()
- * bucket.get_object()
- * bucket.set_acl()
- *
- *
- *
- */
