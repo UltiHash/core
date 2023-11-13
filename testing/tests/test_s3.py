633c94b949eb1e510efafe5580b1e4a3b11907c9
sjulti <118182787+sjulti@users.noreply.github.com>
2023-11-13 13:29:10 +0100

    Move testing code to python library. (#207)

--- testing/tests/test_s3.py
+++ testing/tests/test_s3.py
@@ -6,60 +6,8 @@
 import pytest
 import string
 import util
+from s3_util import has_bucket, has_object, unused_bucket_name, unused_object_key
 
-# ########################################################################
-#
-# Supporting functions related to the test domain (SUPPORT)
-#
-# ########################################################################
-
-#
-# Valid characters in bucket names according to
-# https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-#
-BUCKET_NAME_CHARACTERS=string.ascii_lowercase + string.digits
-
-#
-# Valid characters in object keys according to
-# https://docs.aws.amazon.com/AmazonS3/latest/userguide/object-keys.html
-#
-OBJECT_NAME_CHARACTERS=string.ascii_letters + string.digits + "!-_.*'()"
-
-
-def has_bucket(s3, bucket):
-    buckets = s3.list_buckets()
-    for b in buckets['Buckets']:
-        if b['Name'] == bucket:
-            return True
-
-    return False
-
-def has_object(s3, bucket, key):
-    objs = s3.list_objects_v2(Bucket=bucket)
-
-    for o in objs['Contents']:
-        if o['Key'] == key:
-            return True
-
-    return False
-
-def unused_bucket_name(s3, length=8):
-    while True:
-        name = util.get_random_name(BUCKET_NAME_CHARACTERS, length)
-        if not has_bucket(s3, name):
-            return name
-
-def unused_object_key(s3, bucket, length=8):
-    while True:
-        name = util.get_random_name(OBJECT_NAME_CHARACTERS, length)
-        if not has_object(s3, bucket, name):
-            return name
-
-# ########################################################################
-#
-# Test cases follow (TEST_CASES)
-#
-# ########################################################################
 
 def test_create_bucket(s3):
     name = unused_bucket_name(s3)
@@ -104,7 +52,7 @@ def test_object_content(s3):
     s3.create_bucket(Bucket=bucket)
 
     name = unused_object_key(s3, bucket)
-    body = util.get_random_name(string.printable, 64 * 1024)
+    body = util.random_string(string.printable, 64 * 1024)
     s3.put_object(Bucket=bucket, Key=name, body=body)
 
     resp = s3.get_object(Bucket=bucket, Key=name)
