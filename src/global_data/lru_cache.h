dd75ea227b68583d5f296d50449699a1ad91efe3
Gongotar <masood.gholami@aol.com>
2023-11-15 17:55:23 +0100

    Performance (#209)
    * performance improvement
    
    * larget common prefix
    
    * fixed test configs
    
    * cleanup
    
    ---------
    
    Co-authored-by: Massi <massi@massi.ultihash.io>

---
+++ src/global_data/lru_cache.h
@@ -0,0 +1,66 @@
+//
+// Created by masi on 11/8/23.
+//
+
+#ifndef UH_CLUSTER_LRU_CACHE_H
+#define UH_CLUSTER_LRU_CACHE_H
+
+#include <unordered_map>
+#include <optional>
+#include <list>
+#include <map>
+#include <memory>
+
+namespace uh::cluster {
+
+template <typename K, typename V>
+class lru_cache {
+
+    struct Node {
+        K key;
+        V value;
+    };
+
+    std::unordered_map<K, typename std::list<Node>::iterator> m_map;
+    std::list<Node> m_lruList;
+    int m_capacity;
+
+public:
+    explicit lru_cache(int capacity): m_capacity {capacity} {
+    }
+
+    void put (const K& key, V&& value) {
+
+        if (const auto f = m_map.find(key) ; f != m_map.cend()) {
+            f->second->value = std::forward <V> (value);
+            m_lruList.splice(m_lruList.cend(), m_lruList, f->second);
+        } else {
+            if (m_map.size() >= m_capacity) {
+                m_map.erase(m_lruList.front().key);
+                m_lruList.pop_front();
+            }
+            m_lruList.emplace_back (key, std::forward <V> (value));
+            m_map.emplace_hint(f, key, std::prev (m_lruList.end()));
+        }
+    }
+
+    std::optional <std::reference_wrapper <const V>> get (const K& key) {
+        if (const auto f = m_map.find(key); f != m_map.cend()) {
+            m_lruList.splice(m_lruList.cend(), m_lruList, f->second);
+            return f->second->value;
+        }
+        return std::nullopt;
+    }
+
+
+    void print () {
+        for (const auto n: m_lruList) {
+            std::cout << "key " << n.key << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+};
+} // end namespace uh::cluster
+
+#endif //UH_CLUSTER_LRU_CACHE_H
