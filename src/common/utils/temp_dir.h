984bc11481e8ff893b1b76d6de078ed31a3668ad
sjulti <118182787+sjulti@users.noreply.github.com>
2024-02-05 13:37:58 +0100

    Eng 144 (#251)

---
+++ src/common/utils/temp_dir.h
@@ -0,0 +1,28 @@
+#ifndef UTIL_TEMP_DIR_H
+#define UTIL_TEMP_DIR_H
+
+#include <filesystem>
+
+
+namespace uh::cluster
+{
+
+// ---------------------------------------------------------------------
+
+class temp_directory
+{
+public:
+    temp_directory();
+    ~temp_directory();
+
+    const std::filesystem::path& path() const;
+
+private:
+    std::filesystem::path m_path;
+};
+
+// ---------------------------------------------------------------------
+
+} // namespace uh::cluster
+
+#endif
