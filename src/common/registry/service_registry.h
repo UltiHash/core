6df3e1f1c5c180ebaddcd809c30bab893749c76a
Stefan Jank <sj@stiftstrasse.de>
2024-02-06 11:23:28 +0100

    modify service registry.

--- src/common/registry/service_registry.h
+++ src/common/registry/service_registry.h
@@ -19,11 +19,12 @@
 
 namespace uh::cluster {
 
+    template <role rl>
     class service_registry {
 
     public:
-        service_registry(uh::cluster::role role, std::size_t index, const std::string& etcd_host) :
-                m_service_name(to_string(role) + "/" + std::to_string(index)),
+        service_registry(std::size_t index, const std::string& etcd_host) :
+                m_service_name(to_string(rl) + "/" + std::to_string(index)),
                 m_etcd_client(etcd_host)
         {
         }
@@ -41,8 +42,9 @@ namespace uh::cluster {
                   m_keepalive(m_client, ttl, m_lease)
             {
                 etcdv3::Transaction txn;
-                for(const auto& pair : kv_pairs)
+                for (const auto& pair : kv_pairs) {
                     txn.add_success_put(pair.first, pair.second, m_lease);
+                }
                 m_client.txn(txn);
             }
 
@@ -51,24 +53,22 @@ namespace uh::cluster {
                 m_client.leaserevoke(m_lease);
             }
 
-            private:
+        private:
             etcd::SyncClient& m_client;
             int64_t m_lease;
             etcd::KeepAlive m_keepalive;
         };
 
         std::unique_ptr<registration> register_service(const server_config& config) {
-
             const std::string announced_key_base = etcd_services_announced_key_prefix + m_service_name;
-
             const std::string key_base = etcd_services_attributes_key_prefix + m_service_name + "/";
 
             const std::map<std::string, std::string> kv_pairs =
-                    {
-                        {key_base + get_config_string(uh::cluster::CFG_ENDPOINT_HOST), boost::asio::ip::host_name()},
-                        {key_base + get_config_string(uh::cluster::CFG_ENDPOINT_PORT),std::to_string(config.port)},
-                        {announced_key_base , {}},
-                    };
+                {
+                    {key_base + get_config_string(uh::cluster::CFG_ENDPOINT_HOST), boost::asio::ip::host_name()},
+                    {key_base + get_config_string(uh::cluster::CFG_ENDPOINT_PORT), std::to_string(config.port)},
+                    {announced_key_base, {}},
+                };
 
             return std::make_unique<registration>(
                 m_etcd_client,
@@ -77,10 +77,8 @@ namespace uh::cluster {
         }
 
     private:
-
         static constexpr std::size_t m_etcd_default_ttl = 10;
 
-        const std::string m_etcd_host;
         const std::string m_service_name;
 
         etcd::SyncClient m_etcd_client;
