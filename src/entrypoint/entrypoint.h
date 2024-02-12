ec81409b7b76189ee58d64c70f3109eca8a18030
Ankit Pandit <anktpndit@gmail.com>
2024-02-12 21:23:21 +0100

    changed to entry

--- src/entrypoint/entrypoint.h
+++ src/entrypoint/entrypoint.h
@@ -13,7 +13,7 @@
 #include "common/network/client.h"
 #include "entrypoint_handler.h"
 
-namespace uh::cluster {
+namespace uh::cluster::entry {
 
 class entrypoint: public service_interface {
 public:
@@ -28,7 +28,7 @@ public:
             m_workers (std::make_shared <boost::asio::thread_pool> (m_config.worker_thread_count)),
             m_server (m_config_registry.get_server_config(),
                       m_config_registry.get_service_name(),
-                      std::make_unique <entry::entrypoint_handler> (m_ioc, m_dedupe_services, m_directory_services, m_workers),
+                      std::make_unique <entrypoint_handler> (m_ioc, m_dedupe_services, m_directory_services, m_workers),
                     m_ioc)
     {
     }
