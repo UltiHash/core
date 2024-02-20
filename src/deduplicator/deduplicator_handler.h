89ced8333e9b159fe4ba0a010255367266a0ed1d
Massi <masoud@ultihash.io>
2024-02-20 16:37:23 +0100

    cache in read_address

--- src/deduplicator/deduplicator_handler.h
+++ src/deduplicator/deduplicator_handler.h
@@ -22,8 +22,7 @@ public:
           m_fragment_set(m_dedupe_conf.working_dir / "log", storage),
           m_storage(storage),
           m_dedupe_workers(std::move(dedupe_workers)) {
-        if (m_dedupe_conf.min_fragment_size >
-            m_storage.l1_cache_sample_size()) {
+        if (m_dedupe_conf.min_fragment_size > m_storage.cached_sample_size()) {
             throw std::invalid_argument("L1 cache sample size should not be "
                                         "smaller than the min fragment size!");
         }
@@ -127,7 +126,7 @@ private:
             auto common_prefix = largest_common_prefix(
                 integration_data, frag_data.get_str_view());
             if (common_prefix >= m_dedupe_conf.min_fragment_size) {
-                if (common_prefix == m_storage.l1_cache_sample_size() and l1) {
+                if (common_prefix == m_storage.cached_sample_size() and l1) {
                     frag_data = m_storage.read(frag.pointer, frag.size);
                     common_prefix += largest_common_prefix(
                         integration_data.substr(common_prefix),
