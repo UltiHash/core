bd8748c31f6443f82da25b8d5a99fb9e40907c63
Ankit Pandit <anktpndit@gmail.com>
2024-02-23 11:41:19 +0100

    refactored complete multipart's pugi with boost xml parser

--- src/entrypoint/rest/utils/parser/xml_parser.cpp
+++ src/entrypoint/rest/utils/parser/xml_parser.cpp
@@ -1,5 +1,30 @@
 #include "xml_parser.h"
 
+namespace uh::cluster {
+bool boost_xml_parser::parse(const std::string& body) {
+
+    bool flag;
+    try {
+        pt::read_xml(body, m_tree);
+        flag = true;
+    } catch (const std::exception& e) {
+        flag = false;
+    }
+
+    return flag;
+}
+
+std::vector<std::reference_wrapper<const pt::ptree>>
+boost_xml_parser::get_nodes(pt::ptree::path_type&& path) {
+    if (m_tree.empty())
+        return {};
+
+    std::vector<std::reference_wrapper<const pt::ptree>> paths;
+    enumerate(m_tree, path, std::back_inserter(paths));
+    return paths;
+}
+} // namespace uh::cluster
+
 namespace uh::cluster::rest::utils::parser {
 
 bool xml_parser::parse(const std::string& xml_string) {
--- src/entrypoint/rest/utils/parser/xml_parser.h
+++ src/entrypoint/rest/utils/parser/xml_parser.h
@@ -1,7 +1,49 @@
 #pragma once
 
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
 #include <pugixml.hpp>
 #include <span>
+
+namespace uh::cluster {
+namespace pt = boost::property_tree;
+
+class boost_xml_parser {
+public:
+    boost_xml_parser() = default;
+
+    bool parse(const std::string& body);
+
+    std::vector<std::reference_wrapper<const pt::ptree>>
+    get_nodes(pt::ptree::path_type&& path);
+
+private:
+    pt::ptree m_tree;
+
+    template <typename Tree, typename Out>
+    Out enumerate(const Tree& pt, Tree::path_type path, Out out) {
+        if (path.empty())
+            return out;
+
+        if (path.single()) {
+            auto name = path.reduce();
+            for (auto& child : pt) {
+                if (child.first == name)
+                    *out++ = child.second;
+            }
+        } else {
+            auto head = path.reduce();
+            for (auto& child : pt) {
+                if (head == "*" || child.first == head) {
+                    out = enumerate(child.second, path, out);
+                }
+            }
+        }
+
+        return out;
+    };
+};
+} // namespace uh::cluster
 
 namespace uh::cluster::rest::utils::parser {
 
