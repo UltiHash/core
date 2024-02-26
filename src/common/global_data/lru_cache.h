a966dfc51b639f406e278bae38e8763a257636c8
Max Plauth <max@ultihash.io>
2024-02-26 11:15:28 +0100

    added doxygen strings to lru cache

--- src/common/global_data/lru_cache.h
+++ src/common/global_data/lru_cache.h
@@ -19,14 +19,25 @@ template <typename K, typename V> class lru_cache {
     std::unordered_map<K, typename std::list<Node>::iterator> m_map;
     std::list<Node> m_lruList;
     size_t m_capacity;
-    std::mutex m;
+    std::mutex m_mutex;
 
 public:
     explicit lru_cache(size_t capacity)
         : m_capacity{capacity} {}
 
+    /**
+     * @brief Inserts a key-value pair into the cache.
+     *
+     * If the key already exists in the cache, updates its corresponding value.
+     * If the key does not exist:
+     *  - If the cache is full, removes the least recently used item.
+     *  - Inserts the new key-value pair into the cache.
+     *
+     * @param key The key to insert or update.
+     * @param value The value associated with the key.
+     */
     void put(const K& key, V&& value) {
-        std::lock_guard<std::mutex> lock(m);
+        std::lock_guard<std::mutex> lock(m_mutex);
         if (const auto f = m_map.find(key); f != m_map.cend()) {
             f->second->value = std::forward<V>(value);
             m_lruList.splice(m_lruList.cend(), m_lruList, f->second);
@@ -40,8 +51,18 @@ public:
         }
     }
 
+    /**
+     * @brief Retrieves the value associated with the given key.
+     *
+     * If the key exists in the cache, moves it to the most recently used
+     * position.
+     *
+     * @param key The key to search for in the cache.
+     * @return An optional reference to the value associated with the key if
+     * found; otherwise, std::nullopt.
+     */
     std::optional<std::reference_wrapper<const V>> get(const K& key) noexcept {
-        std::lock_guard<std::mutex> lock(m);
+        std::lock_guard<std::mutex> lock(m_mutex);
         if (const auto f = m_map.find(key); f != m_map.cend()) {
             m_lruList.splice(m_lruList.cend(), m_lruList, f->second);
             return f->second->value;
@@ -49,20 +70,25 @@ public:
         return std::nullopt;
     }
 
+    /**
+     * @brief Retrieves the value associated with the given key or a default
+     * value.
+     *
+     * If the key exists in the cache, moves it to the most recently used
+     * position. If the key does not exist, returns the provided default value.
+     *
+     * @param key The key to search for in the cache.
+     * @param default_value The default value to return if the key is not found.
+     * @return The value associated with the key if found; otherwise, the
+     * provided default value.
+     */
     V get(const K& key, V&& default_value) noexcept {
-        std::lock_guard<std::mutex> lock(m);
+        std::lock_guard<std::mutex> lock(m_mutex);
         if (const auto f = m_map.find(key); f != m_map.cend()) {
             m_lruList.splice(m_lruList.cend(), m_lruList, f->second);
             return f->second->value;
         }
         return default_value;
-    }
-
-    void print() {
-        for (const auto n : m_lruList) {
-            std::cout << "key " << n.key << std::endl;
-        }
-        std::cout << std::endl;
     }
 };
 } // end namespace uh::cluster
