bebdc753ad630a5e7a0e5b91a8b6f78e0a71d525
Stefan Jank <stefan@ultihash.io>
2024-03-01 16:20:43 +0100

    Allow using build cache for container builds.

--- testing/scripts/uh-build-container.sh
+++ testing/scripts/uh-build-container.sh
@@ -6,6 +6,34 @@
 
 . $UH_TEST_BASE/activate
 
+print_help()
+{
+    echo "uh-build-container.sh [options]"
+    echo
+    echo "Build a debug version of the service base container"
+    echo
+    echo "options are:"
+    echo " -h, --help           print this text"
+    echo " -c, --cached         allow caching build results"
+}
+
+cached="--no-cache"
+
+while [ -n "$1" ]; do
+
+    case "$1" in
+        -h|--help)  print_help
+                    exit 0
+                    ;;
+        -c|--cached)   cached=""
+                    ;;
+        *)  service="$1"
+            ;;
+    esac
+
+    shift
+done
+
 if ! docker pull $UH_IMAGE_BUILD_BASE; then
     echo "pulling build-base image failed" 1>&2
     exit 1
@@ -15,7 +43,7 @@ if docker image inspect $UH_IMAGE_TEST_TAG &> /dev/null; then
     docker image rm --force $UH_IMAGE_TEST_TAG
 fi
 
-if ! docker build --no-cache --build-arg DebugTools=True --build-arg BuildType=RelWithDebInfo --file $UH_IMAGE_DOCKER_FILE --tag $UH_IMAGE_TEST_TAG $UH_CORE_ROOT; then
+if ! docker build $cached --build-arg DebugTools=True --build-arg BuildType=RelWithDebInfo --file $UH_IMAGE_DOCKER_FILE --tag $UH_IMAGE_TEST_TAG $UH_CORE_ROOT; then
     echo "docker build failed" 1>&2
     exit 1
 fi
