93b61ac96ee47351e55bfaecdc0cc8995ef6c618
Massi <masoud@ultihash.io>
2024-03-04 14:42:17 +0100

    addressed comments

--- test/test_free_spot_manager.cpp
+++ test/test_free_spot_manager.cpp
@@ -34,9 +34,6 @@ struct fixture {
         return m_log_file;
     }
 
-    static void teardown() {
-    }
-
     std::array<uint128_t, 100> m_offsets;
     std::array<std::size_t, 100> m_sizes{};
 
@@ -53,16 +50,11 @@ BOOST_FIXTURE_TEST_CASE(push_free_spot_test, fixture) {
     free_spot_manager fsm (get_log_file ());
     std::size_t expected_total_free_size = 0;
 
-    int failure = 0;
     for (size_t i = 0; i < m_offsets.size(); ++i) {
         fsm.push_free_spot(m_offsets[i], m_sizes[i]);
         expected_total_free_size += m_sizes[i];
-
-        if (fsm.total_free_spots() != big_int{0, expected_total_free_size}) {
-            failure ++;
-        }
     }
-    BOOST_TEST(failure == 0);
+    BOOST_TEST(fsm.total_free_spots() == big_int{0, expected_total_free_size})
 }
 
 BOOST_FIXTURE_TEST_CASE(note_free_spot_test, fixture) {
@@ -74,12 +66,9 @@ BOOST_FIXTURE_TEST_CASE(note_free_spot_test, fixture) {
     for (size_t i = 0; i < m_offsets.size(); ++i) {
         fsm.note_free_spot(m_offsets[i], m_sizes[i]);
         expected_total_free_size += m_sizes[i];
-
-        if (fsm.total_free_spots() != big_int{0, 0}) {
-            failure ++;
-        }
     }
-    BOOST_TEST(failure == 0);
+
+    BOOST_TEST(fsm.total_free_spots() == big_int())
 
     fsm.push_noted_free_spots();
 
@@ -110,20 +99,20 @@ BOOST_FIXTURE_TEST_CASE(pop_free_spot_test, fixture) {
 }
 
 BOOST_FIXTURE_TEST_CASE(persistent_free_spot_test, fixture) {
-        std::size_t expected_total_free_size = 0;
+    std::size_t expected_total_free_size = 0;
 
-        {
-            free_spot_manager fsm (get_log_file ());
-            for (size_t i = 0; i < m_offsets.size(); ++i) {
-                fsm.push_free_spot(m_offsets[i], m_sizes[i]);
-                expected_total_free_size += m_sizes[i];
-            }
+    {
+        free_spot_manager fsm (get_log_file ());
+        for (size_t i = 0; i < m_offsets.size(); ++i) {
+            fsm.push_free_spot(m_offsets[i], m_sizes[i]);
+            expected_total_free_size += m_sizes[i];
         }
+    }
 
-        {
-            free_spot_manager fsm (get_log_file ());
-            BOOST_TEST((fsm.total_free_spots() == big_int{0, expected_total_free_size}));
-        }
+    {
+        free_spot_manager fsm (get_log_file ());
+        BOOST_TEST((fsm.total_free_spots() == big_int{0, expected_total_free_size}));
+    }
 }
 
 // ---------------------------------------------------------------------
