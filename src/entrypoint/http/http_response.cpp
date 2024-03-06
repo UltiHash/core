b891c9693652dd886e994e48ed069ffd8375e5ba
sjulti <118182787+sjulti@users.noreply.github.com>
2024-03-06 13:49:53 +0100

    Eng 262 - Multipart uploads (#323)
    Co-authored-by: Max Plauth <max@ultihash.io>

--- src/entrypoint/http/http_response.cpp
+++ src/entrypoint/http/http_response.cpp
@@ -35,6 +35,6 @@ void http_response::set_bandwidth(double bandwidth) {
     m_res.set("uh-bandwidth-mbps", std::to_string(bandwidth));
 }
 
-void http_response::set_etag(std::string etag) { m_etag = std::move(etag); }
+void http_response::set_etag(std::string etag) { m_res.set("ETag", etag); }
 
 } // namespace uh::cluster
