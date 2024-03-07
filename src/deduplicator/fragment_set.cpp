c9888828f3740975ce77f4452e4ea4c1c796769d
Max Plauth <max@ultihash.io>
2024-03-07 10:54:18 +0100

    avoid several concurrency issues

--- src/deduplicator/fragment_set.cpp
+++ src/deduplicator/fragment_set.cpp
@@ -4,15 +4,14 @@ namespace uh::cluster {
 fragment_set::fragment_set(const std::filesystem::path& set_log_path,
                            global_data_view& storage)
     : m_storage(storage),
-      m_set_log(set_log_path) {}
-
-void fragment_set::load() { m_set_log.replay(m_set, m_storage, m); }
+      m_set_log(set_log_path) {
+    m_set_log.replay(m_set, m_storage);
+}
 
 fragment_set::response fragment_set::find(std::string_view data) {
     fragment_set_element f{data, m_storage};
-    std::shared_lock<std::shared_mutex> lock(m);
+    std::shared_lock<std::shared_mutex> lock(m_mutex);
     const auto res = m_set.lower_bound(f);
-    lock.unlock();
 
     response resp{.hint = res};
     if (res != m_set.cend()) [[likely]] {
@@ -31,7 +30,7 @@ void fragment_set::insert(
     fragment_set_element f{data, pointer, m_storage};
     m_set_log.append(
         {set_operation::INSERT, f.get_pointer(), f.get_size(), f.get_prefix()});
-    std::lock_guard<std::shared_mutex> lock(m);
+    std::lock_guard<std::shared_mutex> lock(m_mutex);
     m_set.emplace_hint(hint, std::move(f));
 }
 } // namespace uh::cluster
