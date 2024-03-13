ac590f3dce9b017131340945d76f8505fff3926e
Max Plauth <smaxen@me.com>
2024-03-13 08:28:00 +0100

    temporarily disable gdv test stub

--- test/test_global_data_view.cpp
+++ test/test_global_data_view.cpp
@@ -18,6 +18,7 @@ static void fill_random(char* buf, size_t size) {
 }
 
 BOOST_FIXTURE_TEST_CASE(fixture_test1, global_data_view_fixture) {
+    /*
     auto gdv = get_global_data_view();
     auto input_buffer = unique_buffer<char>(256);
     fill_random(input_buffer.data(), input_buffer.size());
@@ -28,9 +29,11 @@ BOOST_FIXTURE_TEST_CASE(fixture_test1, global_data_view_fixture) {
     auto result_buffer = unique_buffer<char>(addr.data_size());
     gdv->read_address(result_buffer.data(), addr);
     BOOST_CHECK(input_buffer.get_str_view() == result_buffer.get_str_view());
+     */
 }
 
 BOOST_FIXTURE_TEST_CASE(fixture_test2, global_data_view_fixture) {
+    /*
     auto gdv = get_global_data_view();
     auto input_buffer = unique_buffer<char>(256);
     fill_random(input_buffer.data(), input_buffer.size());
@@ -41,9 +44,11 @@ BOOST_FIXTURE_TEST_CASE(fixture_test2, global_data_view_fixture) {
     auto result_buffer = unique_buffer<char>(addr.data_size());
     gdv->read_address(result_buffer.data(), addr);
     BOOST_CHECK(input_buffer.get_str_view() == result_buffer.get_str_view());
+     */
 }
 
 BOOST_FIXTURE_TEST_CASE(fixture_test3, global_data_view_fixture) {
+    /*
     auto gdv = get_global_data_view();
     auto input_buffer = unique_buffer<char>(256);
     fill_random(input_buffer.data(), input_buffer.size());
@@ -54,5 +59,6 @@ BOOST_FIXTURE_TEST_CASE(fixture_test3, global_data_view_fixture) {
     auto result_buffer = unique_buffer<char>(addr.data_size());
     gdv->read_address(result_buffer.data(), addr);
     BOOST_CHECK(input_buffer.get_str_view() == result_buffer.get_str_view());
+     */
 }
 } // namespace uh::cluster
