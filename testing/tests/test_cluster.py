37c362acca0d679f57ef362d8c0cf23290d247ab
Ankit Pandit <anktpndit@gmail.com>
2024-03-15 11:39:44 +0100

    multithreaded multiparts scrript done.

--- testing/tests/test_cluster.py
+++ testing/tests/test_cluster.py
@@ -13,18 +13,18 @@ def bucket(s3):
     s3.create_bucket(Bucket=name)
     yield name
 
-    response = s3.list_objects_v2(Bucket=name)
-    if 'Contents' in response:
-        objects = [{'Key': obj['Key']} for obj in response['Contents']]
-        print(objects)
-        s3.delete_objects(Bucket=name, Delete={'Objects': objects})
-
-    s3.delete_bucket(Bucket=name)
+    # response = s3.list_objects_v2(Bucket=name)
+    # if 'Contents' in response:
+    #     objects = [{'Key': obj['Key']} for obj in response['Contents']]
+    #     print(objects)
+    #     s3.delete_objects(Bucket=name, Delete={'Objects': objects})
+    #
+    # s3.delete_bucket(Bucket=name)
 
 def multi_chunk_upload(s3, bucket, file):
     key = unused_object_key(s3, bucket)
     s3.upload_file(file, Bucket=bucket, Key=key)
-    return key
+    return key, file
 
 def get_object(s3, bucket, key):
     resp = s3.get_object(Bucket=bucket, Key=key)
@@ -33,6 +33,14 @@ def get_object(s3, bucket, key):
 def read_file(path):
     with open(path, 'rb') as f:
         return f.read()
+
+def verify(s3, bucket, key, file):
+    received_file = get_object(s3, bucket, key)
+    if received_file == read_file(file):
+        assert True
+    else:
+        print("invalid contents retrieved")
+        assert False
 
 def worker_manager(workers, func, s3, files, bucket):
     with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
@@ -50,12 +58,15 @@ class worker_manager:
         self.task_queue = queue.Queue()
         self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=workers)
 
+    def __del__(self):
+        self.executor.shutdown()
+
     def post_task(self, task):
         self.task_queue.put(task)
 
     def run_tasks(self):
         futures = []
-        keys = []
+        values = []
 
         while not self.task_queue.empty():
             task = self.task_queue.get()
@@ -63,21 +74,18 @@ class worker_manager:
             futures.append(future)
 
         for future in concurrent.futures.as_completed(futures):
-            keys.append(future.result())
+            values.append(future.result())
 
-        self.executor.shutdown()
+        return values
 
-        return keys
-
-def test_basic_multipart_uploads(s3, files, bucket):
+def test_single_upload_onsingle_thread(s3, files, bucket):
     file = files[0]
     manager = worker_manager()
     manager.post_task(lambda: multi_chunk_upload(s3, bucket, file))
 
     keys = manager.run_tasks()
 
-    received_file = get_object(s3, bucket, keys[0])
-    print(len(received_file))
+    received_file = get_object(s3, bucket, keys[0][0])
 
     if received_file == read_file(file):
         s3.delete_bucket(Bucket=bucket)
@@ -87,15 +95,28 @@ def test_basic_multipart_uploads(s3, files, bucket):
         s3.delete_bucket(Bucket=bucket)
         assert False
 
-# def test_multithreaded_multipart_uploads(s3, files, bucket):
-#     # file = files[0]
-#     # keys = worker_manager(10, multi_chunk_upload, s3, [file], bucket)
-#     # received_file = get_object(s3, bucket, keys[0])
-#     #
-#     # if received_file == read_file(file):
-#     #     assert True
-#     # else:
-#     #     print("invalid contents retrieved")
-#     #     assert False
-#
-#     assert True
+def test_multithreaded_upload_singlethreaded_download(s3, files, bucket):
+    manager = worker_manager(10)
+    for file in files:
+        manager.post_task(lambda: multi_chunk_upload(s3, bucket, file))
+
+    keys = manager.run_tasks()
+
+    for key, file in keys:
+        verify(s3, bucket, key, file)
+
+    assert True
+
+def test_multithreaded_upload_multithreaded_download(s3, files, bucket):
+    manager = worker_manager(10)
+    for file in files:
+        manager.post_task(lambda: multi_chunk_upload(s3, bucket, file))
+
+    keys = manager.run_tasks()
+
+    for key, file in keys:
+        manager.post_task(lambda: verify(s3, bucket, key, file))
+
+    manager.run_tasks()
+
+    assert True
