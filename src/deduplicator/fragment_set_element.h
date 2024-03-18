1ab90d3b11c26a6b03cd2036737f887ea6f6406d
Max Plauth <max@ultihash.io>
2024-03-18 15:47:26 +0100

    added test cases/documentation for fragment_set and fragment_set_element

--- src/deduplicator/fragment_set_element.h
+++ src/deduplicator/fragment_set_element.h
@@ -6,12 +6,6 @@
 namespace uh::cluster {
 
 class fragment_set_element {
-    std::reference_wrapper<global_data_view> m_storage;
-    uint128_t m_pointer{};
-    uint16_t m_size{};
-    uint128_t m_prefix{0};
-    std::optional<std::string_view> m_data{};
-
 public:
     fragment_set_element(const uint128_t& ptr, uint16_t size_,
                          const uint128_t& prefix_, global_data_view& storage);
@@ -21,13 +15,28 @@ public:
                          global_data_view& storage);
     fragment_set_element(fragment_set_element&& f) noexcept;
 
-    void catch_frag(const fragment_set_element& f, shared_buffer<char>& data,
-                    std::string_view& str, bool& l1) const;
+    /**
+     * @brief Provides ordering information for fragment_set_element instances
+     * @param f A constant reference to an fragment_set_element to be compared
+     * against
+     * @return true if this fragment_set_element is lexicographically smaller
+     * than #f, false otherwise.
+     */
     bool operator<(const fragment_set_element& f) const;
 
     [[nodiscard]] const uint128_t& pointer() const;
     [[nodiscard]] uint16_t size() const;
     [[nodiscard]] const uint128_t& prefix() const;
+
+private:
+    std::reference_wrapper<global_data_view> m_storage;
+    uint128_t m_pointer{};
+    uint16_t m_size{};
+    uint128_t m_prefix{0};
+    std::optional<std::string_view> m_data{};
+
+    void catch_frag(const fragment_set_element& f, shared_buffer<char>& data,
+                    std::string_view& str, bool& l1) const;
 };
 } // namespace uh::cluster
 
---
+++ test/test_fragment_set.cpp
@@ -0,0 +1,238 @@
+#ifdef SINGLE_TEST_RUNNER
+#define BOOST_TEST_NO_MAIN
+#else
+#define BOOST_TEST_MODULE "fragment set tests"
+#endif
+
+#include "common/utils/temp_directory.h"
+#include "deduplicator/fragment_set.h"
+#include "gdv_fixture.h"
+#include <boost/test/unit_test.hpp>
+
+// ------------- Tests Suites Follow --------------
+
+namespace uh::cluster {
+
+BOOST_FIXTURE_TEST_CASE(insert_find_basic, global_data_view_fixture) {
+    temp_directory tmp_dir;
+    std::filesystem::path frag_set_log_path = tmp_dir.path() / "logfile";
+    auto gdv = get_global_data_view();
+    fragment_set frag_set(frag_set_log_path, *gdv);
+
+    shared_buffer<char> fragment_a(8 * KIBI_BYTE);
+    memset(fragment_a.data(), 'a', 8 * KIBI_BYTE);
+    auto addr_a = gdv->write(fragment_a.get_str_view());
+
+    shared_buffer<char> fragment_b(4 * KIBI_BYTE);
+    memset(fragment_b.data(), 'b', 4 * KIBI_BYTE);
+    auto addr_b = gdv->write(fragment_b.get_str_view());
+
+    shared_buffer<char> fragment_c(2 * KIBI_BYTE);
+    memset(fragment_c.data(), 'c', 2 * KIBI_BYTE);
+    auto addr_c = gdv->write(fragment_c.get_str_view());
+
+    // find a, see that the result shows no low/high candidates, insert a
+    {
+        auto result_a = frag_set.find(fragment_a.get_str_view());
+        BOOST_CHECK(!result_a.low.has_value());
+        BOOST_CHECK(!result_a.high.has_value());
+        frag_set.insert(addr_a.first().pointer, fragment_a.get_str_view(),
+                        result_a.hint);
+    }
+
+    // find a again, see that now high candidate points to previously inserted
+    // a, insert a again
+    {
+        auto result_a = frag_set.find(fragment_a.get_str_view());
+        BOOST_CHECK(!result_a.low.has_value());
+        BOOST_CHECK(result_a.high.has_value());
+        auto prefix_a = shared_buffer<char>(sizeof(uint128_t));
+        memcpy(prefix_a.data(), result_a.high->get().prefix().get_data(),
+               sizeof(uint128_t));
+        BOOST_CHECK(prefix_a.get_str_view() ==
+                    fragment_a.get_str_view().substr(0, sizeof(uint128_t)));
+        BOOST_CHECK(result_a.high->get().pointer() == addr_a.first().pointer);
+        BOOST_CHECK(result_a.high->get().size() == addr_a.first().size);
+        frag_set.insert(addr_a.first().pointer, fragment_a.get_str_view(),
+                        result_a.hint);
+    }
+
+    // find c, see that low candidate points to previously inserted a, insert c
+    {
+        auto result_c = frag_set.find(fragment_c.get_str_view());
+        frag_set.insert(addr_c.first().pointer, fragment_c.get_str_view(),
+                        result_c.hint);
+        BOOST_CHECK(result_c.low.has_value());
+        auto prefix_a = shared_buffer<char>(sizeof(uint128_t));
+        memcpy(prefix_a.data(), result_c.low->get().prefix().get_data(),
+               sizeof(uint128_t));
+        BOOST_CHECK(prefix_a.get_str_view() ==
+                    fragment_a.get_str_view().substr(0, sizeof(uint128_t)));
+        BOOST_CHECK(result_c.low->get().pointer() == addr_a.first().pointer);
+        BOOST_CHECK(result_c.low->get().size() == addr_a.first().size);
+        BOOST_CHECK(!result_c.high.has_value());
+    }
+
+    // find b, see that low points to a, high to c, insert b
+    {
+        auto result_b = frag_set.find(fragment_b.get_str_view());
+        frag_set.insert(addr_b.first().pointer, fragment_b.get_str_view(),
+                        result_b.hint);
+        BOOST_CHECK(result_b.low.has_value());
+        auto prefix_b_low = shared_buffer<char>(sizeof(uint128_t));
+        memcpy(prefix_b_low.data(), result_b.low->get().prefix().get_data(),
+               sizeof(uint128_t));
+        BOOST_CHECK(prefix_b_low.get_str_view() ==
+                    fragment_a.get_str_view().substr(0, sizeof(uint128_t)));
+        BOOST_CHECK(result_b.low->get().pointer() == addr_a.first().pointer);
+        BOOST_CHECK(result_b.low->get().size() == addr_a.first().size);
+        BOOST_CHECK(result_b.high.has_value());
+        auto prefix_b_high = shared_buffer<char>(sizeof(uint128_t));
+        memcpy(prefix_b_high.data(), result_b.high->get().prefix().get_data(),
+               sizeof(uint128_t));
+        BOOST_CHECK(prefix_b_high.get_str_view() ==
+                    fragment_c.get_str_view().substr(0, sizeof(uint128_t)));
+        BOOST_CHECK(result_b.high->get().pointer() == addr_c.first().pointer);
+        BOOST_CHECK(result_b.high->get().size() == addr_c.first().size);
+    }
+}
+
+BOOST_FIXTURE_TEST_CASE(insert_find_rebuild, global_data_view_fixture) {
+    temp_directory tmp_dir;
+    std::filesystem::path frag_set_log_path = tmp_dir.path() / "logfile";
+    auto gdv = get_global_data_view();
+
+    shared_buffer<char> fragment_a(8 * KIBI_BYTE);
+    memset(fragment_a.data(), 'a', 8 * KIBI_BYTE);
+    auto addr_a = gdv->write(fragment_a.get_str_view());
+
+    shared_buffer<char> fragment_b(4 * KIBI_BYTE);
+    memset(fragment_b.data(), 'b', 4 * KIBI_BYTE);
+    auto addr_b = gdv->write(fragment_b.get_str_view());
+
+    shared_buffer<char> fragment_c(2 * KIBI_BYTE);
+    memset(fragment_c.data(), 'c', 2 * KIBI_BYTE);
+    auto addr_c = gdv->write(fragment_c.get_str_view());
+
+    {
+        fragment_set frag_set(frag_set_log_path, *gdv);
+
+        // find a, see that the result shows no low/high candidates, insert a
+        {
+            auto result_a = frag_set.find(fragment_a.get_str_view());
+            BOOST_CHECK(!result_a.low.has_value());
+            BOOST_CHECK(!result_a.high.has_value());
+            frag_set.insert(addr_a.first().pointer, fragment_a.get_str_view(),
+                            result_a.hint);
+        }
+
+        // find a again, see that now high candidate points to previously
+        // inserted a, insert a again
+        {
+            auto result_a = frag_set.find(fragment_a.get_str_view());
+            BOOST_CHECK(!result_a.low.has_value());
+            BOOST_CHECK(result_a.high.has_value());
+            auto prefix_a = shared_buffer<char>(sizeof(uint128_t));
+            memcpy(prefix_a.data(), result_a.high->get().prefix().get_data(),
+                   sizeof(uint128_t));
+            BOOST_CHECK(prefix_a.get_str_view() ==
+                        fragment_a.get_str_view().substr(0, sizeof(uint128_t)));
+            BOOST_CHECK(result_a.high->get().pointer() ==
+                        addr_a.first().pointer);
+            BOOST_CHECK(result_a.high->get().size() == addr_a.first().size);
+            frag_set.insert(addr_a.first().pointer, fragment_a.get_str_view(),
+                            result_a.hint);
+        }
+    }
+
+    // destruct frag set and reconstruct it again from frag_set_log_path
+
+    {
+        fragment_set frag_set(frag_set_log_path, *gdv);
+
+        // find c, see that low candidate points to previously inserted a,
+        // insert c
+        {
+            auto result_c = frag_set.find(fragment_c.get_str_view());
+            frag_set.insert(addr_c.first().pointer, fragment_c.get_str_view(),
+                            result_c.hint);
+            BOOST_CHECK(result_c.low.has_value());
+            auto prefix_a = shared_buffer<char>(sizeof(uint128_t));
+            memcpy(prefix_a.data(), result_c.low->get().prefix().get_data(),
+                   sizeof(uint128_t));
+            BOOST_CHECK(prefix_a.get_str_view() ==
+                        fragment_a.get_str_view().substr(0, sizeof(uint128_t)));
+            BOOST_CHECK(result_c.low->get().pointer() ==
+                        addr_a.first().pointer);
+            BOOST_CHECK(result_c.low->get().size() == addr_a.first().size);
+            BOOST_CHECK(!result_c.high.has_value());
+        }
+
+        // find b, see that low points to a, high to c, insert b
+        {
+            auto result_b = frag_set.find(fragment_b.get_str_view());
+            frag_set.insert(addr_b.first().pointer, fragment_b.get_str_view(),
+                            result_b.hint);
+            BOOST_CHECK(result_b.low.has_value());
+            auto prefix_b_low = shared_buffer<char>(sizeof(uint128_t));
+            memcpy(prefix_b_low.data(), result_b.low->get().prefix().get_data(),
+                   sizeof(uint128_t));
+            BOOST_CHECK(prefix_b_low.get_str_view() ==
+                        fragment_a.get_str_view().substr(0, sizeof(uint128_t)));
+            BOOST_CHECK(result_b.low->get().pointer() ==
+                        addr_a.first().pointer);
+            BOOST_CHECK(result_b.low->get().size() == addr_a.first().size);
+            BOOST_CHECK(result_b.high.has_value());
+            auto prefix_b_high = shared_buffer<char>(sizeof(uint128_t));
+            memcpy(prefix_b_high.data(),
+                   result_b.high->get().prefix().get_data(), sizeof(uint128_t));
+            BOOST_CHECK(prefix_b_high.get_str_view() ==
+                        fragment_c.get_str_view().substr(0, sizeof(uint128_t)));
+            BOOST_CHECK(result_b.high->get().pointer() ==
+                        addr_c.first().pointer);
+            BOOST_CHECK(result_b.high->get().size() == addr_c.first().size);
+        }
+    }
+}
+
+BOOST_FIXTURE_TEST_CASE(less_operator, global_data_view_fixture) {
+    temp_directory tmp_dir;
+    std::filesystem::path frag_set_log_path = tmp_dir.path() / "logfile";
+    auto gdv = get_global_data_view();
+    fragment_set frag_set(frag_set_log_path, *gdv);
+
+    shared_buffer<char> fragment_a(8 * KIBI_BYTE);
+    memset(fragment_a.data(), 'a', 8 * KIBI_BYTE);
+    auto addr_a = gdv->write(fragment_a.get_str_view());
+
+    shared_buffer<char> fragment_b(8 * KIBI_BYTE);
+    memset(fragment_b.data(), 'a', 2 * KIBI_BYTE);
+    memset(fragment_b.data() + 2 * KIBI_BYTE, 'b', 2 * KIBI_BYTE);
+    memset(fragment_b.data() + 4 * KIBI_BYTE, 'a', 2 * KIBI_BYTE);
+    memset(fragment_b.data() + 4 * KIBI_BYTE, 'b', 2 * KIBI_BYTE);
+
+    auto addr_b = gdv->write(fragment_b.get_str_view());
+
+    shared_buffer<char> fragment_c(8 * KIBI_BYTE);
+    memset(fragment_c.data(), 'a', 2 * KIBI_BYTE);
+    memset(fragment_c.data() + 2 * KIBI_BYTE, 'b', 2 * KIBI_BYTE);
+    memset(fragment_c.data() + 4 * KIBI_BYTE, 'a', 2 * KIBI_BYTE);
+    memset(fragment_c.data() + 4 * KIBI_BYTE, 'c', 2 * KIBI_BYTE);
+    auto addr_c = gdv->write(fragment_c.get_str_view());
+
+    // This will create fragment_elements which ONLY contain the prefix
+    fragment_set_element frag_element_a(fragment_a.get_str_view(),
+                                        addr_a.first().pointer, *gdv);
+    fragment_set_element frag_element_b(fragment_b.get_str_view(),
+                                        addr_b.first().pointer, *gdv);
+    fragment_set_element frag_element_c(fragment_c.get_str_view(),
+                                        addr_c.first().pointer, *gdv);
+
+    // Since all fragments have identical prefix, calling operator< will be
+    // forced to consult gdv to get full body
+    BOOST_CHECK(frag_element_a < frag_element_b);
+    BOOST_CHECK(frag_element_a < frag_element_c);
+    BOOST_CHECK(frag_element_b < frag_element_c);
+}
+
+} // namespace uh::cluster
