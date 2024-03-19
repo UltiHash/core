aff43c76fe5e66902761ee8d2bc7d4770d295897
Ankit Pandit <anktpndit@gmail.com>
2024-03-19 19:40:26 +0100

    removed std::cout

--- src/entrypoint/http/http_request.cpp
+++ src/entrypoint/http/http_request.cpp
@@ -1,5 +1,4 @@
 #include "http_request.h"
-#include <iostream>
 
 namespace uh::cluster {
 
@@ -77,8 +76,6 @@ coro<std::unique_ptr<http_request>> read_request(asio::ip::tcp::socket& s) {
                                             asio::use_awaitable);
 
     rv->m_uri = uri(rv->m_req);
-
-    std::cout << *rv << std::endl;
 
     co_return rv;
 }
