0dcd7c3200da9e7a059a616dc9d6729ac8cff361
Max Plauth <max@ultihash.io>
2024-03-21 10:42:13 +0100

    initialize ser/des buffers first

--- src/deduplicator/fragment_set_log.cpp
+++ src/deduplicator/fragment_set_log.cpp
@@ -13,7 +13,7 @@ fragment_set_log::fragment_set_log(std::filesystem::path log_path)
 
 void fragment_set_log::append(const log_entry& entry) {
 
-    std::array<char, m_entry_size> buf;
+    std::array<char, m_entry_size> buf{};
     zpp::bits::out{buf, zpp::bits::size4b{}}(entry).or_throw();
     m_log_file.write(buf.data(), m_entry_size);
 }
@@ -42,7 +42,7 @@ fragment_set_log::~fragment_set_log() {
 }
 
 [[nodiscard]] fragment_set_log::log_entry fragment_set_log::read_entry() {
-    std::array<char, m_entry_size> buf;
+    std::array<char, m_entry_size> buf{};
     m_log_file.read(buf.data(), m_entry_size);
     log_entry entry;
     zpp::bits::in{buf, zpp::bits::size4b{}}(entry).or_throw();
