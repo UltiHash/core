356b17e45ad5b622fcf3bd4b18c664c139229e10
Massi <masoud@ultihash.io>
2024-03-21 15:41:16 +0100

    dummy test for ci error

--- src/storage/data_store.cpp
+++ src/storage/data_store.cpp
@@ -67,7 +67,7 @@ data_store::data_store(data_store_config conf, std::size_t id, bool adaptive)
     metric<storage_used_space_gauge, byte, int64_t>::register_gauge_callback(
         std::bind(&data_store::get_used_space, this));
     m_used = fetch_used_space();
-    std::cout << m_open_files.size() << " " << m_used << m_last_fd << " " << m_last_file_data_end << std::endl;
+    std::cout << "creating " << m_open_files.size() << " " << m_used << " " << m_last_fd << " " << m_last_file_data_end << std::endl;
 }
 
 address data_store::write(std::span<char> data) {
@@ -139,6 +139,8 @@ data_store::~data_store() {
         fsync(open_file);
         close(open_file);
     }
+    std::cout << "destroying " << m_open_files.size() << " " << m_used << " " << m_last_fd << " " << m_last_file_data_end << std::endl;
+
 }
 
 std::pair<int, long> data_store::get_file_offset_pair(const uint128_t& pointer) const {
