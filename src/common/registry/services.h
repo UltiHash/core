ed5196eedc9e45a3ee44fd21de052e8cad2ed343
Stefan Jank <stefan@ultihash.io>
2024-04-04 21:55:55 +0200

    Add broadcast to services.

--- src/common/registry/services.h
+++ src/common/registry/services.h
@@ -179,6 +179,72 @@ public:
         return clients_list;
     }
 
+    template <typename func, typename result = std::invoke_result<
+                                 func, acquired_messenger<client>,
+                                 std::size_t>::type::value_type>
+    requires(std::is_same_v<client, coro_client> &&
+             std::is_same_v<result, void>)
+    coro<void> broadcast(func f) const {
+        auto clients = get_clients();
+        if (clients.empty()) {
+            throw std::runtime_error("no services available");
+        }
+
+        std::vector<std::shared_ptr<awaitable_promise<void>>> results(
+            clients.size());
+
+        std::size_t index = 0;
+        for (const auto& c : clients) {
+            auto promise = std::make_shared<awaitable_promise<void>>(m_ioc);
+
+            auto messenger = co_await c->acquire_messenger();
+            boost::asio::co_spawn(m_ioc, f(std::move(messenger), index),
+                                  use_awaitable_promise(promise));
+
+            results[index] = promise;
+            ++index;
+        }
+
+        for (auto p : results) {
+            co_await p->get();
+        }
+    }
+
+    template <typename func, typename result = std::invoke_result<
+                                 func, acquired_messenger<client>,
+                                 std::size_t>::type::value_type>
+    requires(std::is_same_v<client, coro_client> &&
+             !std::is_same_v<result, void>)
+    coro<std::vector<result>> broadcast(func f) const {
+        auto clients = get_clients();
+        if (clients.empty()) {
+            throw std::runtime_error("no services available");
+        }
+
+        std::vector<std::shared_ptr<awaitable_promise<result>>> results(
+            clients.size());
+
+        std::size_t index = 0;
+        for (const auto& c : clients) {
+            auto promise =
+                std::make_shared<awaitable_promise<coro<result>>>(m_ioc);
+
+            auto messenger = co_await c->acquire_messenger();
+            boost::asio::co_spawn(m_ioc, f(messenger, index),
+                                  use_awaitable_promise(promise));
+
+            results[index] = promise;
+            ++index;
+        }
+
+        std::vector<result> rv;
+        for (auto p : results) {
+            rv.push_back(co_await p->get());
+        }
+
+        co_return rv;
+    }
+
 private:
     struct service_endpoint {
         std::size_t id{};
