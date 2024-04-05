e9867a0cea657da4f3b57a6de2c2b0b735e986f3
Massi <masoud@ultihash.io>
2024-04-05 14:14:43 +0200

    fix test

--- test/test_fragment_set.cpp
+++ test/test_fragment_set.cpp
@@ -24,8 +24,8 @@ void insert_a_again(shared_buffer<char>& fragment_a, address& addr_a,
     BOOST_CHECK(!result_a.low.has_value());
     BOOST_CHECK(result_a.high.has_value());
     auto prefix_a = shared_buffer<char>(sizeof(uint128_t));
-    memcpy(prefix_a.data(), result_a.high->get().prefix().get_data(),
-           sizeof(uint128_t));
+    memcpy(prefix_a.data(), result_a.high->get().prefix().data(),
+           result_a.high->get().prefix().size());
     BOOST_CHECK(prefix_a.get_str_view() ==
                 fragment_a.get_str_view().substr(0, sizeof(uint128_t)));
     BOOST_CHECK(result_a.high->get().pointer() == addr_a.first().pointer);
@@ -42,8 +42,8 @@ void insert_c(shared_buffer<char>& fragment_a, address& addr_a,
                     result_c.hint);
     BOOST_CHECK(result_c.low.has_value());
     auto prefix_a = shared_buffer<char>(sizeof(uint128_t));
-    memcpy(prefix_a.data(), result_c.low->get().prefix().get_data(),
-           sizeof(uint128_t));
+    memcpy(prefix_a.data(), result_c.low->get().prefix().data(),
+           result_c.low->get().prefix().size());
     BOOST_CHECK(prefix_a.get_str_view() ==
                 fragment_a.get_str_view().substr(0, sizeof(uint128_t)));
     BOOST_CHECK(result_c.low->get().pointer() == addr_a.first().pointer);
@@ -60,16 +60,16 @@ void insert_b(shared_buffer<char>& fragment_a, address& addr_a,
                     result_b.hint);
     BOOST_CHECK(result_b.low.has_value());
     auto prefix_b_low = shared_buffer<char>(sizeof(uint128_t));
-    memcpy(prefix_b_low.data(), result_b.low->get().prefix().get_data(),
-           sizeof(uint128_t));
+    memcpy(prefix_b_low.data(), result_b.low->get().prefix().data(),
+           result_b.low->get().prefix().size());
     BOOST_CHECK(prefix_b_low.get_str_view() ==
                 fragment_a.get_str_view().substr(0, sizeof(uint128_t)));
     BOOST_CHECK(result_b.low->get().pointer() == addr_a.first().pointer);
     BOOST_CHECK(result_b.low->get().size() == addr_a.first().size);
     BOOST_CHECK(result_b.high.has_value());
     auto prefix_b_high = shared_buffer<char>(sizeof(uint128_t));
-    memcpy(prefix_b_high.data(), result_b.high->get().prefix().get_data(),
-           sizeof(uint128_t));
+    memcpy(prefix_b_high.data(), result_b.high->get().prefix().data(),
+           result_b.high->get().prefix().size());
     BOOST_CHECK(prefix_b_high.get_str_view() ==
                 fragment_c.get_str_view().substr(0, sizeof(uint128_t)));
     BOOST_CHECK(result_b.high->get().pointer() == addr_c.first().pointer);
@@ -109,17 +109,14 @@ BOOST_FIXTURE_TEST_CASE(insert_find_rebuild, global_data_view_fixture) {
     shared_buffer<char> fragment_a(8 * KIBI_BYTE);
     memset(fragment_a.data(), 'a', 8 * KIBI_BYTE);
     auto addr_a = gdv->write(fragment_a.get_str_view());
-    gdv->add_l1(addr_a.first().pointer, fragment_a.get_str_view());
 
     shared_buffer<char> fragment_b(4 * KIBI_BYTE);
     memset(fragment_b.data(), 'b', 4 * KIBI_BYTE);
     auto addr_b = gdv->write(fragment_b.get_str_view());
-    gdv->add_l1(addr_b.first().pointer, fragment_b.get_str_view());
 
     shared_buffer<char> fragment_c(2 * KIBI_BYTE);
     memset(fragment_c.data(), 'c', 2 * KIBI_BYTE);
     auto addr_c = gdv->write(fragment_c.get_str_view());
-    gdv->add_l1(addr_c.first().pointer, fragment_c.get_str_view());
 
     {
         fragment_set frag_set(frag_set_log_path, *gdv);
@@ -167,12 +164,16 @@ BOOST_FIXTURE_TEST_CASE(less_operator, global_data_view_fixture) {
     auto addr_c = gdv->write(fragment_c.get_str_view());
 
     // This will create fragment_elements which ONLY contain the prefix
+    auto prefix_a = fragment_a.get_str_view().substr(0, std::min (PREFIX_SIZE, fragment_a.size()));
+    auto prefix_b = fragment_b.get_str_view().substr(0, std::min (PREFIX_SIZE, fragment_b.size()));
+    auto prefix_c = fragment_c.get_str_view().substr(0, std::min (PREFIX_SIZE, fragment_c.size()));
+
     fragment_set_element frag_element_a(fragment_a.get_str_view(),
-                                        addr_a.first().pointer, *gdv);
+                                        addr_a.first().pointer, std::string(prefix_a), *gdv);
     fragment_set_element frag_element_b(fragment_b.get_str_view(),
-                                        addr_b.first().pointer, *gdv);
+                                        addr_b.first().pointer, std::string(prefix_b), *gdv);
     fragment_set_element frag_element_c(fragment_c.get_str_view(),
-                                        addr_c.first().pointer, *gdv);
+                                        addr_c.first().pointer, std::string(prefix_c), *gdv);
 
     // Since all fragments have identical prefix, calling operator< will be
     // forced to consult gdv to get full body
@@ -189,8 +190,7 @@ BOOST_FIXTURE_TEST_CASE(insert_performance, global_data_view_fixture) {
     fragment_set_log::log_entry entry = {
         .op = INSERT,
         .pointer = {0x6465647570, 0x6c69636174696f6e},
-        .size = 13,
-        .prefix = {0x707265666978}};
+        .size = 13};
 
     const auto start = std::chrono::steady_clock::now();
     for (std::size_t i = 0; i < 10000000; i++) {
