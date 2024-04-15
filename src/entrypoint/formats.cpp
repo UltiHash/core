083ceadccb86cbb9a8c7c36019ea9487e75ccc59
Stefan Jank <stefan@ultihash.io>
2024-04-15 18:01:24 +0200

    Store modified time as std::chrono::time_point. Formatting of time_point in EP.

---
+++ src/entrypoint/formats.cpp
@@ -0,0 +1,27 @@
+#include "formats.h"
+
+#include <ctime>
+#include <iomanip>
+
+namespace uh::cluster {
+
+std::string imf_fixdate(const utc_time& ts) {
+    std::stringstream ss;
+
+    auto t = utc_time::clock::to_time_t(ts);
+    ss << std::put_time(std::localtime(&t), "%a, %d %b %Y %H:%M:%S %Z");
+
+    return ss.str();
+}
+
+std::string iso8601_date(const utc_time& ts) {
+    std::stringstream ss;
+
+    auto t = utc_time::clock::to_time_t(ts);
+    tm buf;
+    ss << std::put_time(gmtime_r(&t, &buf), "%FT%TZ");
+
+    return ss.str();
+}
+
+} // namespace uh::cluster
