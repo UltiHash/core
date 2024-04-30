e20cb5eb5d3087c40d7ded91f2b54054a9977f84
Massi <masoud@ultihash.io>
2024-04-30 18:54:09 +0200

    log test message for feature test failure in list bucket

--- src/entrypoint/entrypoint_handler.h
+++ src/entrypoint/entrypoint_handler.h
@@ -60,7 +60,7 @@ public:
         } catch (const boost::system::system_error& se) {
             if (se.code() != http::error::end_of_stream) {
                 LOG_ERROR() << se.what();
-                command_exception err(http::status::bad_request, "BadRequest",
+                command_exception err(http::status::bad_request, se.what(),
                                       se.what());
                 http::write(s, make_response(err));
                 s.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
