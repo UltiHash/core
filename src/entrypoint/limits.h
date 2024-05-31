e2a5a63d4c621dcf15794efb4e6d27a32c21e24f
sjulti <118182787+sjulti@users.noreply.github.com>
2024-05-31 11:59:15 +0200

    Remove get_bucket command. (#422)

---
+++ src/entrypoint/limits.h
@@ -0,0 +1,40 @@
+#ifndef CORE_ENTRYPOINT_LIMITS_H
+#define CORE_ENTRYPOINT_LIMITS_H
+
+#include <atomic>
+
+namespace uh::cluster {
+
+class limits {
+public:
+    limits(std::size_t max_data_size);
+
+    /**
+     * Set storage size without checking.
+     */
+    void storage_size(std::size_t size);
+
+    /**
+     * Check internal storage size and increment the counter.
+     */
+    void check_storage_size(std::size_t increment);
+
+    /**
+     * Reduce internal storage size counter.
+     */
+    void free_storage_size(std::size_t decrement);
+
+    // warn about a nearly reached size limit at this percentage
+    static constexpr unsigned SIZE_LIMIT_WARNING_PERCENTAGE = 80;
+    // number of files to upload between two warnings
+    static constexpr unsigned SIZE_LIMIT_WARNING_INTERVAL = 100;
+
+private:
+    std::size_t m_max_data_size;
+    std::atomic<std::size_t> m_data_storage_size;
+    unsigned m_warn_counter = SIZE_LIMIT_WARNING_INTERVAL;
+};
+
+} // namespace uh::cluster
+
+#endif
