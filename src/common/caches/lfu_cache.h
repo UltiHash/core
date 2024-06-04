e92374feb72645c863b10f950f4287af3b3b1ef8
Massi <masoud@ultihash.io>
2024-06-04 17:09:46 +0200

    minor fixes

--- src/common/caches/lfu_cache.h
+++ src/common/caches/lfu_cache.h
@@ -39,20 +39,19 @@ public:
 
     inline void put_non_existing(const Key& key, Value&& val) {
 
-        if (m_freq_buckets.begin()->freq != 1) {
+        if (m_freq_buckets.front().freq != 1) {
             m_freq_buckets.emplace_front(1);
         }
 
         auto first_bucket = m_freq_buckets.begin();
         first_bucket->items.emplace_back(key);
 
-        key_data k_data{
-            .val = std::forward<Value>(val),
-            .bucket = first_bucket,
-            .pos = std::prev(first_bucket->items.cend()),
-        };
-
-        m_key_data.emplace(key, std::move(k_data));
+        m_key_data.emplace(key,
+                           key_data{
+                               .val = std::forward<Value>(val),
+                               .bucket = first_bucket,
+                               .pos = std::prev(first_bucket->items.cend()),
+                           });
         if (m_capacity == 0) {
             auto& front_list = m_freq_buckets.front().items;
             const auto& rem_key = front_list.front();
@@ -66,9 +65,9 @@ public:
             if (front_list.empty()) {
                 m_freq_buckets.pop_front();
             }
-            m_capacity++;
+        } else {
+            m_capacity--;
         }
-        m_capacity--;
     }
 
     inline void use(const Key& key) {
@@ -110,105 +109,6 @@ private:
     }
 };
 
-/*
-template <typename Key, typename Value> class lfu_cache {
-
-    struct freq_bucket {
-        int freq;
-        std::list<Key> keys;
-    };
-
-    struct item {
-        Value value;
-        std::list<freq_bucket>::iterator bucket;
-        std::list<Key>::iterator position;
-    };
-
-    public:
-        explicit lfu_cache(size_t capacity,
-std::optional<std::function<void(Key, Value)>> removal_callback = std::nullopt)
-            : m_capacity{capacity},
-              m_removal_callback{std::move(removal_callback)} {}
-
-        std::optional<std::reference_wrapper<Value>> get(const Key& key) {
-            auto it = m_lookup.find(key);
-            if (it == m_lookup.cend())
-                return std::nullopt;
-            m_lookup[key] = increment(key, it->second.value);
-            return it->second.value;
-        }
-
-        inline void use(const Key& key) {
-            if (auto it = m_lookup.find(key); it != m_lookup.end())
-                m_lookup[key] = increment(key, it->second.value);
-        }
-
-        inline void put(const Key& key, Value&& val) {
-            m_lookup[key] = increment(key, std::forward<Value>(val));
-        }
-
-    private:
-        item increment(const Key& key, const Value& val) {
-            auto it = m_lookup.find(key);
-            if (it == m_lookup.end()) {
-                if (m_capacity == 0) {
-                    drop_least();
-                    ++m_capacity;
-                }
-                auto bucket = get_one_bucket();
-                bucket->keys.push_back(key);
-                --m_capacity;
-                return {val, bucket, std::prev(bucket->keys.end())};
-            } else {
-                auto bucket = get_next_bucket(it->second.bucket);
-                bucket->keys.push_back(key);
-                it->second.bucket->keys.erase(it->second.position);
-                maybe_drop_bucket(it->second.bucket);
-                return {val, bucket, std::prev(bucket->keys.end())};
-            }
-        }
-
-        std::list<freq_bucket>::iterator get_one_bucket() {
-            if (m_buckets.empty() || m_buckets.front().freq != 1) {
-                m_buckets.push_front({1,{}});
-            }
-            return m_buckets.begin();
-        }
-
-        std::list<freq_bucket>::iterator
-        get_next_bucket(std::list<freq_bucket>::iterator curr) {
-            auto next = std::next(curr);
-            if (next == m_buckets.end() || curr->freq + 1 < next->freq) {
-                next = m_buckets.insert(next, {curr->freq + 1,{}});
-            }
-            return next;
-        }
-
-        void maybe_drop_bucket(std::list<freq_bucket>::iterator bucket) {
-            if (bucket->keys.empty())
-                m_buckets.erase(bucket);
-        }
-
-        void drop_least() {
-            auto& key = m_buckets.front().keys.front();
-            m_buckets.front().keys.pop_front();
-
-            auto itr = m_lookup.find(key);
-            if (m_removal_callback) {
-                (*m_removal_callback)(key,
-                                      itr->second.value);
-            }
-            m_lookup.erase(itr);
-            maybe_drop_bucket(m_buckets.begin());
-        }
-
-        size_t m_capacity;
-        std::optional<std::function<void(Key, Value)>> m_removal_callback;
-
-        std::unordered_map<Key, item> m_lookup;
-        std::list<freq_bucket> m_buckets;
-    };
-*/
 } // end namespace uh::cluster
 
 #endif // UH_CLUSTER_LFU_CACHE_H
