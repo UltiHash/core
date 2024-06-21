a08242d79af24fb4604208ee2478ded3abba6723
Gongotar <masood.gholami@aol.com>
2024-06-21 17:20:16 +0200

    cache fix (#446)

--- src/common/global_data/config.h
+++ src/common/global_data/config.h
@@ -8,7 +8,7 @@ namespace uh::cluster {
 
 struct global_data_view_config {
     std::size_t storage_service_connection_count = 16;
-    std::size_t read_cache_capacity_l2 = 4000ul;
+    std::size_t read_cache_capacity_l2 = 400000ul;
 };
 } // namespace uh::cluster
 #endif // UH_CLUSTER_CONFIG_H
