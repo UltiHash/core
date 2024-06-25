9685cda4b73744009c745bacbdefbf0aca36ad59
Massi <masoud@ultihash.io>
2024-06-25 13:25:46 +0200

    initial version

--- src/common/telemetry/traces.cpp
+++ src/common/telemetry/traces.cpp
@@ -6,6 +6,9 @@
 #include "opentelemetry/sdk/trace/simple_processor_factory.h"
 #include "opentelemetry/sdk/trace/tracer_provider_factory.h"
 #include "opentelemetry/trace/propagation/http_trace_context.h"
+#include "opentelemetry/sdk/trace/tracer_provider.h"
+#include "opentelemetry/trace/provider.h"
+#include "common/utils/common.h"
 
 namespace uh::cluster {
 namespace trace_sdk = opentelemetry::sdk::trace;
@@ -18,11 +21,11 @@ void initialize_traces_exporter(const std::string& endpoint) {
             trace_opts);
     auto processor =
         trace_sdk::SimpleSpanProcessorFactory::Create(std::move(exporter));
-    tracer_provider =
+    trace::tracer_provider =
         trace_sdk::TracerProviderFactory::Create(std::move(processor));
 
     std::shared_ptr<opentelemetry::trace::TracerProvider> api_provider =
-        tracer_provider;
+        trace::tracer_provider;
     opentelemetry::trace::Provider::SetTracerProvider(api_provider);
 
     opentelemetry::context::propagation::GlobalTextMapPropagator::
@@ -35,5 +38,86 @@ void initialize_traces_exporter(const std::string& endpoint) {
 std::shared_ptr<opentelemetry::trace::Tracer> get_tracer() {
     auto provider = opentelemetry::trace::Provider::GetTracerProvider();
     return provider->GetTracer("uh-cluster-traces", OPENTELEMETRY_SDK_VERSION);
+}
+
+auto trace::span(
+    const std::string& name,
+    std::optional<opentelemetry::context::Context> context) {
+    opentelemetry::trace::StartSpanOptions opt;
+    if (context) {
+        opt.parent = *context;
+    }
+    return tracer()->StartSpan(name, opt);
+}
+
+auto trace::scoped_span(
+    const std::string& name,
+    std::optional<opentelemetry::context::Context> context) {
+    opentelemetry::trace::StartSpanOptions opt;
+    if (context) {
+        opt.parent = *context;
+    }
+    return opentelemetry::trace::Scope(tracer()->StartSpan(name, opt));
+}
+
+auto trace::deserialize_context(std::vector<char>&& buf) {
+    carrier c (std::move(buf));
+    auto prop        = opentelemetry::context::propagation::GlobalTextMapPropagator::GetGlobalPropagator();
+    auto current_ctx = opentelemetry::context::RuntimeContext::GetCurrent();
+    auto new_context = prop->Extract(c, current_ctx);
+    return new_context;
+}
+
+auto trace::serialize_context(opentelemetry::context::Context& context){
+    carrier c;
+    auto prop = opentelemetry::context::propagation::GlobalTextMapPropagator::GetGlobalPropagator();
+    prop->Inject(c, context);
+    return c.extract_buffer();
+}
+
+trace::~trace() {
+    if (tracer_provider) {
+#ifdef OPENTELEMETRY_DEPRECATED_SDK_FACTORY
+        dynamic_cast<opentelemetry::sdk::trace::TracerProvider*>(
+            tracer_provider.get())
+            ->ForceFlush();
+#else
+        tracer_provider->ForceFlush();
+#endif /* OPENTELEMETRY_DEPRECATED_SDK_FACTORY */
+    }
+
+    tracer_provider.reset();
+    std::shared_ptr<opentelemetry::trace::TracerProvider> none;
+    opentelemetry::trace::Provider::SetTracerProvider(none);
+}
+
+trace::carrier::carrier(std::vector<char>&& buf) // for deserializing
+    : m_buf(std::move(buf)) {
+    auto [in, out] = zpp::bits::in_out(m_buf);
+    while (in.position() < m_buf.size()) {
+        std::string key;
+        in(key).or_throw();
+        std::string value;
+        in(value).or_throw();
+        m_kv.emplace(std::move(key), std::move(value));
+    }
+}
+opentelemetry::nostd::string_view
+trace::carrier::Get(opentelemetry::nostd::string_view key) const noexcept {
+    auto it = m_kv.find(std::string(key));
+    if (it != m_kv.end()) {
+        return it->second;
+    }
+    return "";
+}
+void trace::carrier::Set(opentelemetry::nostd::string_view key,
+                  opentelemetry::nostd::string_view value) noexcept {
+    zpp::bits::out{m_buf, zpp::bits::size4b{}, zpp::bits::append{}}(key)
+        .or_throw();
+    zpp::bits::out{m_buf, zpp::bits::size4b{}, zpp::bits::append{}}(value)
+        .or_throw();
+}
+std::vector<char> trace::carrier::extract_buffer() noexcept {
+    return std::move(m_buf);
 }
 } // namespace uh::cluster
