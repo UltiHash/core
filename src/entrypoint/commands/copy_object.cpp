c132a9cb738516960d2694d76f334c3ef678bf6d
Stefan Jank <stefan@ultihash.io>
2024-06-27 15:05:07 +0200

    Correctly name ETag in response.

--- src/entrypoint/commands/copy_object.cpp
+++ src/entrypoint/commands/copy_object.cpp
@@ -39,7 +39,7 @@ coro<void> copy_object::handle(http_request& req) const {
     boost::property_tree::ptree pt;
     pt.put("CopyObjectResult.LastModified", iso8601_date(obj.last_modified));
     if (obj.etag) {
-        pt.put("CopyObjectResult.Etag", *obj.etag);
+        pt.put("CopyObjectResult.ETag", *obj.etag);
     }
 
     http_response res;
