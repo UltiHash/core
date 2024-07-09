446b108b8a1626991a5c5464f6b245391ad8aeb0
Max Plauth <max@ultihash.io>
2024-07-09 14:47:51 +0200

    initial LMDB-backed refcounter mechanism

---
+++ src/storage/reference_counter.h
@@ -0,0 +1,23 @@
+#ifndef UH_CLUSTER_REFERENCE_COUNTER_H
+#define UH_CLUSTER_REFERENCE_COUNTER_H
+
+#include <cstdint>
+#include <filesystem>
+#include <lmdb++.h>
+#include <map>
+#include <set>
+
+namespace uh::cluster {
+class reference_counter {
+public:
+    explicit reference_counter(const std::filesystem::path& root);
+    void initialize(const std::set<std::size_t>&);
+    std::map<std::size_t, std::size_t> decrement(const std::set<std::size_t>& pages);
+    std::map<std::size_t, std::size_t> increment(const std::set<std::size_t>& pages);
+    //std::size_t at(std::size_t page_id);
+private:
+    lmdb::env m_env;
+
+};
+}
+#endif // UH_CLUSTER_REFERENCE_COUNTER_H
