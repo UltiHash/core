b3a20b63130e84224f0a11f0299997477c7ae9a0
Max Plauth <max@ultihash.io>
2024-07-30 09:57:14 +0200

    refcount 0 triggers cb

--- src/storage/data_store.cpp
+++ src/storage/data_store.cpp
@@ -1,6 +1,7 @@
 #include "data_store.h"
 #include "common/telemetry/metrics.h"
 #include "common/utils/pointer_traits.h"
+#include <iostream>
 #include <mutex>
 
 namespace uh::cluster {
@@ -12,7 +13,8 @@ data_store::data_store(data_store_config conf,
       m_data_store_id(data_store_id),
       m_root(working_dir / std::to_string(data_store_id)),
       m_conf(conf),
-      m_refcounter(m_root, m_conf.page_size) {
+      m_refcounter(m_root, m_conf.page_size,
+                   std::bind_front(&data_store::internal_delete, this)) {
 
     m_open_files.reserve(2 * m_conf.max_data_store_size / m_conf.file_size + 1);
 
@@ -388,6 +390,10 @@ data_store::find_async_data(size_t pointer, size_t size) {
         }
     }
     return {0, nullptr};
+}
+
+void data_store::internal_delete(std::size_t offset, std::size_t size) {
+    std::cout << "internal_delete triggered" << std::endl;
 }
 
 } // end namespace uh::cluster
--- src/storage/reference_counter.cpp
+++ src/storage/reference_counter.cpp
@@ -3,10 +3,12 @@
 
 namespace uh::cluster {
 
-reference_counter::reference_counter(const std::filesystem::path& root,
-                                     const std::size_t page_size)
+reference_counter::reference_counter(
+    const std::filesystem::path& root, const std::size_t page_size,
+    const std::function<void(std::size_t offset, std::size_t size)>& cb)
     : m_env(lmdb::env::create()),
-      m_page_size(page_size) {
+      m_page_size(page_size),
+      m_cb(cb) {
     m_env.set_max_dbs(1);
     m_env.set_mapsize(TEBI_BYTE);
     if (!std::filesystem::exists(root)) {
@@ -19,6 +21,10 @@ void reference_counter::decrement(const std::size_t offset,
                                   const std::size_t size) {
     lmdb::txn txn = lmdb::txn::begin(m_env, nullptr, 0);
     lmdb::dbi dbi = lmdb::dbi::open(txn, nullptr);
+
+    std::vector<std::pair<std::size_t, std::size_t>> marked_for_deletion;
+    std::optional<std::size_t> deleteRangeStart;
+    std::optional<std::size_t> deleteRangeEnd;
 
     for (std::size_t page_pointer = offset; page_pointer < offset + size;
          page_pointer += m_page_size) {
@@ -40,13 +46,39 @@ void reference_counter::decrement(const std::size_t offset,
             throw std::runtime_error("reference counter is already at zero");
         }
 
-        --current_value;
+        if (--current_value == 0) {
+            if (deleteRangeStart.has_value() && deleteRangeEnd.has_value() &&
+                page_id == deleteRangeEnd.value() + 1) {
+                deleteRangeEnd = page_id;
+            } else {
+                if (deleteRangeStart.has_value() &&
+                    deleteRangeEnd.has_value()) {
+                    marked_for_deletion.emplace_back(deleteRangeStart.value(),
+                                                     deleteRangeEnd.value());
+                }
+                deleteRangeStart = page_id;
+                deleteRangeEnd = page_id;
+            }
+        }
+
         value = std::string_view(reinterpret_cast<const char*>(&current_value),
                                  sizeof(current_value));
         dbi.put(txn, key, value);
     }
 
+    if (deleteRangeStart.has_value() && deleteRangeEnd.has_value()) {
+        marked_for_deletion.emplace_back(deleteRangeStart.value(),
+                                         deleteRangeEnd.value());
+    }
+
     txn.commit();
+
+    for (auto range : marked_for_deletion) {
+        std::size_t del_offset = range.first * m_page_size;
+        std::size_t del_size =
+            (range.second - range.first) * m_page_size + m_page_size;
+        m_cb(del_offset, del_size);
+    }
 }
 
 void reference_counter::increment(const std::size_t offset,
