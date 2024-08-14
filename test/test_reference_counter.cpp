5b12ad57f874e0daf21439df0c87a75270213bb5
Max Plauth <max@ultihash.io>
2024-08-14 15:46:29 +0200

    adapted reference counter behaviour

--- test/test_reference_counter.cpp
+++ test/test_reference_counter.cpp
@@ -19,15 +19,18 @@ BOOST_AUTO_TEST_CASE(test_increment_decrement) {
         [&delete_triggered](std::size_t offset, std::size_t size) {
             delete_triggered = true;
         });
+    BOOST_CHECK_THROW(refcounter.increment(0, DEFAULT_PAGE_SIZE),
+                      std::out_of_range);
     BOOST_CHECK_THROW(refcounter.decrement(0, DEFAULT_PAGE_SIZE),
                       std::runtime_error);
-    BOOST_CHECK_NO_THROW(refcounter.increment(0, DEFAULT_PAGE_SIZE));
+    BOOST_CHECK_NO_THROW(refcounter.increment(0, DEFAULT_PAGE_SIZE, true));
     BOOST_CHECK(!delete_triggered);
     BOOST_CHECK_NO_THROW(refcounter.decrement(0, DEFAULT_PAGE_SIZE));
     BOOST_CHECK(delete_triggered);
     BOOST_CHECK_THROW(refcounter.decrement(0, DEFAULT_PAGE_SIZE),
                       std::runtime_error);
-    BOOST_CHECK_NO_THROW(refcounter.increment(0, DEFAULT_PAGE_SIZE));
+    BOOST_CHECK_THROW(refcounter.increment(0, DEFAULT_PAGE_SIZE),
+                      std::out_of_range);
 }
 
 BOOST_AUTO_TEST_CASE(test_increment_restart_decrement) {
@@ -35,9 +38,11 @@ BOOST_AUTO_TEST_CASE(test_increment_restart_decrement) {
     {
         reference_counter refcounter(testdir.path(), DEFAULT_PAGE_SIZE,
                                      dummy_delete);
+        BOOST_CHECK_THROW(refcounter.increment(0, DEFAULT_PAGE_SIZE),
+                          std::out_of_range);
         BOOST_CHECK_THROW(refcounter.decrement(0, DEFAULT_PAGE_SIZE),
                           std::runtime_error);
-        BOOST_CHECK_NO_THROW(refcounter.increment(0, DEFAULT_PAGE_SIZE));
+        BOOST_CHECK_NO_THROW(refcounter.increment(0, DEFAULT_PAGE_SIZE, true));
     }
     {
         bool delete_triggered = false;
@@ -51,7 +56,8 @@ BOOST_AUTO_TEST_CASE(test_increment_restart_decrement) {
         BOOST_CHECK(delete_triggered);
         BOOST_CHECK_THROW(refcounter.decrement(0, DEFAULT_PAGE_SIZE),
                           std::runtime_error);
-        BOOST_CHECK_NO_THROW(refcounter.increment(0, DEFAULT_PAGE_SIZE));
+        BOOST_CHECK_THROW(refcounter.increment(0, DEFAULT_PAGE_SIZE),
+                          std::out_of_range);
     }
 }
 
@@ -63,11 +69,13 @@ BOOST_AUTO_TEST_CASE(test_bulk_increment_decrement) {
         [&delete_triggered](std::size_t offset, std::size_t size) {
             delete_triggered++;
         });
-    refcounter.increment(0, GIBI_BYTE);
+    BOOST_CHECK_THROW(refcounter.increment(0, GIBI_BYTE), std::out_of_range);
+    BOOST_CHECK_NO_THROW(refcounter.increment(0, GIBI_BYTE, true));
     BOOST_CHECK(delete_triggered == 0);
-    refcounter.decrement(0, GIBI_BYTE);
+    BOOST_CHECK_NO_THROW(refcounter.decrement(0, GIBI_BYTE));
     BOOST_CHECK(delete_triggered == 1);
     BOOST_CHECK_THROW(refcounter.decrement(0, GIBI_BYTE), std::runtime_error);
+    BOOST_CHECK_THROW(refcounter.increment(0, GIBI_BYTE), std::out_of_range);
 }
 
 } // end namespace uh::cluster
