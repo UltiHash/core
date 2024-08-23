66381234c473657d7eac8f56b76e2eeba289bcef
Masoud Jami <massi@pop-os.fritz.box>
2024-08-23 14:48:22 +0200

    sginal handler fix

--- src/common/utils/signal_handler.h
+++ src/common/utils/signal_handler.h
@@ -19,11 +19,21 @@ public:
         : m_ioc(1),
           m_signals(m_ioc, SIGINT, SIGTERM) {
 
+        sigset_t set;
+        sigemptyset(&set);
+        sigaddset(&set, SIGINT);
+        sigaddset(&set, SIGTERM);
+        if (pthread_sigmask(SIG_BLOCK, &set, nullptr) != 0)
+            throw std::runtime_error("Could not setup the signal handler");
         m_signals.async_wait(
             [this](const boost::system::error_code& error, int signal) {
                 handle_signal(error, signal);
             });
-        m_signal_handler_thread = std::thread([this]() { m_ioc.run(); });
+        m_signal_handler_thread = std::thread([this, set]() {
+            if (pthread_sigmask(SIG_UNBLOCK, &set, nullptr) != 0)
+                throw std::runtime_error("Could not setup the signal handler");
+            m_ioc.run();
+        });
     }
 
     void add_callback(const std::function<void(void)>& fn) {
