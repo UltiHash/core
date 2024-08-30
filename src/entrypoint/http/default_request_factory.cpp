f91e829cb5e2f756f2219cd8740ab6c770eff592
Stefan Jank <stefan@ultihash.io>
2024-08-30 10:02:57 +0200

    Support reading trailing headers in default request factory.

--- src/entrypoint/http/default_request_factory.cpp
+++ src/entrypoint/http/default_request_factory.cpp
@@ -14,21 +14,25 @@ namespace {
 
 std::unique_ptr<ep::http::body> make_body(partial_parse_result& req) {
 
-    /* Amazon will upload data using chunked transfer without explicitly setting
-     * the `Transfer-Encoding` header for signed data. This also prevents us
-     * from using beasts HTTP parser for decoding the transfer encoding.
-     *
-     * (see
-     * https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-auth-using-authorization-header.html#sigv4-auth-header-overview)
-     */
-    auto content_sha = req.optional("x-amz-content-sha256");
-    if (content_sha &&
-        (*content_sha == "STREAMING-UNSIGNED-PAYLOAD-TRAILER" ||
-         *content_sha == "STREAMING-AWS4-HMAC-SHA256-PAYLOAD" ||
-         *content_sha == "STREAMING-AWS4-HMAC-SHA256-PAYLOAD-TRAILER" ||
-         *content_sha == "STREAMING-AWS4-ECDSA-P256-SHA256-PAYLOAD" ||
-         *content_sha == "STREAMING-AWS4-ECDSA-P256-SHA256-PAYLOAD-TRAILER")) {
+    auto transfer_encoding = req.optional("content-encoding");
+    if (!transfer_encoding || *transfer_encoding != "aws-chunked") {
+
+        std::size_t length =
+            std::stoul(req.optional("content-length").value_or("0"));
+        return std::make_unique<raw_body>(req, length);
+    }
+
+    auto content_sha = req.require("x-amz-content-sha256");
+    if (content_sha == "STREAMING-AWS4-HMAC-SHA256-PAYLOAD" ||
+        content_sha == "STREAMING-AWS4-ECDSA-P256-SHA256-PAYLOAD") {
         return std::make_unique<chunked_body>(req);
+    }
+
+    if (content_sha == "STREAMING-UNSIGNED-PAYLOAD-TRAILER" ||
+        content_sha == "STREAMING-AWS4-HMAC-SHA256-PAYLOAD-TRAILER" ||
+        content_sha == "STREAMING-AWS4-ECDSA-P256-SHA256-PAYLOAD-TRAILER") {
+        return std::make_unique<chunked_body>(
+            req, chunked_body::trailing_headers::read);
     }
 
     std::size_t length =
