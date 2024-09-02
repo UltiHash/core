cd6139a92483f3c5e2a849a6c3d1b54319992a74
Masoud Jami <massi@pop-os.ultihash.io>
2024-09-02 14:02:50 +0200

    ec factory

--- test/test_ec.cpp
+++ test/test_ec.cpp
@@ -1,11 +1,12 @@
 #define BOOST_TEST_MODULE "ec tests"
 
+#include "common/ec/no_ec.h"
 #include "common/telemetry/log.h"
 #include "common/types/common_types.h"
 #include "common/utils/time_utils.h"
 
 #include <boost/test/unit_test.hpp>
-#include <common/ec/ec_calculator.h>
+#include <common/ec/reedsolomon_c.h>
 
 // ------------- Tests Suites Follow --------------
 
@@ -18,14 +19,14 @@ void fill_random(char* buf, size_t size) {
 }
 
 BOOST_AUTO_TEST_CASE(ec_basic) {
-    ec_calculator ec(4, 2);
+    reedsolomon_c ec(4, 2);
     std::string data(64 * KIBI_BYTE, '0');
     fill_random(data.data(), data.size());
     auto encoded = ec.encode(data);
     auto shards = encoded.get();
-    std::vector stats(6, ec_calculator::data_stat::valid);
-    stats[1] = ec_calculator::data_stat::lost;
-    stats[3] = ec_calculator::data_stat::lost;
+    std::vector stats(6, data_stat::valid);
+    stats[1] = data_stat::lost;
+    stats[3] = data_stat::lost;
 
     std::string s1(shards.at(1).size(), '0');
     std::string s3(shards.at(1).size(), '0');
@@ -43,15 +44,15 @@ BOOST_AUTO_TEST_CASE(ec_basic) {
 }
 
 BOOST_AUTO_TEST_CASE(ec_basic_lost) {
-    ec_calculator ec(4, 2);
+    reedsolomon_c ec(4, 2);
     std::string data(64 * KIBI_BYTE, '0');
     fill_random(data.data(), data.size());
     auto encoded = ec.encode(data);
     auto shards = encoded.get();
-    std::vector stats(6, ec_calculator::data_stat::valid);
-    stats[1] = ec_calculator::data_stat::lost;
-    stats[3] = ec_calculator::data_stat::lost;
-    stats[4] = ec_calculator::data_stat::lost;
+    std::vector stats(6, data_stat::valid);
+    stats[1] = data_stat::lost;
+    stats[3] = data_stat::lost;
+    stats[4] = data_stat::lost;
 
     std::string s1(shards.at(1).size(), '0');
     std::string s3(shards.at(1).size(), '0');
@@ -68,11 +69,66 @@ BOOST_AUTO_TEST_CASE(ec_basic_lost) {
     BOOST_CHECK_THROW(ec.recover(shards, stats), std::runtime_error);
 }
 
-BOOST_AUTO_TEST_CASE(no_ec) {
-    ec_calculator ec(4, 0);
+BOOST_AUTO_TEST_CASE(two_times_ec) {
+    reedsolomon_c ec(4, 2);
+    {
+        std::string data(64 * KIBI_BYTE, '0');
+        fill_random(data.data(), data.size());
+        auto encoded = ec.encode(data);
+        auto shards = encoded.get();
+        std::vector stats(6, data_stat::valid);
+        stats[1] = data_stat::lost;
+        stats[3] = data_stat::lost;
+
+        std::string s1(shards.at(1).size(), '0');
+        std::string s3(shards.at(1).size(), '0');
+
+        shards.at(1) = s1;
+        shards.at(3) = s3;
+
+        BOOST_CHECK(shards.at(1) != encoded.get().at(1));
+        BOOST_CHECK(shards.at(3) != encoded.get().at(3));
+
+        ec.recover(shards, stats);
+
+        int i = 0;
+        for (auto s : shards) {
+            BOOST_CHECK(s == encoded.get().at(i++));
+        }
+    }
+
+    {
+        std::string data(64 * KIBI_BYTE, '0');
+        fill_random(data.data(), data.size());
+        auto encoded = ec.encode(data);
+        auto shards = encoded.get();
+        std::vector stats(6, data_stat::valid);
+        stats[1] = data_stat::lost;
+        stats[3] = data_stat::lost;
+
+        std::string s1(shards.at(1).size(), '0');
+        std::string s3(shards.at(1).size(), '0');
+
+        shards.at(1) = s1;
+        shards.at(3) = s3;
+
+        BOOST_CHECK(shards.at(1) != encoded.get().at(1));
+        BOOST_CHECK(shards.at(3) != encoded.get().at(3));
+
+        ec.recover(shards, stats);
+
+        int i = 0;
+        for (auto s : shards) {
+            BOOST_CHECK(s == encoded.get().at(i++));
+        }
+    }
+}
+
+BOOST_AUTO_TEST_CASE(no_ec_test) {
+    no_ec nec(4);
     std::string data(64 * KIBI_BYTE, '0');
     fill_random(data.data(), data.size());
-    auto encoded = ec.encode(data);
+    auto encoded = nec.encode(data);
     auto shards = encoded.get();
     const auto shard_size = shards.front().size();
     size_t i = 0;
@@ -85,15 +141,15 @@ BOOST_AUTO_TEST_CASE(no_ec) {
 }
 
 BOOST_AUTO_TEST_CASE(ec_non_divisable) {
-    ec_calculator ec(7, 3);
+    reedsolomon_c ec(7, 3);
     std::string data(64 * KIBI_BYTE, '0');
     fill_random(data.data(), data.size());
     auto encoded = ec.encode(data);
     auto shards = encoded.get();
-    std::vector stats(7, ec_calculator::data_stat::valid);
-    stats[1] = ec_calculator::data_stat::lost;
-    stats[3] = ec_calculator::data_stat::lost;
-    stats[4] = ec_calculator::data_stat::lost;
+    std::vector stats(7, data_stat::valid);
+    stats[1] = data_stat::lost;
+    stats[3] = data_stat::lost;
+    stats[4] = data_stat::lost;
 
     std::string s1(shards.at(1).size(), '0');
     std::string s3(shards.at(1).size(), '0');
@@ -116,17 +172,17 @@ BOOST_AUTO_TEST_CASE(ec_non_divisable) {
 }
 
 BOOST_AUTO_TEST_CASE(large_data) {
-    ec_calculator ec(8, 4);
+    reedsolomon_c ec(8, 4);
     std::string data(1 * GIBI_BYTE, '0');
     fill_random(data.data(), data.size());
 
     auto encoded = ec.encode(data);
 
     auto shards = encoded.get();
-    std::vector stats(12, ec_calculator::data_stat::valid);
-    stats[1] = ec_calculator::data_stat::lost;
-    stats[3] = ec_calculator::data_stat::lost;
-    stats[4] = ec_calculator::data_stat::lost;
+    std::vector stats(12, data_stat::valid);
+    stats[1] = data_stat::lost;
+    stats[3] = data_stat::lost;
+    stats[4] = data_stat::lost;
 
     std::string s1(shards.at(1).size(), '0');
     std::string s3(shards.at(1).size(), '0');
