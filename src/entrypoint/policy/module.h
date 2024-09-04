6a5cf71eb8a8b9804188fc13289f1127f7af9cfc
Stefan Jank <sj@stiftstrasse.de>
2024-09-04 10:02:01 +0200

    Define policy module interface  (#506)

---
+++ src/entrypoint/policy/module.h
@@ -0,0 +1,22 @@
+#ifndef CORE_ENTRYPOINT_POLICY_MODULE_H
+#define CORE_ENTRYPOINT_POLICY_MODULE_H
+
+#include "entrypoint/commands/command.h"
+#include "entrypoint/http/http_request.h"
+
+namespace uh::cluster::ep::policy {
+
+enum class action { allow, deny };
+
+class module {
+public:
+    /**
+     * Check configured policies to determine whether the provided
+     * request is allowed to proceed.
+     */
+    action check(const http_request& request, const command& cmd);
+};
+
+} // namespace uh::cluster::ep::policy
+
+#endif
