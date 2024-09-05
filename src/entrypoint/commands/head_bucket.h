33fbd9b7ed46cf438d907582dd835a99168cb6bd
Stefan Jank <stefan@ultihash.io>
2024-09-05 11:10:09 +0200

    Add support for HeadBucket command.

---
+++ src/entrypoint/commands/head_bucket.h
@@ -0,0 +1,23 @@
+#ifndef CORE_ENTRYPOINT_COMMANDS_HEAD_BUCKET_H
+#define CORE_ENTRYPOINT_COMMANDS_HEAD_BUCKET_H
+
+#include "command.h"
+#include "entrypoint/directory.h"
+
+namespace uh::cluster {
+
+class head_bucket : public command {
+public:
+    explicit head_bucket(directory& dir);
+
+    static bool can_handle(const http_request& req);
+
+    coro<http_response> handle(http_request& req) override;
+
+private:
+    directory& m_directory;
+};
+
+} // namespace uh::cluster
+
+#endif
