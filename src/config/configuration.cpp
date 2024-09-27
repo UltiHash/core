157d54ac796ad7e42b189e715a2558c8d67789b3
Stefan Jank <stefan@ultihash.io>
2024-09-27 13:06:28 +0200

    Expose user database config over CLI.

--- src/config/configuration.cpp
+++ src/config/configuration.cpp
@@ -305,6 +305,11 @@ void configure(CLI::App& app, db::config& cfg) {
                    "Number of connections to multipart database")
         ->default_val(cfg.multipart.count)
         ->envname(ENV_CFG_DB_MULTIPART_CONNECTIONS);
+
+    app.add_option("--db-users-connections", cfg.users.count,
+                   "Number of connections to users database")
+        ->default_val(cfg.users.count)
+        ->envname(ENV_CFG_DB_USERS_CONNECTIONS);
 }
 
 void configure(CLI::App& app, boost::log::trivial::severity_level& log_level) {
