1f94e3aa606966adce99ea3d78ed86d5173f3228
Stefan Jank <stefan@ultihash.io>
2024-09-27 15:59:15 +0200

    Fixup: shift operator.

--- src/common/crypto/scrypt.h
+++ src/common/crypto/scrypt.h
@@ -10,7 +10,7 @@ namespace uh::cluster {
 class scrypt {
 public:
     struct config {
-        uint64_t n = 2u << 16;
+        uint64_t n = 1u << 17;
         uint32_t r = 8u;
         uint32_t p = 1u;
 
