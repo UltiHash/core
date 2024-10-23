6d60cfe7aa37cf303d194f3082decbc8f245976a
Stefan Jank <stefan@ultihash.io>
2024-10-23 11:47:10 +0200

    Test policy variables.

--- test/test_variables.cpp
+++ test/test_variables.cpp
@@ -5,6 +5,7 @@
 #include <entrypoint/commands/command.h>
 #include <entrypoint/http/request.h>
 #include <entrypoint/policy/variables.h>
+#include <test/http_request.h>
 
 using namespace uh::cluster;
 using namespace uh::cluster::ep;
@@ -106,6 +107,59 @@ BOOST_AUTO_TEST_CASE(wildcard_match) {
     BOOST_CHECK(equals_wildcard("*?", "f"));
     BOOST_CHECK(!equals_wildcard("*?", ""));
     BOOST_CHECK(!equals_wildcard("root", "arn:foo:root"));
+}
+
+BOOST_AUTO_TEST_CASE(default_variables) {
+    auto user_arn = "arn:aws:iam::2:random_user";
+    auto request = test::make_request(
+        "GET /bucket/object?delimiter=!!!&prefix=foo HTTP/1.1\r\n"
+        "User-Agent: USER_AGENT\r\n"
+        "X-amz-content-sha256: UNSIGNED-PAYLOAD\r\n"
+        "x-amz-copy-source: COPY_SOURCE\r\n"
+        "Referer: HTTP_REFERER\r\n\r\n",
+        user_arn);
+
+    auto command = test::mock_command("s3:GetObject");
+    variables vars(request, command);
+
+    BOOST_CHECK_EQUAL(vars.get("uh:ActionId").value_or(""), "s3:GetObject");
+    BOOST_CHECK_EQUAL(vars.get("uh:actionid").value_or(""), "s3:GetObject");
+
+    BOOST_CHECK_EQUAL(vars.get("uh:ResourceArn").value_or(""),
+                      "arn:aws:s3:::bucket/object");
+    BOOST_CHECK_EQUAL(vars.get("uh:resourcearn").value_or(""),
+                      "arn:aws:s3:::bucket/object");
+
+    BOOST_CHECK_EQUAL(vars.get("aws:PrincipalArn").value_or(""), user_arn);
+    BOOST_CHECK_EQUAL(vars.get("aws:principalarn").value_or(""), user_arn);
+
+    BOOST_CHECK_EQUAL(vars.get("aws:userid").value_or("non-empty"), "");
+    BOOST_CHECK_EQUAL(vars.get("aws:UsErId").value_or("non-empty"), "");
+
+    BOOST_CHECK_EQUAL(vars.get("aws:SourceIp").value_or(""), "0.0.0.0");
+    BOOST_CHECK_EQUAL(vars.get("aws:SoUrCeIp").value_or(""), "0.0.0.0");
+
+    BOOST_CHECK_EQUAL(vars.get("aws:referer").value_or(""), "HTTP_REFERER");
+    BOOST_CHECK_EQUAL(vars.get("aws:ReFeReR").value_or(""), "HTTP_REFERER");
+
+    BOOST_CHECK_EQUAL(vars.get("aws:UserAgent").value_or(""), "USER_AGENT");
+    BOOST_CHECK_EQUAL(vars.get("aws:usEragEnT").value_or(""), "USER_AGENT");
+
+    BOOST_CHECK_EQUAL(vars.get("s3:x-amz-content-sha256").value_or(""),
+                      "UNSIGNED-PAYLOAD");
+    BOOST_CHECK_EQUAL(vars.get("s3:X-AmZ-CoNtEnT-ShA256").value_or(""),
+                      "UNSIGNED-PAYLOAD");
+
+    BOOST_CHECK_EQUAL(vars.get("s3:x-amz-copy-source").value_or(""),
+                      "COPY_SOURCE");
+    BOOST_CHECK_EQUAL(vars.get("s3:X-AmZ-CoPy-sOuRcE").value_or(""),
+                      "COPY_SOURCE");
+
+    BOOST_CHECK_EQUAL(vars.get("s3:delimiter").value_or(""), "!!!");
+    BOOST_CHECK_EQUAL(vars.get("s3:DeLiMiTeR").value_or(""), "!!!");
+
+    BOOST_CHECK_EQUAL(vars.get("s3:prefix").value_or(""), "foo");
+    BOOST_CHECK_EQUAL(vars.get("s3:PrEfIx").value_or(""), "foo");
 }
 
 } // namespace
