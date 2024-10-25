4cc70064babbe3ea6530c70e415bc46993df2fb7
Sungsik Nam <jmyl@me.com>
2024-10-25 10:16:50 +0200

    Eng 561 - add date format parser (#569)
    Co-authored-by: Sungsik Nam <ssnam@vueron.org>
    Co-authored-by: Max Plauth <max@ultihash.io>

--- src/entrypoint/formats.cpp
+++ src/entrypoint/formats.cpp
@@ -1,7 +1,8 @@
 #include "formats.h"
 
+#include <chrono>
 #include <ctime>
-#include <iomanip>
+#include <stdexcept>
 
 namespace uh::cluster {
 
@@ -15,6 +16,7 @@ std::string imf_fixdate(const utc_time& ts) {
     return ss.str();
 }
 
+// Flow: time_point -> time_t ->  tm -> string
 std::string iso8601_date(const utc_time& ts) {
     std::stringstream ss;
 
@@ -24,5 +26,78 @@ std::string iso8601_date(const utc_time& ts) {
 
     return ss.str();
 }
+
+using namespace std::chrono_literals;
+
+/*
+ * Constants, related to specification
+ */
+constexpr auto constexpr_size = std::char_traits<char>::length;
+constexpr auto date_len = constexpr_size("2011-02-18T23:12:34");
+constexpr auto tz_len = constexpr_size("+02:00");
+
+inline std::runtime_error create_time_format_error() {
+    return std::runtime_error("Time format error: `2011-02-18T23:12:34-02:00` "
+                              "and `2011-02-18T23:12:34Z` supported");
+}
+
+utc_time read_iso8601_date(std::string_view str) {
+
+    auto date_str = std::string_view(str).substr(0, date_len);
+    auto tz_str = std::string_view(str).substr(date_len, str.size() - date_len);
+
+    auto time = detail::read_local_date(date_str);
+    auto offset = detail::read_timezone(tz_str);
+    return time + offset;
+}
+
+namespace detail {
+
+// Flow: string -> tm -> time_t -> time_point
+utc_time read_local_date(std::string_view sv) {
+    if (sv.size() != date_len) [[unlikely]]
+        throw create_time_format_error();
+
+    std::istringstream ss;
+    ss.rdbuf()->pubsetbuf(const_cast<char*>(sv.data()), sv.size());
+
+    std::tm t = {};
+    ss >> std::get_time(&t, "%Y-%m-%dT%H:%M:%S");
+    if (ss.fail()) [[unlikely]]
+        throw create_time_format_error();
+
+    return utc_time::clock::from_time_t(timegm(&t));
+}
+
+std::chrono::hours read_timezone(std::string_view sv) {
+    if (sv == "Z") [[unlikely]]
+        return 0h;
+
+    if (sv.size() != tz_len) [[unlikely]]
+        throw create_time_format_error();
+
+    auto& pol = sv[0];
+    if (pol != '+' && pol != '-') [[unlikely]]
+        throw create_time_format_error();
+
+    // Drop sign
+    std::istringstream ss;
+    ss.rdbuf()->pubsetbuf(const_cast<char*>(sv.data() + 1), sv.size() - 1);
+
+    tm t;
+    ss >> std::get_time(&t, "%H:00"); // Parse time in "HH:00" format
+    if (ss.fail()) [[unlikely]]
+        throw create_time_format_error();
+
+    if (t.tm_hour <= -24 || t.tm_hour >= 24) [[unlikely]]
+        throw create_time_format_error();
+
+    auto offset = std::chrono::hours(t.tm_hour);
+    if (pol == '-')
+        offset = -offset;
+    return offset;
+}
+
+} // namespace detail
 
 } // namespace uh::cluster
