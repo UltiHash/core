1660b6b94b5fef3f84988ce6d38a8be5993f8d96
Sungsik Nam <ssnam@vueron.org>
2024-10-28 12:10:34 +0100

    Change every private function tests to public function tests

--- test/test_formats.cpp
+++ test/test_formats.cpp
@@ -20,19 +20,70 @@ namespace bdata = boost::unit_test::data;
  */
 
 BOOST_AUTO_TEST_CASE(iso8601_date__reverses_read_iso8601_date_when_TZD_is_Z) {
+    // Arrange
     auto str = std::string("2011-02-18T23:12:34Z");
-
+    // Act
     auto read_date = iso8601_date(read_iso8601_date(str));
-
+    // Assert
     BOOST_TEST(read_date == str);
 }
 
-BOOST_AUTO_TEST_CASE(iso8601_date__prints_UTC_time) {
+BOOST_AUTO_TEST_CASE(iso8601_date__handles_minus_timeoffset) {
     auto str = std::string("2011-02-18T23:12:34-02:00");
 
     auto read_date = iso8601_date(read_iso8601_date(str));
 
     BOOST_TEST(read_date == "2011-02-18T21:12:34Z");
+}
+
+BOOST_AUTO_TEST_CASE(iso8601_date__handles_plus_timeoffset) {
+    auto str = std::string("2011-02-18T23:12:34+02:00");
+
+    auto read_date = iso8601_date(read_iso8601_date(str));
+
+    BOOST_TEST(read_date == "2011-02-19T01:12:34Z");
+}
+
+BOOST_DATA_TEST_CASE(iso8601_date__handles_offsets_in_reasonable_range,
+                     bdata::xrange(24)) {
+    constexpr auto hour_offset = std::char_traits<char>::length("2020-01-01T");
+    auto base_date = std::string("2020-01-01T00:00:00");
+
+    std::stringstream ss;
+    auto utc_date = base_date + "Z";
+    ss << std::setw(2) << std::setfill('0') << sample;
+    utc_date[hour_offset] = ss.str()[0];
+    utc_date[hour_offset + 1] = ss.str()[1];
+
+    ss.clear();
+    ss.str("");
+    ss << base_date << "+" << std::setw(2) << std::setfill('0') << sample
+       << ":00";
+    auto date_with_timezone = iso8601_date(read_iso8601_date(ss.str()));
+
+    BOOST_TEST(utc_date == date_with_timezone);
+}
+
+BOOST_DATA_TEST_CASE(read_timezone__does_not_handle_wrong_input,
+                     bdata::make(std::vector<std::string>{
+                         "", "-24:00", "+24:00", "+00:01", "X", "0Z", "00:00",
+                         "+0:00", "+00:0", "*00:00Z", "+00:00Z"}),
+                     str) {
+    BOOST_REQUIRE_THROW(read_iso8601_date("2020-01-01T" + str),
+                        std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(read_iso8601_date__does_not_handle_decimal_fraction) {
+    // Arrange
+    auto str = "2011-02-18T23:12:34.5Z";
+    // Act and Assert
+    BOOST_REQUIRE_THROW(read_iso8601_date(str), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(read_local_date__does_not_handle_wrong_input) {
+    auto str = "2011-02-18X23:12:34Z";
+
+    BOOST_REQUIRE_THROW(detail::read_local_date(str), std::runtime_error);
 }
 
 BOOST_AUTO_TEST_CASE(read_iso8601_date__handles_the_year_2260) {
@@ -58,74 +109,4 @@ BOOST_DATA_TEST_CASE(read_iso8601_date_survive_on_random_test,
     auto read_tp = read_iso8601_date(iso8601_date(tp));
 
     BOOST_TEST(read_tp == tp);
-}
-
-/******************************************************************************
- * Tests for functions in detail namespace.
- */
-
-BOOST_AUTO_TEST_CASE(read_local_date__handles_seconds) {
-    // Arrange
-    auto str = "2011-02-18T23:12:34";
-    // Act
-    auto date = iso8601_date(detail::read_local_date(str));
-    // Assert
-    BOOST_CHECK_EQUAL(date, "2011-02-18T23:12:34Z");
-}
-
-BOOST_AUTO_TEST_CASE(read_local_date__does_not_handle_decimal_fraction) {
-    // Arrange
-    auto str = "2011-02-18T23:12:34.5";
-    // Act and Assert
-    BOOST_REQUIRE_THROW(detail::read_local_date(str), std::runtime_error);
-}
-
-BOOST_AUTO_TEST_CASE(read_local_date__does_not_handle_wrong_input) {
-    auto str = "2011-02-18X23:12:34";
-
-    BOOST_REQUIRE_THROW(detail::read_local_date(str), std::runtime_error);
-}
-
-BOOST_AUTO_TEST_CASE(read_timezone__handles_Z) {
-    auto str = "Z";
-
-    auto offset = detail::read_timezone(str);
-
-    BOOST_TEST(offset == 0h);
-}
-
-BOOST_AUTO_TEST_CASE(read_timezone__handles_plus_TZ) {
-    auto str = "+02:00";
-
-    auto offset = detail::read_timezone(str);
-
-    BOOST_TEST(offset == 2h);
-}
-
-BOOST_AUTO_TEST_CASE(read_timezone__handles_minus_TZ) {
-    auto str = "-02:00";
-
-    auto offset = detail::read_timezone(str);
-
-    BOOST_TEST(offset == -2h);
-}
-
-BOOST_DATA_TEST_CASE(read_timezone__handles_offsets_in_reasonable_range,
-                     bdata::xrange(-23, 24)) {
-    std::stringstream ss;
-    auto sample_offset = std::chrono::hours(sample);
-    ss << ((sample < 0) ? "-" : "+");
-    ss << std::setw(2) << std::setfill('0') << std::abs(sample) << ":00";
-
-    auto offset = detail::read_timezone(ss.str());
-
-    BOOST_TEST(offset == sample_offset);
-}
-
-BOOST_DATA_TEST_CASE(read_timezone__does_not_handle_wrong_input,
-                     bdata::make(std::vector<std::string>{
-                         "-24:00", "+24:00", "+00:01", "X", "0Z", "00:00",
-                         "+0:00", "+00:0", "*00:00Z", "+00:00Z"}),
-                     str) {
-    BOOST_REQUIRE_THROW(detail::read_timezone(str), std::runtime_error);
 }
