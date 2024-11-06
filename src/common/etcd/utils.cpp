5e686aca287208a6e349a9de50115bc7777ec6a0
Sungsik Nam <ssnam@vueron.org>
2024-11-06 13:04:12 +0100

    Add retry on the constructor of SyncClient

--- src/common/etcd/utils.cpp
+++ src/common/etcd/utils.cpp
@@ -1,13 +1,21 @@
 #include "utils.h"
 
+#include "common/telemetry/log.h"
+
 namespace uh::cluster {
 
 etcd::SyncClient make_etcd_client(const etcd_config& cfg) {
-    if (cfg.username && cfg.password) {
-        return etcd::SyncClient(cfg.url, *cfg.username, *cfg.password);
-    }
+    while (true) {
+        try {
+            if (cfg.username && cfg.password) {
+                return etcd::SyncClient(cfg.url, *cfg.username, *cfg.password);
+            }
 
-    return etcd::SyncClient(cfg.url);
+            return etcd::SyncClient(cfg.url);
+        } catch (const std::exception& e) {
+            LOG_INFO() << "Exception from etcd::SyncClient, " << e.what();
+        }
+    }
 }
 
 } // namespace uh::cluster
