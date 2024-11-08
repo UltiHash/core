6ecf2996b23530fbde9fcf4910bd24f5ab527d87
Sungsik Nam <sungsik@ultihash.io>
2024-11-08 08:33:26 +0100

    Wait for etcd forever (#587)
    Co-authored-by: Sungsik Nam <ssnam@vueron.org>

--- src/common/etcd/utils.cpp
+++ src/common/etcd/utils.cpp
@@ -1,13 +1,32 @@
 #include "utils.h"
 
+#include "common/telemetry/log.h"
+
 namespace uh::cluster {
 
-etcd::SyncClient make_etcd_client(const etcd_config& cfg) {
-    if (cfg.username && cfg.password) {
-        return etcd::SyncClient(cfg.url, *cfg.username, *cfg.password);
-    }
+std::unique_ptr<etcd::SyncClient> make_etcd_client(const etcd_config& cfg) {
+    while (true) {
+        try {
+            std::unique_ptr<etcd::SyncClient> client;
+            if (cfg.username && cfg.password) {
+                client = std::make_unique<etcd::SyncClient>(
+                    cfg.url, *cfg.username, *cfg.password);
+            } else {
+                client = std::make_unique<etcd::SyncClient>(cfg.url);
+            }
 
-    return etcd::SyncClient(cfg.url);
+            if (!client->head().is_ok()) {
+                LOG_INFO() << "cannot connect to etcd. trying to reconnect";
+                sleep(1);
+                continue;
+            }
+            return client;
+        } catch (const std::exception& e) {
+            LOG_INFO() << "cannot connect to etcd. trying to reconnect: "
+                       << e.what();
+            sleep(1);
+        }
+    }
 }
 
 } // namespace uh::cluster
