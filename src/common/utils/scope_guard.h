501ba39934c4963d23fc47e281b3dd17a05e32b1
Stefan Jank <sjank@ltrace.de>
2024-11-20 11:40:12 +0100

    Add value_guard.

--- src/common/utils/scope_guard.h
+++ src/common/utils/scope_guard.h
@@ -31,6 +31,40 @@ template <typename fini> guard<fini> scope_guard(fini f) {
     return guard<fini>(std::move(f));
 }
 
+template <typename value, typename fini> class value_guard {
+public:
+    value_guard(value v, fini f)
+        : m_value(std::move(v)),
+          m_f(std::move(f)) {}
+
+    ~value_guard() {
+        try {
+            release();
+        } catch (...) {
+        }
+    }
+
+    void release() {
+        if (m_active) {
+            m_active = false;
+            m_f();
+        }
+    }
+
+    value& operator*() { return m_value; }
+    value* operator->() { return &m_value; }
+
+private:
+    bool m_active = true;
+    fini m_f;
+    value m_value;
+};
+
+template <typename value, typename fini>
+value_guard<value, fini> make_value_guard(value v, fini f) {
+    return value_guard<value, fini>(std::move(v), std::move(f));
+}
+
 } // namespace uh::cluster
 
 #endif
