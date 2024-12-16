UltiHash/core

commit f3767e46f4ea76ebba90353ee76ead7775909d92

Eng 777 remove cold fragment impl (#633)
Co-authored-by: S3A Tests <s3a.tests@ultihash.io>

src/deduplicator/dedupe_set/fragment_set.cpp
@@ -16,9 +16,7 @@ fragment_set::response fragment_set::find(const std::string_view& data) {
     auto res = m_set.lower_bound(f);
 
     response resp;
-    if (res->m_state != COLD) {
-        resp.hint.emplace(res);
-    }
+    resp.hint.emplace(res);
 
     if (res != m_set.cend()) [[likely]] {
         resp.high.emplace(fragment{res->pointer(), res->size()}, res->prefix());
@@ -71,19 +69,7 @@ std::lock_guard<std::shared_mutex> fragment_set::lock() {
 
 void fragment_set::remove(
     const std::set<fragment_set_element>::const_iterator& itr) {
-    if (itr->m_hint_count > 0) {
-        itr->m_state = COLD;
-        m_colds.emplace_front(itr);
-    } else {
-        m_set.erase(itr);
-    }
-    std::erase_if(m_colds, [this](const auto& item) {
-        if (item->m_hint_count == 0) {
-            m_set.erase(item);
-            return true;
-        }
-        return false;
-    });
+    m_set.erase(itr);
 }
 
 void fragment_set::erase(const fragment& set_element, bool header) {

src/deduplicator/dedupe_set/fragment_set.h
@@ -142,8 +142,6 @@ private:
     lfu_cache<uint128_t, std::set<fragment_set_element>::const_iterator> m_lfu;
     lfu_cache<uint128_t, std::set<fragment_set_element>::const_iterator>
         m_lfu_headers;
-
-    std::forward_list<std::set<fragment_set_element>::const_iterator> m_colds;
 };
 
 } // end namespace uh::cluster

src/deduplicator/dedupe_set/fragment_set_element.cpp
@@ -29,8 +29,7 @@ fragment_set_element::fragment_set_element(const std::string_view& data,
       m_data(std::nullopt) {}
 
 fragment_set_element::fragment_set_element(fragment_set_element&& f) noexcept
-    : m_state(f.m_state),
-      m_storage(f.m_storage),
+    : m_storage(f.m_storage),
       m_pointer(f.m_pointer),
       m_size(f.m_size),
       m_prefix(std::move(f.m_prefix)),

src/deduplicator/dedupe_set/fragment_set_element.h
@@ -4,10 +4,6 @@
 #include "common/global_data/global_data_view.h"
 
 namespace uh::cluster {
-enum fragment_state : uint8_t {
-    NORM,
-    COLD,
-};
 class fragment_set_element {
 public:
     /**
@@ -64,7 +60,6 @@ public:
 
     [[nodiscard]] const std::string& prefix() const noexcept;
 
-    mutable fragment_state m_state = NORM;
     mutable std::atomic<int> m_hint_count = 0;
 
 private:

commit b77d94f99868d824bf2702d63411e2b9570532ca

fix segfault in fragment_set introduced with #633

src/deduplicator/dedupe_set/fragment_set.cpp
@@ -69,7 +69,9 @@ std::lock_guard<std::shared_mutex> fragment_set::lock() {
 
 void fragment_set::remove(
     const std::set<fragment_set_element>::const_iterator& itr) {
-    m_set.erase(itr);
+    if (itr->m_hint_count == 0) {
+        m_set.erase(itr);
+    }
 }
 
 void fragment_set::erase(const fragment& set_element, bool header) {