88179b5986c4b99fc7350ae383b8417a7bb3018c
Sungsik <sungsik@ultihash.io>
2025-01-08 09:21:20 +0100

    Change test regarding to added lease_revoke on manager's destroyer

--- test/test_etcd_manager_on_client_failure.cpp
+++ test/test_etcd_manager_on_client_failure.cpp
@@ -11,7 +11,22 @@ namespace uh::cluster {
 
 BOOST_AUTO_TEST_SUITE(when_client_has_system_failure_a_etcd_manager)
 
-BOOST_AUTO_TEST_CASE(can_read_value_before_lease_time) {
+BOOST_AUTO_TEST_CASE(cannot_read_value_after_lease_time) {
+    auto manager = std::make_unique<etcd_manager>(etcd_config{}, 2 /*second*/);
+    const auto value = std::string("172.0.0.1");
+    manager->put("/test/a", value);
+    manager.reset();
+
+    std::this_thread::sleep_for(2.5s);
+    manager.reset(new etcd_manager({}, 2));
+    auto read = manager->get("/test/a");
+
+    BOOST_TEST(read == "");
+    manager->clear_all();
+}
+
+BOOST_AUTO_TEST_CASE(
+    cannot_read_value_always_because_lease_was_revoked_on_destroyer) {
     auto manager =
         std::make_unique<etcd_manager>(etcd_config{}, 10 /*seconds*/);
     const auto value = std::string("172.0.0.1");
@@ -22,20 +37,6 @@ BOOST_AUTO_TEST_CASE(can_read_value_before_lease_time) {
 
     manager.reset(new etcd_manager({}, 10));
     std::this_thread::sleep_for(1s);
-    auto read = manager->get("/test/a");
-
-    BOOST_TEST(read == value);
-    manager->clear_all();
-}
-
-BOOST_AUTO_TEST_CASE(cannot_read_value_after_lease_time) {
-    auto manager = std::make_unique<etcd_manager>(etcd_config{}, 2 /*second*/);
-    const auto value = std::string("172.0.0.1");
-    manager->put("/test/a", value);
-    manager.reset();
-
-    std::this_thread::sleep_for(2.5s);
-    manager.reset(new etcd_manager({}, 2));
     auto read = manager->get("/test/a");
 
     BOOST_TEST(read == "");
