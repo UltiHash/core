6ecdb60a7d40fb5b0b01870c759c2f3da8c59e21
Max Plauth <max@ultihash.io>
2025-01-20 08:26:05 +0100

    avoid name collisions in sql

--- data/sql/uh_directory/V7__usage_logging.sql
+++ data/sql/uh_directory/V7__usage_logging.sql
@@ -74,9 +74,9 @@ ALTER TABLE __buckets
 CREATE OR REPLACE FUNCTION uh_get_bucket_id(bucket TEXT)
     RETURNS BIGINT
     LANGUAGE plpgsql AS $$
-DECLARE bucket_id BIGINT;
+DECLARE b_id BIGINT;
 BEGIN
-    SELECT b.id INTO bucket_id
+    SELECT b.id INTO b_id
     FROM buckets b
         LEFT JOIN bucket_status s ON b.id = s.bucket_id
     WHERE b.name = bucket
@@ -84,11 +84,11 @@ BEGIN
     ORDER BY b.id DESC
     LIMIT 1;
 
-    IF bucket_id IS NULL THEN
+    IF b_id IS NULL THEN
         RAISE EXCEPTION 'Bucket "%" does not exist.', bucket;
     END IF;
 
-    RETURN bucket_id;
+    RETURN b_id;
 END;
 $$;
 
@@ -193,24 +193,22 @@ CREATE OR REPLACE PROCEDURE uh_delete_bucket(bucket TEXT)
 DECLARE
     object_count INT;
     rows_inserted INT;
-    bucket_id BIGINT;
+    b_id BIGINT;
 BEGIN
-    SELECT count(1), o.bucket_id INTO object_count, bucket_id
+    SELECT uh_get_bucket_id(bucket) INTO b_id;
+
+    SELECT count(1) INTO object_count
     FROM objects o
         LEFT JOIN object_status s ON o.id = s.object_id
-    WHERE o.bucket_id = uh_get_bucket_id(bucket)
+    WHERE o.bucket_id = b_id
       AND s.object_id IS NULL;
 
     IF object_count != 0 THEN
         RAISE EXCEPTION 'Bucket "%" is not empty.', bucket;
     END IF;
 
-    IF bucket_id IS NULL THEN
-        RAISE EXCEPTION 'Cannot delete bucket "%", as it does not appear to exist.', bucket;
-    END IF;
-
     INSERT INTO bucket_status (bucket_id, status)
-    VALUES (bucket_id, status_deleted())
+    VALUES (b_id, status_deleted())
     ON CONFLICT DO NOTHING;
 
     GET DIAGNOSTICS rows_inserted = ROW_COUNT;
@@ -417,7 +415,7 @@ $$;
 CREATE OR REPLACE FUNCTION uh_next_deleted() RETURNS TABLE(id BIGINT, address BYTEA)
 LANGUAGE plpgsql AS $$
 DECLARE target_id BIGINT;
-DECLARE target_address BYTEA;
+        target_address BYTEA;
 BEGIN
 SELECT o.id, o.address
    INTO target_id, target_address
@@ -444,21 +442,22 @@ $$;
 --
 CREATE OR REPLACE PROCEDURE uh_put_object(bucket TEXT, object TEXT, address BYTEA, size BIGINT, etag TEXT, mime TEXT)
 LANGUAGE plpgsql AS $$
-DECLARE bucket_id BIGINT;
-        object_id BIGINT;
+DECLARE b_id BIGINT;
+        o_id BIGINT;
 BEGIN
-PERFORM uh_get_bucket_id(bucket) into bucket_id;
+SELECT uh_get_bucket_id(bucket) INTO b_id;
 
-SELECT o.id INTO object_id FROM objects o WHERE o.bucket_id = bucket_id AND name = object;
+SELECT o.id INTO o_id FROM objects o WHERE o.bucket_id = b_id AND name = object;
 
-IF object_id IS NOT NULL THEN
+IF o_id IS NOT NULL THEN
         INSERT INTO object_status (object_id, status)
-        VALUES (object_id, status_deleted());
+        VALUES (o_id, status_deleted())
+        ON CONFLICT DO NOTHING;
 END IF;
 
 EXECUTE 'INSERT INTO objects (bucket_id, name, address, size, last_modified, etag, mime)
         VALUES ($1, $2, $3, $4, $5, $6, $7)'
-    USING bucket_id, object, address, size, ceiled_now(), etag, mime;
+    USING b_id, object, address, size, ceiled_now(), etag, mime;
 END
 $$;
 
