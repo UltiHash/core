7b3625eec5105122b3579b1354e99b76e731b02d
Sungsik <sungsik@ultihash.io>
2025-01-29 19:35:08 +0100

    Add error handling on default_backend_client

--- src/common/license/backend_client.cpp
+++ src/common/license/backend_client.cpp
@@ -15,20 +15,33 @@ namespace uh::cluster {
 
 std::string default_backend_client::get_license() const {
 
-    cpr::Response r =
-        cpr::Get(cpr::Url{"https://" + m_backend_host + "/v1/license"},
-                 cpr::Authentication{m_customer_id, m_access_token,
-                                     cpr::AuthMode::BASIC});
-    return r.text;
+    auto resp = cpr::Get(cpr::Url{"https://" + m_backend_host + "/v1/license"},
+                         cpr::Authentication{m_customer_id, m_access_token,
+                                             cpr::AuthMode::BASIC});
+    if (resp.status_code == 0) {
+        throw std::runtime_error("Invalid response");
+    }
+    if (resp.status_code != 200) {
+        std::error_code ec(resp.status_code, http_category());
+        throw std::system_error(ec, "HTTP request failed");
+    }
+    return resp.text;
 }
 
 void default_backend_client::post_usage(std::string_view usage) {
     // Reference https://docs.libcpr.org/introduction.html#post-requests
-    cpr::Response r = cpr::Post(
-        cpr::Url{"https://" + m_backend_host + "/v1/usage"},
-        cpr::Body{usage.data()}, cpr::Header{{"Content-Type", "text/plain"}},
-        cpr::Authentication{m_customer_id, m_access_token,
-                            cpr::AuthMode::BASIC});
+    auto resp = cpr::Post(cpr::Url{"https://" + m_backend_host + "/v1/usage"},
+                          cpr::Body{usage.data()},
+                          cpr::Header{{"Content-Type", "text/plain"}},
+                          cpr::Authentication{m_customer_id, m_access_token,
+                                              cpr::AuthMode::BASIC});
+    if (resp.status_code == 0) {
+        throw std::runtime_error("Invalid response");
+    }
+    if (resp.status_code != 200) {
+        std::error_code ec(resp.status_code, http_category());
+        throw std::system_error(ec, "HTTP request failed");
+    }
 }
 
 } // namespace uh::cluster
