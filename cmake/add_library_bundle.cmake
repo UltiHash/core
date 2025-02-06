dcd15223c00ce48ea41a6fb76c2f9959e521fce6
Sungsik <sungsik@ultihash.io>
2025-02-06 17:13:56 +0100

    Fix bug on add_library_bundle

--- cmake/add_library_bundle.cmake
+++ cmake/add_library_bundle.cmake
@@ -7,51 +7,62 @@ function(register_additional_interface target)
         PARENT_SCOPE)
 endfunction()
 
+# Declare object/static/shared library
+# Usage:
+# - SOURCES: list of source files
+# - SETTING: list of interfaces, which are used to build the library
+# - INTERFACE: list of interfaces, which should also be propagated to the dependents
+# - PRIVATE: list of libraries, which should be linked privately
+# - PUBLIC: list of libraries, which should be linked publicly
+# - PRIVATE_BUNDLE: list of library bundles, which should be linked privately 
+# - PUBLIC_BUNDLE: list of library bundles, which should be linked publicly
 function(add_library_bundle name)
     cmake_parse_arguments(
-        ARG "" "SOURCES"
-        "SETTING;INTERFACE;PRIVATE;PUBLIC;PRIVATE_BUNDLE;PUBLIC_BUNDLE" ${ARGN})
+        ARGS "" ""
+        "SOURCES;SETTING;INTERFACE;PRIVATE;PUBLIC;PRIVATE_BUNDLE;PUBLIC_BUNDLE" ${ARGN})
 
-    add_library(${name}_object OBJECT ${ARG_SOURCES})
+    add_library(${name}_object OBJECT ${ARGS_SOURCES})
     target_link_libraries(
         ${name}_object
-        PRIVATE ${ARG_INTERFACE} ${ADDITIONAL_INTERFACES} ${ARG_PRIVATE}
-                ${ARG_SETTING} ${ARG_PRIVATE_BUNDLE}
-        PUBLIC ${ARG_PUBLIC} ${ARG_PUBLIC_BUNDLE}
-        INTERFACE ${ARG_INTERFACE} ${ADDITIONAL_INTERFACES})
+        PRIVATE ${ARGS_INTERFACE} ${ADDITIONAL_INTERFACES} ${ARGS_PRIVATE}
+                ${ARGS_SETTING} ${ARGS_PRIVATE_BUNDLE}
+        PUBLIC ${ARGS_PUBLIC} ${ARGS_PUBLIC_BUNDLE}
+        INTERFACE ${ARGS_INTERFACE} ${ADDITIONAL_INTERFACES})
 
     add_library(${name} STATIC $<TARGET_OBJECTS:${name}_object>)
     target_link_libraries(
         ${name}
-        PRIVATE ${ARG_INTERFACE} ${ADDITIONAL_INTERFACES} ${ARG_PRIVATE}
-                ${ARG_SETTING} ${ARG_PRIVATE_BUNDLE}
-        PUBLIC ${ARG_PUBLIC} ${ARG_PUBLIC_BUNDLE}
-        INTERFACE ${ARG_INTERFACE} ${ADDITIONAL_INTERFACES})
+        PRIVATE ${ARGS_INTERFACE} ${ADDITIONAL_INTERFACES} ${ARGS_PRIVATE}
+                ${ARGS_SETTING} ${ARGS_PRIVATE_BUNDLE}
+        PUBLIC ${ARGS_PUBLIC} ${ARGS_PUBLIC_BUNDLE}
+        INTERFACE ${ARGS_INTERFACE} ${ADDITIONAL_INTERFACES})
 
     set(private_static)
     set(public_static)
-    foreach(lib ${ARG_PRIVATE})
+    foreach(lib ${ARGS_PRIVATE})
         list(APPEND private_static $<LINK_LIBRARY:WHOLE_ARCHIVE,${lib}>)
     endforeach()
 
-    foreach(lib ${ARG_PUBLIC})
+    foreach(lib ${ARGS_PUBLIC})
         list(APPEND public_static $<LINK_LIBRARY:WHOLE_ARCHIVE,${lib}>)
     endforeach()
 
     set(private_bundle_shared "")
     set(public_bundle_shared "")
-    foreach(dep IN LISTS ARG_PRIVATE_BUNDLE)
+    foreach(dep IN LISTS ARGS_PRIVATE_BUNDLE)
         list(APPEND private_bundle_shared "${dep}_shared")
     endforeach()
-    foreach(dep IN LISTS ARG_PUBLIC_BUNDLE)
+    foreach(dep IN LISTS ARGS_PUBLIC_BUNDLE)
         list(APPEND public_bundle_shared "${dep}_shared")
     endforeach()
 
     add_library(${name}_shared SHARED $<TARGET_OBJECTS:${name}_object>)
     target_link_libraries(
         ${name}_shared
-        PRIVATE ${ARG_INTERFACE} ${ADDITIONAL_INTERFACES} ${private_static}
-                ${ARG_SETTING} ${private_bundle_shared}
-        PUBLIC ${public_static} ${public_bundle_shared}
-        INTERFACE ${ARG_INTERFACE} ${ADDITIONAL_INTERFACES})
+        PRIVATE ${ARGS_INTERFACE} ${ADDITIONAL_INTERFACES} ${private_static}
+                ${ARGS_SETTING}
+        PUBLIC ${public_static} ${public_bundle_shared} 
+               # Exceptional visibility: every shared library should be propagated
+               ${private_bundle_shared} 
+        INTERFACE ${ARGS_INTERFACE} ${ADDITIONAL_INTERFACES})
 endfunction()
