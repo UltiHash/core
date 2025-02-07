4b28e18deb366a031c4d53e166c9b98967d8250b
sjulti <stefan@ultihash.io>
2025-02-07 14:45:54 +0100

    Fixup: remove json parser for CORS.

--- src/entrypoint/cors/parser.h
+++ src/entrypoint/cors/parser.h
@@ -7,7 +7,6 @@ namespace uh::cluster::ep::cors {
 class parser {
 public:
     static std::map<std::string, info> parse(std::string code);
-    static std::map<std::string, info> parse_json(std::string code);
 };
 
 } // namespace uh::cluster::ep::cors
