7a39cce4d558810354c95c3e30d29a691fd0a1fd
sjulti <stefan@ultihash.io>
2025-02-07 13:32:19 +0100

    Fixup: database scripts.

--- data/sql/uh_directory/V8__bucket_cors.sql
+++ data/sql/uh_directory/V8__bucket_cors.sql
@@ -6,32 +6,28 @@ ALTER TABLE buckets ADD COLUMN cors XML;
 --
 -- uh_get_bucket(bucket): get policy and cors for a bucket
 --
-CREATE OR REPLACE FUNCTION uh_get_bucket_cors(bucket TEXT)
-    RETURNS TABLE(policy JSON, cors XML)
+CREATE OR REPLACE FUNCTION uh_bucket_cors(bucket TEXT)
+    RETURNS TABLE(cors XML)
 LANGUAGE plpgsql AS $$
 DECLARE cors_record XML;
 BEGIN
-    SELECT cors INTO cors_record
-    FROM buckets
-    WHERE name = bucket;
+    SELECT buckets.cors INTO cors_record FROM buckets WHERE name = bucket;
 
     IF NOT FOUND THEN
         RAISE EXCEPTION 'Bucket "%s" does not exist in buckets table', bucket;
     END IF;
 
-    RETURN QUERY SELECT XMLSERIALIZE(DOCUMENT cors_record AS text);
+    RETURN QUERY SELECT cors_record;
 END;
 $$;
 
 --
 -- uh_bucket_set_cors(bucket): set the cors for a bucket
 --
-CREATE OR REPLACE PROCEDURE uh_bucket_set_cors(bucket regclass, cors XML)
+CREATE OR REPLACE PROCEDURE uh_bucket_set_cors(bucket TEXT, config XML)
 LANGUAGE plpgsql AS $$
 BEGIN
-    UPDATE buckets
-    SET cors = XMLPARSE(DOCUMENT cors)
-    WHERE name = bucket;
+    UPDATE buckets SET cors = XMLPARSE(DOCUMENT config) WHERE name = bucket;
 
     IF NOT FOUND THEN
         RAISE EXCEPTION 'Bucket "%s" does not exist in buckets table', bucket;
