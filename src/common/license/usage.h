efc08b66c0c14bdd5033f8cf269d3244fbe94f3e
Max Plauth <smaxen@me.com>
2025-02-13 11:42:22 +0100

    ENG-874: submit usage metrics to licensing backend every full hour (#675)

---
+++ src/common/license/usage.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <common/db/db.h>
+#include <common/types/common_types.h>
+
+namespace uh::cluster {
+
+class usage {
+public:
+    usage(boost::asio::io_context& ioc, const db::config& cfg)
+        : m_db(ioc, connection_factory(ioc, cfg, cfg.directory),
+               cfg.directory.count) {}
+
+    coro<std::size_t> get_usage_for_interval(const utc_time& interval_infimum,
+                                             const utc_time& interval_supremum);
+
+private:
+    pool<db::connection> m_db;
+};
+} // namespace uh::cluster
