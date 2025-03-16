22aa946ca8aa0f711a5a327ef7925c896fe44ea4
Sungsik Nam <jmyl@me.com>
2025-03-16 16:34:43 +0100

    Multipart upload succeeded

--- src/entrypoint/multipart_state.cpp
+++ src/entrypoint/multipart_state.cpp
@@ -23,10 +23,11 @@ multipart_state::instance::lock_upload(const std::string& id) {
 
     boost::asio::co_spawn(
         executor,
-        [f = std::move(f), handle = m_handle, id]() mutable -> coro<void> {
+        [f = std::move(f), handle = m_handle,
+         id]() mutable -> notrace_coro<void> {
             co_await f.get();
             co_await (*handle)->execv("CALL uh_unlock_upload($1)", id);
-        }(),
+        },
         boost::asio::detached);
 
     co_return release_lock{std::move(p)};
