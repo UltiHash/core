7b365a8a156c817b26efa1fe2843fd70c040ada3
Sungsik <sungsik@ultihash.io>
2025-04-28 13:34:33 +0200

    Try to fix CI failure

--- src/storage/group/config.h
+++ src/storage/group/config.h
@@ -7,9 +7,9 @@ namespace uh::cluster::storage {
 
 struct group_config {
     // TODO: Initialize them using {}.
-    std::size_t data_shards{2};
+    std::size_t data_shards{1};
     std::size_t parity_shards{0};
-    std::vector<std::size_t> members{0, 1};
+    std::vector<std::size_t> members{0};
 
     static group_config create(std::string_view json_str);
     static std::vector<group_config> create_multiple(std::string_view json_str);
