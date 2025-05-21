29af8c0a00623ee036ca070c3be4f35f9d96362d
Sungsik <sungsik@ultihash.io>
2025-05-21 12:40:26 +0200

    throw exception with checking fragment's last byte

--- src/storage/group/impl/address_utils.cpp
+++ src/storage/group/impl/address_utils.cpp
@@ -1,6 +1,7 @@
 #include "address_utils.h"
 
-#include "common/coroutines/promise.h"
+#include <common/coroutines/promise.h>
+#include <common/utils/error.h>
 
 namespace uh::cluster {
 
@@ -14,6 +15,18 @@ std::unordered_map<std::size_t, address_info> extract_node_address_map(
     for (size_t i = 0; i < addr.size(); ++i) {
         auto frag = addr.get(i);
         const auto [id, storage_ptr] = get_storage_pointer(frag.pointer);
+        const auto [last_id, _] =
+            get_storage_pointer(frag.pointer + frag.size - 1);
+        if (id != last_id) {
+            throw error_exception(
+                error(error::type::unknown,
+                      std::format("fragment covers multiple storages; "
+                                  "storage_id_of_first_byte: {}, "
+                                  "storage_id_of_last_byte: {}, "
+                                  "pointer: {:X}, size: {}",
+                                  id, last_id, frag.pointer, frag.size)));
+        }
+
         frag.pointer = storage_ptr;
 
         auto& node_pos = addr_map[id];
