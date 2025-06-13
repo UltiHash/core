ce2f22fa4a23f10fe6bee6973e746d4dae71f2cc
Max Plauth <max@ultihash.io>
2025-06-13 10:18:30 +0200

    keep default alignment also on data store level, mostly for testing

--- src/storage/default_data_store.h
+++ src/storage/default_data_store.h
@@ -24,7 +24,8 @@ public:
      * @return local pointer to the allocated storage space and size of
      * allocation
      */
-    allocation_t allocate(std::size_t size, std::size_t alignment);
+    allocation_t allocate(std::size_t size,
+                          std::size_t alignment = DEFAULT_PAGE_SIZE);
 
     /**
      * Writes data to persistent storage. On completion, the provided data
