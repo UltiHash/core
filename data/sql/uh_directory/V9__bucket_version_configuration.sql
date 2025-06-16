6d80bb995e5e9fbb270d35c08b2803386eefaf13
sjulti <stefan@ultihash.io>
2025-06-16 11:42:23 +0200

    Add versioning configuration for buckets.

---
+++ data/sql/uh_directory/V9__bucket_version_configuration.sql
@@ -0,0 +1,54 @@
+--
+-- Constants to describe versioning state
+--
+CREATE OR REPLACE FUNCTION versioning_disabled() RETURNS TEXT
+LANGUAGE sql IMMUTABLE PARALLEL SAFE AS
+    'SELECT ''Disabled''';
+
+CREATE OR REPLACE FUNCTION versioning_enabled() RETURNS TEXT
+LANGUAGE sql IMMUTABLE PARALLEL SAFE AS
+    'SELECT ''Enabled''';
+
+CREATE OR REPLACE FUNCTION versioning_suspended() RETURNS TEXT
+LANGUAGE sql IMMUTABLE PARALLEL SAFE AS
+    'SELECT ''Suspended''';
+
+--
+-- Add column defining configured versioning for a bucket.
+--
+ALTER TABLE buckets ADD COLUMN
+    versioning TEXT NOT NULL DEFAULT versioning_disabled();
+
+--
+-- uh_bucket_versioning(bucket): return configured versioning
+-- for the given bucket
+--
+CREATE OR REPLACE PROCEDURE uh_bucket_set_versioning(bucket TEXT, status TEXT)
+LANGUAGE plpgsql AS $$
+BEGIN
+    UPDATE buckets SET versioning = status WHERE name = bucket;
+
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'Bucket "%s" does not exist in buckets table', bucket;
+    END IF;
+END;
+$$;
+
+--
+-- uh_bucket_versioning(bucket): return configured versioning
+-- for the given bucket
+--
+CREATE OR REPLACE FUNCTION uh_bucket_versioning(bucket TEXT)
+    RETURNS TABLE(status TEXT)
+LANGUAGE plpgsql AS $$
+DECLARE rv TEXT;
+BEGIN
+    SELECT versioning INTO rv FROM buckets WHERE name = bucket;
+
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'Bucket "%s" does not exist in buckets table', bucket;
+    END IF;
+
+    RETURN QUERY SELECT rv;
+END;
+$$;
