f5bab09a607ce9fed99db505bfb392bf84ab8cc7
Sungsik <sungsik@ultihash.io>
2025-06-24 17:13:58 +0200

    WIP: spawning repairer

--- src/storage/group/repairer.h
+++ src/storage/group/repairer.h
@@ -5,6 +5,7 @@
 #include <storage/global/config.h>
 #include <storage/group/config.h>
 #include <storage/group/externals.h>
+#include <storage/group/internals.h>
 
 namespace uh::cluster::storage {
 
@@ -21,11 +22,10 @@ public:
                        service_factory<storage_interface>(
                            m_executor.get_executor(),
                            m_global_config.storage_service_connection_count)) {
-        (void)m_subscriber;
-        // TODO: Spawn coroutines to restore data
-        //
-        // storage_assignment_triggers_manager::put(m_etcd, m_group_config.id,
-        //                                          true);
+        boost::asio::co_spawn(
+            executor.get_executor(),
+            [this]() -> coro<void> { co_await repair(); },
+            boost::asio::detached);
     }
 
 private:
@@ -35,6 +35,20 @@ private:
     global_data_view_config m_global_config;
 
     externals_subscriber m_subscriber;
+
+    coro<void> repair() {
+
+        auto timer = boost::asio::steady_timer(
+            co_await boost::asio::this_coro::executor);
+        timer.expires_after(1s);
+        co_await timer.async_wait(boost::asio::use_awaitable);
+
+        // if( error )
+        // co_return;
+
+        storage_assignment_triggers_manager::put(m_etcd, m_group_config.id,
+                                                 true);
+    }
 };
 
 } // namespace uh::cluster::storage
