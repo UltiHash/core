0e57bbe445f909ea253d8dfcc52c3c7a668f0ffd
Sungsik Nam <jmyl@me.com>
2025-08-07 13:04:26 +0200

    Add get_raw_buffer interface on body

--- src/entrypoint/http/raw_body.h
+++ src/entrypoint/http/raw_body.h
@@ -3,6 +3,8 @@
 #include "body.h"
 #include "raw_request.h"
 
+#include <vector>
+
 namespace uh::cluster::ep::http {
 
 class raw_body : public body {
@@ -13,10 +15,16 @@ public:
 
     coro<std::size_t> read(std::span<char> dest) override;
 
+    std::vector<boost::asio::const_buffer> get_raw_buffer() const override {
+        return m_raw_buffers;
+    }
+
 private:
     boost::asio::ip::tcp::socket& m_socket;
     std::vector<char> m_buffer;
     std::size_t m_length;
+    std::size_t m_read_position = 0;
+    std::vector<boost::asio::const_buffer> m_raw_buffers;
 };
 
 } // namespace uh::cluster::ep::http
