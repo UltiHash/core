7929dd4a41162bc2942601bea8feb911486af346
Sungsik <sungsik@ultihash.io>
2025-08-12 15:30:43 +0200

    Make abstract class for abstracting caches

--- test/unit/cache.h
+++ test/unit/cache.h
@@ -17,7 +17,23 @@ concept has_size = requires(const Value& v) {
 
 template <typename Key, typename Value>
 requires has_size<Value>
-class lru_cache {
+class cache_base {
+public:
+    virtual ~cache_base() = default;
+
+    virtual Value
+    access(const Key& key) = 0; // 공통 기능: 접근 및 내부 상태 업데이트
+    virtual Value get(const Key& key) const = 0; // 읽기 전용 접근
+    virtual void put(const Key& key, const Value& value) = 0;
+    virtual bool contains(const Key& key) const = 0;
+    virtual size_t size() const = 0;
+    virtual size_t get_used_space() const = 0;
+    virtual void clear() = 0;
+};
+
+template <typename Key, typename Value>
+requires has_size<Value>
+class lru_cache : public cache_base<Key, Value> {
 private:
     size_t m_capacity;
     size_t m_current_size = 0;
@@ -42,7 +58,7 @@ public:
         m_items.splice(m_items.begin(), m_items, it->second);
     }
 
-    Value access(const Key& key) {
+    Value access(const Key& key) override {
         std::unique_lock lock(m_mutex);
         auto it = m_cache.find(key);
         if (it == m_cache.end()) {
@@ -53,7 +69,7 @@ public:
         return it->second->second;
     }
 
-    Value get(const Key& key) const {
+    Value get(const Key& key) const override {
         std::shared_lock lock(m_mutex);
         auto it = m_cache.find(key);
         if (it == m_cache.end()) {
@@ -63,12 +79,7 @@ public:
         return it->second->second;
     }
 
-    bool contains(const Key& key) const {
-        std::shared_lock lock(m_mutex);
-        return m_cache.find(key) != m_cache.end();
-    }
-
-    void put(const Key& key, const Value& value) {
+    void put(const Key& key, const Value& value) override {
         std::unique_lock lock(m_mutex);
 
         size_t value_size = value.size();
@@ -94,17 +105,22 @@ public:
         m_current_size += value_size;
     }
 
-    size_t size() const {
+    bool contains(const Key& key) const override {
+        std::shared_lock lock(m_mutex);
+        return m_cache.find(key) != m_cache.end();
+    }
+
+    size_t size() const override {
         std::shared_lock lock(m_mutex);
         return m_cache.size();
     }
 
-    size_t get_used_space() const {
+    size_t get_used_space() const override {
         std::shared_lock lock(m_mutex);
         return m_current_size;
     }
 
-    void clear() {
+    void clear() override {
         std::unique_lock lock(m_mutex);
         m_items.clear();
         m_cache.clear();
